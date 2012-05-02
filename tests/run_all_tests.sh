1b1c7279004a46f021a27660da574cbc3f5f89e4
Vince Weaver <vince@deater.net>
2012-05-02 11:36:55 -0400

    Add a run_all_tests script

---
+++ tests/run_all_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+./test_delete.sh
+./test_large_files.sh
+./test_lock.sh
+./test_lots_of_files.sh
+./test_odd_filenames.sh
+./test_rename.sh
+./test.sh
+./test_undelete2.sh
+./test_undelete.sh
