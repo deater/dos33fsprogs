956766ecc75e0dc11003af2f6df2a1c62e1d0782
Vince Weaver <vincent.weaver@maine.edu>
2016-05-24 16:04:32 -0400

    ksp: update launch files

--- ksp/c/ksp_launch.c
+++ ksp/c/ksp_launch.c
@@ -6,9 +6,20 @@
 /* Also, high school physics (thanks Mr. Brennen) */
 
 
+void home(void) {
+	printf("%c[2J%c[1;1H",27,27);
+}
+
+void htabvtab(int x,int y) {
+	printf("%c[%d;%dH",27,y,x);
+}
+
+
 /* TODO: want 2d-vectors at least */
 
 int main(int argc, char **argv) {
+
+	double angle=0;
 
 	double capsule_mass=1.0;
 
@@ -19,6 +30,7 @@ int main(int argc, char **argv) {
 
 	double tank_mass=0.5;		/* tons */
 	double fuel_mass=4.0;		/* tons */
+	double total_fuel=fuel_mass;
 
 	double gravity=9.8;		/* m/s^2 */
 
@@ -30,6 +42,10 @@ int main(int argc, char **argv) {
 	double total_mass,empty_mass;
 
 	double time=0.0;		/* s */
+
+	int stage=1;
+
+	char input;
 
 	total_mass=engine_mass+tank_mass+fuel_mass+capsule_mass;
 	empty_mass=total_mass-fuel_mass;
@@ -45,9 +61,7 @@ int main(int argc, char **argv) {
 	printf("Fuel flow rate=%lf, time=%lfs\n",
 		fuel_flow_rate,fuel_mass/fuel_flow_rate);
 
-	for(time=0.0;time<500.0;time++) {
-		printf("Time: %lf\n",time);
-		printf("\tFuel left: %lf\n",fuel_mass);
+	while(1) {
 		if (fuel_mass<0.1) {
 			fuel_mass=0.0;
 			rocket_acceleration=-gravity;
@@ -69,9 +83,32 @@ int main(int argc, char **argv) {
 			fuel_mass=fuel_mass-fuel_flow_rate;
 			total_mass=engine_mass+tank_mass+fuel_mass+capsule_mass;
 		}
-		printf("\tRocket acceleration: %lf g\n",rocket_acceleration/gravity);
-		printf("\tRocket velocity: %lf m/s\n",rocket_velocity);
-		printf("\tRocket altitude: %lf m\n",rocket_altitude);
+
+		home();
+
+		htabvtab(1,21);
+
+		printf("Time: %lf\n",time);
+		printf("ALT: %lf m\n",rocket_altitude);
+		printf("VEL: %lf m/s\tStage: %d\n",rocket_velocity,stage);
+		printf("ACCEL: %lf g\tFuel: %lf%%\n",
+			rocket_acceleration/gravity,
+			fuel_mass*100.0/total_fuel);
+
+		htabvtab(30,21);
+		printf("ZURGTROYD");
+
+		htabvtab(30,20);
+		if ((angle>90) && (angle<270)) printf("SCREAM");
+		else if (rocket_velocity>100) printf("SMILE");
+		else if (rocket_acceleration<0) printf("FROWN");
+		else printf("NEUTRAL");
+
+
+
+		scanf("%c",&input);
+
+		time+=1.0;
 	}
 
 	return 0;
