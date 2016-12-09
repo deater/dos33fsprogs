1e90deb41c54043da586c35f52c835745aec97cd
Vince Weaver <vince@deater.net>
2016-12-08 23:04:56 -0500

    char2hex: whitespace cleanup

--- dos33fs-utils/char2hex.c
+++ dos33fs-utils/char2hex.c
@@ -3,15 +3,14 @@
 #include "version.h"
 
 int main(int argc, char **argv) {
-   
-   int input;
-   
-   while(1) {
-    
-      input=fgetc(stdin);
-      if (input==EOF) break;
-      printf("$%02X,",input|0x80);
-   }
- 
-   return 0;
+
+	int input;
+
+	while(1) {
+		input=fgetc(stdin);
+		if (input==EOF) break;
+		printf("$%02X,",input|0x80);
+	}
+
+	return 0;
 }
