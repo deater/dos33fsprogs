65bfb2ba45d974fddeb73aa2d7787f8b19dd09bf
Vince Weaver <vincent.weaver@maine.edu>
2018-02-14 13:03:32 -0500

    gr-sim: working on l4d

--- gr-sim/l4d.c
+++ gr-sim/l4d.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "gr-sim.h"
+#include "6502_emulate.h"
 
 #define MAX_INPUT	65536
 static unsigned char input[MAX_INPUT];
@@ -19,14 +19,69 @@ static unsigned char input[MAX_INPUT];
 #define A4H	0x43
 
 static short s;
+static int n,z,c,v;
 
-void getsrc(void) {
+void adc(int value) {
+
+	int temp_a;
+	int temp_value;
+	int result;
+
+	temp_a=a&0xff;
+	temp_value=value&0xff;
+
+	result=(temp_a+temp_value+c);
+
+	c=(result&0x100)>>8;
+	n=(result&0x80)>>7;
+
+	v=!!((a^result)&(value^result)&0x80);
+
+	a=result&0xff;
+	z=(a==0);
+
+}
+
+void sbc(int value) {
+	int temp_a;
+	int result;
+	int temp_value;
+
+	temp_a=a&0xff;
+	temp_value=value&0xff;
+
+	result=temp_a-temp_value-(!c);
+
+	c=(result&0x100)>>8;
+	n=(result&0x80)>>7;
+
+	v=!!((a^result)&((255-value)^result)&0x80);
+
+	a=result&0xff;
+	z=(a==0);
+}
+
+void cmp(int value) {
+
+	int temp_a;
+	int temp_value;
+
+	temp_a=a&0xff;
+	temp_value=value&0xff;
+	temp_a=temp_a-temp_value;
+	c=(temp_a&0x100)>>8;
+	n=(temp_a&0x80)>>7;
+	z=(a==0);
+}
+
+static void getsrc(void) {
 //getsrc:
 	a=ram[y_indirect(src,y)];		// lda 	(src), y
 	ram[src]++;				//inc	src
 	if (ram[src]!=0) goto done_getsrc;	//bne	+
 	ram[src+1]++;				//inc	src+1
 done_getsrc: ;
+	printf("LOADED %02X%02X-1: %02X\n",ram[src+1],ram[src],a);
 	//+	rts
 }
 
@@ -44,28 +99,61 @@ static void pla(void) {
 
 void buildcount(void) {
 
-	int cnew,c;
-
 //buildcount:
 	x=1;					// ldx	#1
-	ram[count+1]=x; 			//stx	count+1
-	if (a!=0xf) goto done_buildcount;	//cmp	#$0f
-						//bne	++
+	ram[count+1]=x; 			// stx	count+1
+	cmp(0xf);				// cmp	#$0f
+	if (z==0) goto done_buildcount;		// bne	++
 minus_buildcount:
 	ram[count]=a;				//-	sta	count
 	getsrc();				//jsr	getsrc
 	x=a;					//tax
 	c=0;					//clc
-	cnew=(a+ram[count]+c)>0xff;
-	a=a+ram[count]+c;			//adc	count
-	if (cnew) goto skip_buildcount;		// bcc	+
+	adc(ram[count]);			//adc	count
+	if (c==0) goto skip_buildcount;		// bcc	+
 	ram[count+1]++;				//inc	count+1
 skip_buildcount:
 	x++;					//+	inx
-	x&=0xf;
+	x&=0xff;
 	if (x==0) goto minus_buildcount;	//beq	-
 done_buildcount: ;				//++	rts
 }
+
+
+static void putdst(void) {
+	printf("PUTADDR=%04X\n",y_indirect(dst,y));
+						// putdst:
+	ram[y_indirect(dst,y)]=a;		//	sta 	(dst), y
+	ram[dst]++;				//	inc	dst
+	if (ram[dst]!=0) goto putdst_end;	//	bne	+
+	ram[dst+1]++;				//	inc	dst+1
+putdst_end:;
+						//+	rts
+	printf("PUT: %02X%02X-1,%02X = %02X\n",ram[dst+1],ram[dst],y,a);
+	printf("0x8000 = %02X\n",ram[0x8000]);
+}
+
+static void getput(void) {
+						// getput:
+	getsrc();				// jsr	getsrc
+	putdst();				// ; fallthrough
+}
+
+static void docopy(void) {
+						// docopy:
+docopy_label:
+	getput();				// jsr	getput
+	x--;					// dex
+	if (x!=0) goto docopy_label;		// bne	docopy
+	ram[count+1]--;				// dec	count+1
+	if (ram[count+1]!=0) goto docopy_label;	//bne	docopy
+						//rts
+}
+
+
+
+
+
 
 int main(int argc, char **argv) {
 
@@ -98,7 +186,7 @@ int main(int argc, char **argv) {
 	//oep = 0; //first unpacked byte to run, you must set this by yourself
 	orgoff = 0x8000; //offset of first unpacked byte, you must set this by yourself
 	paksize	= size; //size of packed data, you must set this by yourself if hiunp=0
-	pakoff = 0x2000;
+	pakoff = 0x2008; // 8 byte offset to data?
 
 
 //LCBANK2	=	$c083
@@ -117,88 +205,84 @@ int main(int argc, char **argv) {
 	a=(orgoff&0xff);	//lda	#<orgoff
 	ram[dst]=a;		// sta	dst
 
-unpack:				//;unpacker entrypoint
-	goto unpmain;		//	jmp	unpmain
+	printf("packed addr: %02X%02X\n",ram[src+1],ram[src]);
+	printf("packed end : %02X%02X\n",ram[end+1],ram[end]);
+	printf("dest addr  : %02X%02X\n",ram[dst+1],ram[dst]);
+
+
+//unpack:				//;unpacker entrypoint
+	goto unpmain;			// jmp	unpmain
 
 unpmain:
-	y=0;			//ldy	#0
+	y=0;				//ldy	#0
 
 parsetoken:
-	getsrc();		// jsr	getsrc
-	pha();			// pha
-	a<<=1;			// lsr
-	a<<=1;			// lsr
-	a<<=1;			// lsr
-	a<<=1;			// lsr
+	getsrc();			// jsr	getsrc
+	pha();				// pha
+	a>>=1;				// lsr
+	a>>=1;				// lsr
+	a>>=1;				// lsr
+	a>>=1;				// lsr
 	a&=0xff;
-	if (a==0) goto copymatches; //beq	copymatches
-#if 0
-	jsr	buildcount
-	tax
-	jsr	docopy
-	lda	src
-	cmp	end
-	lda	src+1
-	sbc	end+1
-	bcs	done
-#endif
+	if (a==0) goto copymatches; 	// beq	copymatches
+
+	buildcount();			// jsr	buildcount
+	x=a;				// tax
+	docopy();			// jsr	docopy
+	a=ram[src];			// lda	src
+	cmp(ram[end]);			// cmp	end
+	a=ram[src+1];			// lda	src+1
+	sbc(ram[end+1]);		// sbc	end+1
+	if (c) {
+		printf("Done!\n");
+		printf("src        : %02X%02X\n",ram[src+1],ram[src]);
+		printf("packed end : %02X%02X\n",ram[end+1],ram[end]);
+		goto done;		// bcs	done
+	}
+
 copymatches:
-	getsrc();		//jsr	getsrc
-#if 0
-	sta	delta
-	jsr	getsrc
-	sta	delta+1
-	pla
-	and	#$0f
-	jsr	buildcount
-	clc
-	adc	#4
-	tax
-	bcc	+
-	inc	count+1
-+	lda	src+1
-	pha
-	lda	src
-	pha
-	sec
-	lda	dst
-	sbc	delta
-	sta	src
-	lda	dst+1
-	sbc	delta+1
-	sta	src+1
-	jsr	docopy
-	pla
-	sta	src
-	pla
-	sta	src+1
-	jmp	parsetoken
+	getsrc();			// jsr	getsrc
+	ram[delta]=a;			// sta	delta
+	getsrc();			// jsr	getsrc
+	ram[delta+1]=a;			// sta	delta+1
+	pla();				// pla
+	a=a&0xf;			// and	#$0f
+	buildcount();			// jsr	buildcount
+	c=0;				// clc
+	adc(4);				// adc	#4
+	x=a;				// tax
+	if (c==0) goto copy_skip;	// bcc	+
+	ram[count+1]++;			// inc	count+1
+copy_skip:
+	a=ram[src+1];			//+	lda	src+1
+	pha();				// pha
+	a=ram[src];			// lda	src
+	pha();				// pha
+	c=1;				// sec
+	a=ram[dst];			// lda	dst
+	sbc(ram[delta]);		// sbc	delta
+	ram[src]=a;			// sta	src
+	a=ram[dst+1];			// lda	dst+1
+	sbc(ram[delta+1]);		// sbc	delta+1
+	ram[src+a]=a;			// sta	src+1
+	docopy();			// jsr	docopy
+	pla();				// pla
+	ram[src]=a;			// sta	src
+	pla();				// pla
+	ram[src+1]=a;			// sta	src+1
+	goto parsetoken;		// jmp	parsetoken
 
-done
-	pla
-	rts
-
-docopy
-	jsr	getput
-	dex
-	bne	docopy
-	dec	count+1
-	bne	docopy
-	rts
+done:
+	pla();				// pla
 
 
+	int i;
+	for(i=0;i<256;i++) {
+		if (i%16==0) printf("\n%04X: ",0x8000+i);
+		printf("%02X ",ram[0x8000+i]);
+	}
 
-getput
-	jsr	getsrc
-
-putdst
-	sta 	(dst), y
-	inc	dst
-	bne	+
-	inc	dst+1
-+	rts
-
-#endif
-
+	printf("\n");
+					// rts
 	return 0;
 }
