c1d5cb5281110c57ed4cbf2467de248e9528b1dc
Vince Weaver <vincent.weaver@maine.edu>
2018-07-03 16:12:39 -0400

    tfv: hook up some more dialog

--- gr-sim/tfv/tfv_dialog.h
+++ gr-sim/tfv/tfv_dialog.h
@@ -11,6 +11,33 @@
 #define DIALOG_MATHEMAGICIAN	3
 #define DIALOG_DARTH_TATER	4
 
+#define DIALOG_OSCAR		5
+#define DIALOG_NICOLE		6
+#define DIALOG_CINDY		7
+#define DIALOG_ELAINE		8
+#define DIALOG_CAFETERIA_LADY	9
+
+#define DIALOG_METRO_WORKER	10
+#define DIALOG_TINY_CAPABARA	11
+#define DIALOG_GIANT_GUINEA_PIG	12
+#define DIALOG_LARGE_BIRD	13
+
+#define DIALOG_MERMAID		14
+
+#define DIALOG_CMW		15
+#define DIALOG_MALL		16
+#define DIALOG_MINIGOLF		17
+
+#define DIALOG_SCARYNUN		18
+#define DIALOG_GUS		19
+#define DIALOG_RAISTLIN		20
+#define DIALOG_FORD		21
+
+#define DIALOG_CAPTAIN_STEVE	22
+#define DIALOG_BRIGHID		23
+#define DIALOG_RACHAEL		24
+#define DIALOG_MREE		25
+
 struct dialog_words {
 	char *words;
 	int next;
@@ -25,6 +52,7 @@ struct dialog_type {
 
 struct dialog_type dialog[100]={
 
+	// Talbot Hall
 	[DIALOG_LIZ_WILL] = {
 		.statement[0].words="Let\'s discuss cool things in the lounge.",
 		.statement[0].next=0,
@@ -36,7 +64,7 @@ struct dialog_type dialog[100]={
 		.statement[2].action=ACTION_TIME,
 	},
 	[DIALOG_PETE] = {
-		.statement[0].words="Your journey may take you to darkest Bel Air.",
+		.statement[0].words="Your journey takes you toward Bel Air.",
 		.statement[0].next=1,
 		.statement[1].words="PLOT!",
 		.statement[1].next=1,
@@ -57,65 +85,99 @@ struct dialog_type dialog[100]={
 		.statement[2].action=ACTION_ITEM2,
 		.statement[2].item=ITEM_5K_RESISTOR,
 	},
+	// Dining Hall
+	[DIALOG_OSCAR]= {
+		.statement[0].words="Beware the killer crabs",
+		.statement[0].next=1,
+		.statement[1].words="They want to meet you",
+		.statement[1].next=2,
+		.statement[2].words="They want to eat you",
+		.statement[2].next=0,
+	},
+	[DIALOG_NICOLE]= {
+		.statement[0].words="...",
+		.statement[0].next=0,
+	},
+	[DIALOG_CINDY]= {
+		.statement[0].words="...",
+		.statement[0].next=0,
+	},
+	[DIALOG_ELAINE]= {
+		.statement[0].words="Remember Ohm\'s Law",
+		.statement[0].next=0,
+		.statement[1].words="Twinkle Twinkle Little Star",
+		.statement[1].next=0,
+		.statement[2].words="V is equal to IR",
+		.statement[2].next=0,
+	},
+	[DIALOG_CAFETERIA_LADY]= {
+		.statement[0].words="Happpy Birthday!",
+		.statement[0].next=2,
+		.statement[1].words="Have a cupcake",
+		.statement[1].next=0,
+	},
+	// Metro Station
+	[DIALOG_METRO_WORKER]= {
+		.statement[0].words="Would you like to buy at SmartPass?",
+		.statement[0].next=1,
+		.statement[1].words="Sorry, all trains cancelled.  SmartTrip.",
+		.statement[0].next=0,
+	},
+	[DIALOG_TINY_CAPABARA]= {
+		.statement[0].words="GRONK",
+		.statement[0].next=0,
+	},
+	[DIALOG_GIANT_GUINEA_PIG]= {
+		.statement[0].words="SQUEAK?",
+		.statement[0].next=0,
+//		-> YES
+//		-> NO
+	},
+	[DIALOG_LARGE_BIRD]= {
+		.statement[0].words="WARK?",
+		.statement[0].next=0,
+//		-> YES
+//		-> NO
+	},
+	// FOUNTAIN
+	[DIALOG_MERMAID]= {
+		.statement[0].words="Did ye put bubbles in yon fountain?",
+		.statement[0].next=0,
+		// restore heatlh?
+	},
+	// BEL AIR
+	[DIALOG_CMW]= {
+		.statement[0].words="No admittance without black trenchcoat",
+		.statement[0].next=0,
+	},
+	[DIALOG_MALL]= {
+		.statement[0].words="...",
+		.statement[0].next=0,
+	},
+	[DIALOG_MINIGOLF]= {
+		.statement[0].words="Closed for the season",
+		.statement[0].next=0,
+	},
+	// VIDEO HOMEROOM
+	[DIALOG_SCARYNUN]= {
+		.statement[0].words="...",
+		.statement[0].next=0,
+	},
+	[DIALOG_GUS]= {
+		.statement[0].words="I found this...",
+		.statement[0].next=0,
+	},
+	[DIALOG_RAISTLIN]= {
+		.statement[0].words="This may aid you on your journey",
+		.statement[0].next=0,
+		// AMIGA?
+	},
+	[DIALOG_FORD]= {
+		.statement[0].words="557-0868 Utopia BBS is really great!",
+		.statement[0].next=0,
+		// AMIGA?
+	},
 #if 0
-DINING HALL
-	OSCAR
-		BEWARE THE KILLER CRABS
-		THEY WANT TO MEET YOU
-		THEY WANT TO EAT YOU
-	NICOLE
-	CINDY
-	ELAINE
-		REMEMBER OHMS LAW
-		TWINKLE TWINKLE LITTLE STAR
-		V IS EQUAL TO IR
-	CAFETERIA LADY
-		HAPPY BIRTHDAY
-
-METRO STATION
-	METRO WORKER
-		WOULD YOU LIKE TO BUY A SMARTPASS
-		SORRY ALL TRAINS CANCELLED: SMARTTRIP
-	TINY CAPABARA
-		GRONK
-	GIANT GUINEA PIG
-		SQUEAK?
-		-> YES
-		-> NO
-	LARGE BIRD
-		WARK?
-		-> YES
-		-> NO
-
-FOUNTAIN -- drink from it restore heatlh?
-	MERMAID
-		Did ye put bubbles in yon fountain?
-
-
-
-BEL AIR
-
-CMW
-	NO ADMITTANCE WITHOUT TRENCHCOAT
-
-MALL
-
-MINIGOLF
-	CLOSED FOR THE SEASON
-
-JC
-
-
-
-	VIDEO HOMEROOM
-		SISTER SCARYNUN
-		GUS	I FOUND THIS
-		RAISTLIN
-			THIS MAY AID YOU ON YOUR JOURNEY
-			AMIGA
-		FORD
-			557-0868 UTOPIA BBS IS REALLY GREAT
-
 	AP CALCULUS, TEAM I-1
 		PADRINO
 			I MET SOMEONE AT THE DOG SHOW
@@ -149,25 +211,42 @@ JC
 			*SWOON*
 
 		MEAN LADY
-
-	MATH OFFICE (ACADEMIC TEAM)
-		CAPTAIN STEVE
-			"REMEMBER REYERSON\'S RULE"
-			ANY GIVEN TEAM CAN BE BEATEN
-			ON ANY GIVEN DAY
-		BRIGHID
-		RACHAEL YRBK
-			AP BIO LAB PARTNERS
-			MUST STICK TOGETHER
-		MREE
-			"I\'M NOT EVIL"
-			NO ESTA AQUI
-
-	PATRIOT ROOM
+#endif
+	// MATH OFFICE (ACADEMIC TEAM)
+	[DIALOG_CAPTAIN_STEVE]= {
+		.statement[0].words="Remember Reyerson\'s Rule",
+		.statement[0].next=0,
+		.statement[1].words="Any given team can be beaten",
+		.statement[1].next=0,
+		.statement[2].words="on any given day.",
+		.statement[2].next=0,
+	},
+	[DIALOG_BRIGHID]= {
+		.statement[0].words="...",
+		.statement[0].next=0,
+	},
+	[DIALOG_RACHAEL]= {
+		.statement[0].words="AP Bio Lab Partners must stick together",
+		.statement[0].next=0,
+	},
+	[DIALOG_MREE]= {
+		.statement[0].words="I\'m not evil",
+		.statement[0].next=0,
+		.statement[1].words="No esta aqui",
+		.statement[1].next=0,
+	},
+#if 0
+	PATRIOT ROOM:
 		AGENT N
+		GRABOWSKI
+		FRESHMAN STEVE
+		FRESHMAN JENNY
+
+
+	OFFICE:
 		ACTING PRINCIPAL ROBOKNEE
 			SINCE WE HAVE NO ELECTRICITY WE HAVE NO LIGHTS
-
+		SUSIE: Squeak
 PUZZLE:
 
 	Need wire, 
@@ -190,6 +269,3 @@ Then RK attack
 #endif
 
 };
-
-
-
