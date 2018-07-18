1dc5a474acfd40fb0c8a24e4a35996e74fbcf184
Vince Weaver <vince@deater.net>
2018-07-18 17:13:33 -0400

    kfest: start drawing lines

--- kfest2018/raster.s
+++ kfest2018/raster.s
@@ -1,7 +1,17 @@
 ; Kansasfest HackFest Entry
 
+; Soft Switches
+
+SET_GR	= $C050 ; Enable graphics
+FULLGR	= $C052	; Full screen, no text
+LORES	= $C056	; Enable LORES graphics
+
+
+; ROM routines
+
 TEXT	= $FB36				;; Set text mode
 HOME	= $FC58				;; Clear the text screen
+
 
 
 	;===================
@@ -10,4 +20,26 @@ HOME	= $FC58				;; Clear the text screen
 	jsr	TEXT
 	jsr	HOME
 
+	; GR part
+	bit	LORES
+	bit	SET_GR
+	bit	FULLGR
 
+	lda     #$55
+	ldx	#39
+border_loop:
+	sta	$5d0,X		; 38
+
+	dex
+	bpl	border_loop
+
+
+loop_forever:
+	jmp	loop_forever
+
+
+;	H		E		L		L	O
+.byte	$D1,$00,$D1,$00, $D1,$01,$00, $D1,$00,$00, $D1,$00,$00, $D0,$01,$D0,$00
+.byte	$24,$04,$24,$00, $24,$20,$00, $24,$20,$00, $24,$20,$00, $04,$20,$04,$00
+.byte	$C9,$C0,$C9,$00, $C9,$C0,$00, $C9,$00,$00, $C9,$00,$00, $C9,$00,$C0,$00
+.byte	$06,$00,$06,$00, $06,$00,$00, $06,$00,$00, $06,$00,$00, $06,$00,$06,$00
