69a34222aa0212437905197cb7a2929a88f9a0d4
Vince Weaver <vincent.weaver@maine.edu>
2018-08-29 11:57:54 -0400

    megademo: load some graphics

--- megademo/megademo.s
+++ megademo/megademo.s
@@ -3,6 +3,19 @@
 ; by deater (Vince Weaver) <vince@deater.net>
 
 
+.include "hardware.inc"
+
+
+	;===================
+	; set graphics mode
+	;===================
+	jsr	HOME
+
+	bit	PAGE0                   ; first graphics page
+	bit	FULLGR			; full screen graphics
+	bit	HIRES			; hires mode !!!
+	bit	SET_GR			; graphics mode
+
 
 	;===================
 	; do nothing
@@ -11,3 +24,6 @@ do_nothing:
 	jmp	do_nothing
 
 
+.align $1000
+
+.incbin "c64.img"
