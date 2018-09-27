c6011796955ab616063606a7c026895a8ea0f91c
Vince Weaver <vince@deater.net>
2018-09-26 22:41:10 -0400

    credits: use generic move_letters code

--- split_screen/credits.s
+++ split_screen/credits.s
@@ -1,6 +1,7 @@
 ;
 ;	Cycle-counting text/hgr/lowres demo
 ;		by Vince Weaver
+;
 
 .include "zp.inc"
 
@@ -17,6 +18,7 @@
 	BLARGH	= $69
 	MBASE	= $97
 	MBOFFSET = $98
+	WAITING = $99
 
 	;===================
 	; init screen
@@ -96,20 +98,20 @@ no_init_mb:
 
 	; Wait
 
-	jsr	wait_until_keypressed
+;	jsr	wait_until_keypressed
 
 	; GR part
-	bit	LORES
-	bit	SET_GR
-	bit	FULLGR
+;	bit	LORES
+;	bit	SET_GR
+;	bit	FULLGR
 
-	jsr	draw_bottom_green				; 6
+;	jsr	draw_bottom_green				; 6
 
 	; Wait
 
 ;	jsr	wait_until_keypressed
 
-	bit	HIRES
+;	bit	HIRES
 
 
 	; Wait
@@ -561,22 +563,21 @@ loop6:
 
 
 
-	jsr	move_letters					; 6+110
+	jsr	move_letters					; 6+126
 
 	; Blanking time:	 4550
-	; move_letters		 -116
+	; move_letters		 -132
 	; JMP at end		   -3
-	;========================4431 cycles
+	;========================4415 cycles
 
-	; Try X=176 Y=5 cycles=4431
+	; Try X=24 Y=35 cycles=4411 R4
 
-;	lda	#0							; 2
-;	lda	#0							; 2
-	ldy	#5							; 2
-loop7:
-	ldx	#176							; 2
-loop8:
-	dex								; 2
+	nop
+	nop
+
+	ldy	#35							; 2
+loop7:	ldx	#24							; 2
+loop8:	dex								; 2
 	bne	loop8							; 2nt/3
 	dey								; 2
 	bne	loop7							; 2nt/3
@@ -763,133 +764,8 @@ green_loop:
 
 ; 4 + (40*55) + 6 - 1
 
-	;===============================================
-	; Move Letters
-	;===============================================
-	; Normal	=10+25+38+9 = 82     need 28  (28)
-	; End of line   =10+25+38+37 = 110
-	; Next line	=10+5+12+34 = 61     need 49  (28+21)
-	; done entirely =10+5+9 = 24         need 86  (28+21+37)
 
-	; all forced to be 109
-
-move_letters:
-	ldy	#0							; 2
-	lda	(LETTERL),Y						; 5
-	sta	LETTER							; 3
-								;==========
-								;       10
-	bmi	letter_special
-									; 2
-	lda	LETTERY							; 3
-	asl								; 2
-	tay								; 2
-	lda	gr_offsets,Y	; lookup low-res memory address		; 4
-	sta	BASL		; store out low byte of addy		; 3
-	lda	gr_offsets+1,Y	; look up high byte			; 4
-	sta	BASH							; 3
-	lda	#0		; cycle-killer				; 2
-								;==========
-								;       25
-
-	ldy	#0		; erase old char with space		; 2
-	lda	#' '|$80						; 2
-	ldy	LETTERX							; 3
-	sta	(BASL),Y						; 6
-
-	dey			; draw new char				; 2
-	sty	LETTERX							; 3
-	lda	LETTER							; 3
-	ora	#$80							; 2
-	ldy	LETTERX							; 3
-	sta	(BASL),Y						; 6
-
-	lda	LETTERX							; 3
-	cmp	LETTERD							; 3
-								;===========
-								;        38
-	beq	letter_next
-									; 2
-	lda	#0							; 2
-	lda	#0							; 2
-	jmp	waste_28						; 3
-								;==========
-								;	  9
-letter_next:
-									; 3
-	clc								; 2
-	lda	LETTERL							; 3
-	adc	#1							; 2
-	sta	LETTERL							; 3
-	lda	LETTERH							; 3
-	adc	#0							; 2
-	sta	LETTERH							; 3
-	inc	LETTERD							; 5
-	lda	#39							; 2
-	sta	LETTERX							; 3
-	rts								; 6
-								;===========
-								;        37
-
-letter_special:
-									; 3
-	cmp	#$ff							; 2
-								;==========
-								;         5
-
-	beq	letter_done
-									; 2
-	ldy	#1							; 2
-	lda	(LETTERL),Y						; 5
-	sta	LETTERY							; 3
-								;===========
-								;	 12
-
-	iny								; 2
-	lda	(LETTERL),Y						; 5
-	sta	LETTERD							; 3
-
-	clc								; 2
-	lda	LETTERL							; 3
-	adc	#3							; 2
-	sta	LETTERL							; 3
-	lda	LETTERH							; 3
-	adc	#0							; 2
-	sta	LETTERH							; 3
-	lda	LETTERH		; waste					; 3
-	jmp	waste_21						; 3
-
-								;===========
-								;        34
-letter_done:
-				; 3
-	lda	LETTERH		; 3
-	lda	LETTERH		; 3
-
-waste_37:
-	ldx	#0		; 2
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-waste_21:
-	ldx	#0		; 2
-	ldx	#0		; 2
-	ldx	#0		; 2
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-
-waste_28:
-	ldx	#0		; 2
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	inc	BLARGH		; 5
-	rts			; 6
+.align $100
 
 letters:
 	;.byte	1,15
@@ -911,16 +787,12 @@ letters:
 	.byte	3,16,"IS HARD!"
 	.byte	255
 
-
-
-
-
 .include "vapor_lock.s"
 .include "delay_a.s"
 .include "gr_hline.s"
-.include "../asm_routines/text_print.s"
+.include "move_letters.s"
 .include "mockingboard.s"
-.include "../asm_routines/keypress.s"
+;.include "../asm_routines/keypress.s"
 
 .align	$100
 .include "tfv_sprites.inc"
