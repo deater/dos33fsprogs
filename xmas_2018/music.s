b2d104b9fc434677b4f5beba596495179fe92732
Vince Weaver <vince@deater.net>
2018-12-14 18:42:39 -0500

    xmas2018: add missing file

---
+++ xmas_2018/music.s
@@ -0,0 +1,139 @@
+mah00:
+.incbin "music/mock.ah.00"
+mah01:
+.incbin "music/mock.ah.01"
+mah02:
+.incbin "music/mock.ah.02"
+mah03:
+.incbin "music/mock.ah.03"
+mah04:
+.incbin "music/mock.ah.04"
+mah05:
+.incbin "music/mock.ah.05"
+mah06:
+.incbin "music/mock.ah.06"
+mah07:
+.incbin "music/mock.ah.07"
+
+mal00:
+.incbin "music/mock.al.00"
+mal01:
+.incbin "music/mock.al.01"
+mal02:
+.incbin "music/mock.al.02"
+mal03:
+.incbin "music/mock.al.03"
+mal04:
+.incbin "music/mock.al.04"
+mal05:
+.incbin "music/mock.al.05"
+mal06:
+.incbin "music/mock.al.06"
+mal07:
+.incbin "music/mock.al.07"
+
+
+
+mbh00:
+.incbin "music/mock.bh.00"
+mbh01:
+.incbin "music/mock.bh.01"
+mbh02:
+.incbin "music/mock.bh.02"
+mbh03:
+.incbin "music/mock.bh.03"
+mbh04:
+.incbin "music/mock.bh.04"
+mbh05:
+.incbin "music/mock.bh.05"
+mbh06:
+.incbin "music/mock.bh.06"
+mbh07:
+.incbin "music/mock.bh.07"
+
+mbl00:
+.incbin "music/mock.bl.00"
+mbl01:
+.incbin "music/mock.bl.01"
+mbl02:
+.incbin "music/mock.bl.02"
+mbl03:
+.incbin "music/mock.bl.03"
+mbl04:
+.incbin "music/mock.bl.04"
+mbl05:
+.incbin "music/mock.bl.05"
+mbl06:
+.incbin "music/mock.bl.06"
+mbl07:
+.incbin "music/mock.bl.07"
+
+mch00:
+.incbin "music/mock.ch.00"
+mch01:
+.incbin "music/mock.ch.01"
+mch02:
+.incbin "music/mock.ch.02"
+mch03:
+.incbin "music/mock.ch.03"
+mch04:
+.incbin "music/mock.ch.04"
+mch05:
+.incbin "music/mock.ch.05"
+mch06:
+.incbin "music/mock.ch.06"
+mch07:
+.incbin "music/mock.ch.07"
+
+mcl00:
+.incbin "music/mock.cl.00"
+mcl01:
+.incbin "music/mock.cl.01"
+mcl02:
+.incbin "music/mock.cl.02"
+mcl03:
+.incbin "music/mock.cl.03"
+mcl04:
+.incbin "music/mock.cl.04"
+mcl05:
+.incbin "music/mock.cl.05"
+mcl06:
+.incbin "music/mock.cl.06"
+mcl07:
+.incbin "music/mock.cl.07"
+
+
+mnh00:
+.incbin "music/mock.nh.00"
+mnh01:
+.incbin "music/mock.nh.01"
+mnh02:
+.incbin "music/mock.nh.02"
+mnh03:
+.incbin "music/mock.nh.03"
+mnh04:
+.incbin "music/mock.nh.04"
+mnh05:
+.incbin "music/mock.nh.05"
+mnh06:
+.incbin "music/mock.nh.06"
+mnh07:
+.incbin "music/mock.nh.07"
+
+mnl00:
+.incbin "music/mock.nl.00"
+mnl01:
+.incbin "music/mock.nl.01"
+mnl02:
+.incbin "music/mock.nl.02"
+mnl03:
+.incbin "music/mock.nl.03"
+mnl04:
+.incbin "music/mock.nl.04"
+mnl05:
+.incbin "music/mock.nl.05"
+mnl06:
+.incbin "music/mock.nl.06"
+mnl07:
+.incbin "music/mock.nl.07"
+
