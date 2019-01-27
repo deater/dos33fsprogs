23701db51daa7cede2e84c5cde1cdc48b87b4139
Vince Weaver <vince@deater.net>
2019-01-26 21:52:00 -0500

    ootw: update intro

--- ootw/intro.s
+++ ootw/intro.s
@@ -1,10 +1,122 @@
 ;=====================================
 ; Intro
 
+.include "zp.inc"
+.include "hardware.inc"
+
 intro:
+
+	;===========================
+	; Enable graphics
+
+	bit	LORES
+	bit	SET_GR
+	bit	FULLGR
+
+	;===========================
+	; Setup pages (is this necessary?)
+
+	lda	#0
+	sta	DRAW_PAGE
+	lda	#1
+	sta	DISP_PAGE
+
+;===============================
+;===============================
+; Opening scene with car
+;===============================
+;===============================
+
+	;=============================
+	; Load background to $c00
+
+	lda	#$0c
+	sta	BASH
+	lda	#$00
+	sta	BASL			; load image off-screen $c00
+
+	lda	#>(building_rle)
+	sta	GBASH
+	lda	#<(building_rle)
+	sta	GBASL
+	jsr	load_rle_gr
+
+	;=================================
+	; copy $c00 to both pages $400/$800
+
+	jsr	gr_copy_to_current
+	jsr	page_flip
+	jsr	gr_copy_to_current
+
+building_loop:
+	lda	KEYPRESS
+	bpl	building_loop
+
+	;===============================
+	; Walk into door
+
+	;===============================
+	; Elevator going down
+
+	;===============================
+	; Getting out of Elevator
+
+	;===============================
+	; Keycode
+
+	;===============================
+	; Scanner
+
+	;===============================
+	; Spinny DNA / Key
+
+	;===============================
+	; Sitting at Desk
+
+	;===============================
+	; Peanut OS
+
+	;===============================
+	; Particle Accelerator Screen
+
+	;===============================
+	; Soda
+
+	;===============================
+	; More crazy screen
+
+	;===============================
+	; Thunderstorm Outside
+
+	;===============================
+	; Tunnel 1
+
+	;===============================
+	; Tunnel 2
+
+	;===============================
+	; Zappo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 	rts
 
+.include "gr_pageflip.s"
+.include "gr_unrle.s"
+.include "gr_copy.s"
+.include "gr_offsets.s"
 
 .include "intro_building.inc"
