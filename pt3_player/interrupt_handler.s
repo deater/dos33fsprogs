26e7d2f99001ffaaa809f3002bcee7422c125111
Peter Ferrie <peter.ferrie@gmail.com>
2019-06-07 23:23:29 -0400

    pt3_player: optimize mockingboard writes

--- pt3_player/interrupt_handler.s
+++ pt3_player/interrupt_handler.s
@@ -105,15 +105,14 @@ mb_not_13:
 	; address
 	stx	MOCK_6522_ORA1		; put address on PA1		; 4
 	stx	MOCK_6522_ORA2		; put address on PA2		; 4
-	lda	#MOCK_AY_LATCH_ADDR	; latch_address for PB1		; 2
-	sta	MOCK_6522_ORB1		; latch_address on PB1          ; 4
-	sta	MOCK_6522_ORB2		; latch_address on PB2		; 4
+	ldy	#MOCK_AY_LATCH_ADDR	; latch_address for PB1		; 2
+	sty	MOCK_6522_ORB1		; latch_address on PB1          ; 4
+	sty	MOCK_6522_ORB2		; latch_address on PB2		; 4
 	ldy	#MOCK_AY_INACTIVE	; go inactive			; 2
 	sty	MOCK_6522_ORB1						; 4
 	sty	MOCK_6522_ORB2						; 4
 
         ; value
-	lda	AY_REGISTERS,X		; load register value		; 4
 	sta	MOCK_6522_ORA1		; put value on PA1		; 4
 	sta	MOCK_6522_ORA2		; put value on PA2		; 4
 	lda	#MOCK_AY_WRITE		;				; 2
@@ -122,7 +121,7 @@ mb_not_13:
 	sty	MOCK_6522_ORB1						; 4
 	sty	MOCK_6522_ORB2						; 4
 								;===========
-								; 	60
+								; 	56
 mb_no_write:
 	inx				; point to next register	; 2
 	cpx	#14			; if 14 we're done		; 2
