a4cf65e28f07709af1d2ac6302da751558eaa2f7
Vince Weaver <vince@deater.net>
2019-07-25 09:16:23 -0400

    water: trying a new effect

--- gr-sim/water/water.c
+++ gr-sim/water/water.c
@@ -1,39 +1,148 @@
-// Based on Fluid_Sim
-// an IOCCC 2012 winner
-// by Yusuke Endoh
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-#define G 1	// gravity
-#define P 4	// pressure
-#define V 8	// viscosity
+#include "gr-sim.h"
+#include "tfv_utils.h"
+#include "tfv_zp.h"
 
-#include <stdio.h>//  .IOCCC                                         Fluid-  #
-#include <unistd.h>  //2012                                         _Sim!_  #
-#include <complex.h>  //||||                     ,____.              IOCCC-  #
-double complex	a[97687], *p, *q, *r = a, w = 0, d;
-int		x         , y;
-char		b         [6856] = "\x1b[2J" "\x1b" "[1;1H     ", *o = b, *t;
+#include "demo_title.c"
+#if 0
+static int fade_map[2][16]={
+//       0 1 2 3 4 5 6 7  8 9 a b c d e f
+	{0,1,2,2,4,5,2,5, 8,8,5,3,4,8,4,7},
+	{0,0,0,5,0,0,5,5, 0,0,0,5,8,5,5,5},
 
-int main(
-) {
+};
+#endif
 
-	for (; 0 < (x = getc(stdin));)
-		w = x > 10 ? 32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, w - I : (x = w + 2);;
-	for (;; puts(o), o = b + 4) {
-		for (p = a; p[2] = p[1] * 9, p < r; p += 5)
-			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
-				if (0 < (x = 1 - w))
-					p[2] += w * w;
-		for (p = a; p[3] = G, p < r; p += 5)
-			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
-				if (0 < (x = 1 - w))
-					p[3] += w * (d * (3 - p[2] - q[2]) * P + p[4] * V - q[4] * V) / p[2];
-		for (x = 011; 2012 - 1 > x++;)
-			b[x] = 0;
-		for (p = a; (t = b + 10 + (x = *p * I) + 80 * (y = *p / 2), *p += p[4] += p[3] / 10 * !p[1]), p < r; p += 5)
-			x = 0 <= x && x < 79 && 0 <= y && y < 23 ? 1[1[*t |= 8, t] |= 4, t += 80] = 1, *t |= 2 : 0;
-		for (x = 011; 2012 - 1 > x++;)
-			b[x] = " '`-.|//,\\" "|\\_" "\\/\x23\n"[x % 80 - 9 ? x[b] : 16];;
+static int fade_map[2][16]={
+//       0 1 2 3 4 5 6 7  8 9 a b c d e f
+	{0,0,0,2,0,0,2,5, 5,8,5,3,4,8,4,7},
+	{0,0,0,0,0,0,5,0, 0,0,0,0,8,0,0,5},
 
-		usleep(12321);
-	} return 0;
+};
+
+
+int main(int argc, char **argv) {
+
+	int x,temph,templ,ch;
+
+	grsim_init();
+	gr();
+
+//	clear_screens();
+
+	ram[DRAW_PAGE]=PAGE0;
+	clear_bottom();
+	ram[DRAW_PAGE]=PAGE1;
+	clear_bottom();
+	ram[DRAW_PAGE]=PAGE2;
+	clear_bottom();
+
+
+//	clear_bottom(PAGE0);
+//	clear_bottom(PAGE1);
+//	clear_bottom(PAGE2);
+
+//	grsim_unrle(demo_rle,0x400);
+	grsim_unrle(demo_rle,0xc00);
+
+	gr_copy_to_current(0xc00);
+	page_flip();
+	gr_copy_to_current(0xc00);
+	page_flip();
+
+	while(1) {
+		repeat_until_keypressed();
+
+		/* Fade step 1 */
+		for(x=0xc00;x<0x1000;x++) {
+			temph=ram[x]&0xf0;
+			templ=ram[x]&0x0f;
+
+			templ=fade_map[0][templ];
+			temph=fade_map[0][temph>>4];
+
+			ram[x-0x800]=(temph<<4)|templ;
+		}
+		grsim_update();
+
+		ch=repeat_until_keypressed();
+		if (ch=='q') break;
+
+		/* Fade step 2 */
+
+		for(x=0xc00;x<0x1000;x++) {
+			temph=ram[x]&0xf0;
+			templ=ram[x]&0x0f;
+
+			templ=fade_map[1][templ];
+			temph=fade_map[1][temph>>4];
+
+			ram[x-0x800]=(temph<<4)|templ;
+		}
+		grsim_update();
+
+		ch=repeat_until_keypressed();
+		if (ch=='q') break;
+
+		/* Fade to black */
+
+		for(x=0x400;x<0x800;x++) {
+			ram[x]=0x00;
+		}
+		grsim_update();
+
+		ch=repeat_until_keypressed();
+		if (ch=='q') break;
+
+
+		/* Unfade step 2 */
+		for(x=0xc00;x<0x1000;x++) {
+			temph=ram[x]&0xf0;
+			templ=ram[x]&0x0f;
+
+			templ=fade_map[1][templ];
+			temph=fade_map[1][temph>>4];
+
+			ram[x-0x800]=(temph<<4)|templ;
+		}
+		grsim_update();
+
+		ch=repeat_until_keypressed();
+		if (ch=='q') break;
+
+		/* Unfade step 1 */
+		for(x=0xc00;x<0x1000;x++) {
+			temph=ram[x]&0xf0;
+			templ=ram[x]&0x0f;
+
+			templ=fade_map[0][templ];
+			temph=fade_map[0][temph>>4];
+
+			ram[x-0x800]=(temph<<4)|templ;
+		}
+		grsim_update();
+
+		ch=repeat_until_keypressed();
+		if (ch=='q') break;
+
+		/* Total unfade */
+		for(x=0xc00;x<0x1000;x++) {
+			temph=ram[x]&0xf0;
+			templ=ram[x]&0x0f;
+
+			ram[x-0x800]=(temph)|templ;
+		}
+		grsim_update();
+
+		ch=repeat_until_keypressed();
+		if (ch=='q') break;
+
+	}
+
+	return 0;
 }
+
