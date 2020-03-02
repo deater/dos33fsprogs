6e668a3363be3bda45f1889ae2bd52f305b46890
Vince Weaver <vincent.weaver@maine.edu>
2020-03-02 15:46:04 -0500

    mist: can read letter now

--- mist/mist.s
+++ mist/mist.s
@@ -542,6 +542,94 @@ done_turning:
 
 	rts
 
+
+
+; Catherine,
+; I've left for you a message
+; of utmost importance in
+; our fore-chamber beside
+; the dock.  Enter the number
+; of Marker Switches on
+; this island into the imager
+; to retrieve the message.
+;             Yours,
+;                 Atrus
+
+letter:
+;        01234567890123456789
+.byte  9,1,"  CATHERINE,          ",0
+.byte  9,3,"  I THINK SOME WEIRD  ",0
+.byte  9,5,"  GUY IS OUT ROAMING  ",0
+.byte  9,7,"  AROUND OUR ISLAND!  ",0
+.byte  9,9,"  MAYBE HE CAN SOLVE  ",0
+.byte 9,11,"  ALL OF OUR DEEP     ",0
+.byte 9,13,"  FAMILY PROBLEMS     ",0
+.byte 9,15,"  WHILE I MESS        ",0
+.byte 9,17,"  WITH MY BOOKS.      ",0
+.byte 9,19,"         YOURS,       ",0
+.byte 9,21,"             ATRUS    ",0
+
+clear_line:
+.byte 9,0, "                      ",0
+
+	;================
+	; read the letter
+
+read_letter:
+;	jsr	TEXT
+;	jsr	HOME
+	bit	KEYRESET
+
+	bit	SET_TEXT
+
+	jsr	clear_all
+
+	; clear
+
+	ldx	#0
+clear_line_loop:
+	lda	#<clear_line
+	sta	OUTL
+	lda	#>clear_line
+	sta	OUTH
+
+
+	stx	clear_line+1
+	jsr	move_and_print
+	inx
+	cpx	#24
+	bne	clear_line_loop
+
+
+	lda	#<letter
+	sta	OUTL
+	lda	#>letter
+	sta	OUTH
+
+	ldx	#0
+letter_loop:
+	jsr	move_and_print
+
+	inx
+	cpx	#12
+	bne	letter_loop
+
+	jsr	page_flip
+
+wait_done_letter:
+	lda	KEYPRESS
+	bpl	wait_done_letter
+	bit	KEYRESET
+
+	; turn graphics back on
+
+	bit	SET_GR
+;	bit	PAGE0
+;	bit	FULLGR
+
+	rts
+
+
 	;==========================
 	; includes
 	;==========================
@@ -551,8 +639,10 @@ done_turning:
 	.include	"gr_offsets.s"
 	.include	"gr_pageflip.s"
 	.include	"gr_putsprite_crop.s"
+	.include	"text_print.s"
+	.include	"gr_fast_clear.s"
 
-	.include "audio.s"
+	.include	"audio.s"
 
 	.include	"mist_graphics.inc"
 
@@ -778,14 +868,14 @@ location7:
 	.byte	$ff		; south exit_dir
 	.byte	DIRECTION_E	; east exit_dir
 	.byte	DIRECTION_W	; west exit_dir
-	.byte	$ff		; special exit
+	.byte	$00		; special exit
 	.word	$0000		; north bg
 	.word	$0000		; south bg
 	.word	step_land2_e_rle	; east bg
 	.word	step_land2_w_rle	; west bg
-	.byte	$ff,$ff		; special x
-	.byte	$ff,$ff		; special y
-	.word	$0000		; special function
+	.byte	29,32		; special x
+	.byte	38,45		; special y
+	.word	read_letter-1
 	.byte	BG_WEST	| BG_EAST
 
 ; steps outside dentist chair branch
@@ -991,16 +1081,6 @@ location16:
 
 
 
-; Catherine,
-; I've left for you a message
-; of utmost importance in
-; our fore-chamber beside
-; the dock.  Enter the number
-; of Marker Switches on
-; this island into the imager
-; to retrieve the message.
-;             Yours,
-;                 Atrus
 
 
 
