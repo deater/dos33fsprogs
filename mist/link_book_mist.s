fc19c9e6f464036ee03d1c9af0de94e692ba0c66
Vince Weaver <vince@deater.net>
2020-03-11 09:47:48 -0400

    mist: temporarily disable sound

--- mist/link_book_mist.s
+++ mist/link_book_mist.s
@@ -4,14 +4,14 @@
 mist_link_book:
 
 	; play sound effect?
-
+.if 0
 	lda	#<audio_link_noise
 	sta	BTC_L
 	lda	#>audio_link_noise
 	sta	BTC_H
 	ldx	#43		; 45 pages long???
 	jsr	play_audio
-
+.endif
 	lda	#1
 	sta	LOCATION
 	jsr	change_location
