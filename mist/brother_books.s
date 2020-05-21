23c0d54397158c729b1aa656702398e5d86d45c3
Vince Weaver <vince@deater.net>
2020-05-21 02:24:21 -0400

    mist: update the blue book

--- mist/brother_books.s
+++ mist/brother_books.s
@@ -124,6 +124,23 @@ open_blue_book:
 
 red_book_animation:
 
+	; draw the text
+	lda	RED_PAGE_COUNT
+	asl
+	tay
+
+	lda	sirrus_dialog,Y
+	sta	OUTL
+	lda	sirrus_dialog+1,Y
+	sta	OUTH
+
+repeat_sirrus_string:
+	jsr	move_and_print
+
+	ldy	#0
+	lda	(OUTL),Y
+	bpl	repeat_sirrus_string
+
 	lda	ANIMATE_FRAME
 	asl
 	tay
@@ -141,6 +158,18 @@ advance_red_book:
 	sta	YPOS
 
 	jsr	put_sprite_crop
+
+	lda	FRAMEL
+	and	#$f
+	bne	done_animate_red_book
+
+	inc	ANIMATE_FRAME
+	lda	ANIMATE_FRAME
+	cmp     #4
+	bne	done_animate_red_book
+	lda	#1
+	sta	ANIMATE_FRAME
+done_animate_red_book:
 
 	; Play the "bring red pages" audio
 
@@ -166,6 +195,7 @@ red_book_sprite_sequence0:
 	.word red_book_static2_sprite
 	.word red_book_static1_sprite
 	.word red_book_static2_sprite
+	.word red_book_static1_sprite
 
 red_book_sprite_sequence1:
 	.word red_book_static1_sprite
@@ -205,6 +235,52 @@ red_book_static2_sprite:
 	.byte $A1,$Af,$A1,$A3,$A1,$A1,$A1,$A1,$A1
 
 
+blue_book_sprite_sequence0:
+	.word blue_book_static1_sprite
+	.word blue_book_static2_sprite
+	.word blue_book_static1_sprite
+	.word blue_book_static2_sprite
+	.word blue_book_static1_sprite
+
+blue_book_sprite_sequence1:
+	.word blue_book_static1_sprite
+	.word blue_book_static2_sprite
+	.word blue_book_face_sprite
+	.word blue_book_static2_sprite
+
+
+blue_book_face_sprite:
+	.byte 9,7
+	.byte $88,$b8,$8b,$bb,$8b,$b8,$88,$88,$26
+	.byte $88,$bb,$0b,$bb,$0b,$bb,$88,$88,$26
+	.byte $62,$bb,$bb,$33,$3b,$bb,$88,$88,$26
+	.byte $62,$8b,$b8,$b8,$b8,$8b,$88,$62,$26
+	.byte $62,$88,$bb,$b0,$b0,$83,$58,$52,$26
+	.byte $dd,$d8,$88,$88,$88,$d8,$dd,$55,$dd
+	.byte $Ad,$Ad,$A8,$A8,$A8,$Ad,$Ad,$A5,$Ad
+
+blue_book_static1_sprite:
+	.byte 9,7
+	.byte $22,$27,$22,$2f,$27,$22,$22,$26,$22
+	.byte $72,$26,$22,$27,$22,$26,$22,$27,$f2
+	.byte $62,$22,$72,$26,$22,$2f,$26,$72,$62
+	.byte $22,$2f,$22,$22,$26,$22,$27,$f2,$22
+	.byte $00,$00,$00,$00,$00,$00,$00,$00,$00
+	.byte $62,$27,$f2,$22,$f6,$22,$62,$27,$22
+	.byte $A2,$A2,$A6,$A2,$A2,$A7,$A2,$A2,$A6
+
+blue_book_static2_sprite:
+	.byte 9,7
+	.byte $02,$02,$02,$07,$02,$02,$02,$0f,$02
+	.byte $20,$70,$20,$20,$60,$20,$20,$70,$20
+	.byte $26,$22,$6f,$22,$2f,$72,$26,$22,$62
+	.byte $2f,$22,$22,$22,$26,$22,$27,$22,$f2
+	.byte $22,$27,$f2,$2f,$27,$62,$f2,$27,$22
+	.byte $26,$22,$27,$22,$22,$22,$72,$26,$72
+	.byte $A2,$Af,$A2,$A6,$A2,$A2,$A2,$A2,$A2
+
+
+
 
 ;==========================
 ; books dialog
@@ -222,6 +298,7 @@ sirrus_dialog:
 red_dialog0:
 	.byte 15,22
 	.byte "**STATIC**",0
+	.byte $80
 
 ; red 1
 ; who are you, bring me a red page, I can't see you
@@ -233,6 +310,7 @@ red_dialog1:
 	.byte "WHO ARE YOU? I CAN'T SEE YOU",0
 	.byte 0,22
 	.byte "BRING ME A RED PAGE. I AM SIRRUS.",0
+	.byte $80
 
 ; red 2
 ; You've returned.  Thank you for the red page.
@@ -247,6 +325,7 @@ red_dialog2:
 	.byte "I BEG YOU TO FIND REMAINING RED PAGES",0
 	.byte 0,23
 	.byte "DON'T WASTE TIME ON MY GUILTY BROTHER",0
+	.byte $80
 
 ; red 3
 ; Free me from my prison.  I am called Sirrus.
@@ -261,6 +340,7 @@ red_dialog3:
 	.byte "I NEED MORE PAGES, DON'T TOUCH BLUE ONES",0
 	.byte 0,23
 	.byte "DO NOT HELP MY WICKED BROTHER ACHENAR",0
+	.byte $80
 
 ; red 4
 ; With each page I can see you more clearly.
@@ -275,6 +355,7 @@ red_dialog4:
 	.byte "ACHENAR IS GUILTY OF CONQUEST",0
 	.byte 0,23
 	.byte "FREE ME AND I WILL REWARD YOU",0
+	.byte $80
 
 ; red 5
 ; You've finally returned.  You must think Achenar is guilty.
@@ -295,6 +376,7 @@ red_dialog5:
 	.byte "USE PAGE 158 IN PATTERN BOOK",0
 	.byte 0,24
 	.byte "DO NOT TOUCH THE GREEN BOOK",0
+	.byte $80
 
 ; ending
 ; I am free!  Thank you!  You've done the right thing.
@@ -309,6 +391,7 @@ red_dialog6:
 	.byte "LET ME RIP SOME PAGES OUT!",0
 	.byte 0,23
 	.byte "I HOPE YOU'RE INTO BOOKS!  GOODBYE!",0
+	.byte $80
 
 ;==========================
 ; blue/Achenar
@@ -330,6 +413,7 @@ blue_dialog1:
 	.byte "SIRRUS IS THAT YOU?  WHO ARE YOU?",0
 	.byte 0,22
 	.byte "BRING BLUE PAGES. MUST HAVE BLUE PAGES",0
+	.byte $80
 
 ; blue 2
 ; you've returned,  I'm Achenar.  don't listen to my brother.
@@ -346,6 +430,7 @@ blue_dialog2:
 	.byte "BRING BLUE PAGES, NOT RED",0
 	.byte 0,24
 	.byte "I WILL HAVE MY RETRIBUTION",0
+	.byte $80
 
 ; blue 3
 ; you've returned, good.  bring blue pages.
@@ -360,6 +445,7 @@ blue_dialog3:
 	.byte "GREEDY SIRRUS TRAPPED ME HERE",0
 	.byte 0,23
 	.byte "BRING BLUE PAGES, DON'T TOUCH THE RED",0
+	.byte $80
 
 ; blue 4
 ; my friend, I see you think Sirrus is guilty.
@@ -374,6 +460,7 @@ blue_dialog4:
 	.byte "HAVE YOU OBSERVED HIS LUST FOR RICHES",0
 	.byte 0,23
 	.byte "PLEASE BRING MORE BLUE PAGES",0
+	.byte $80
 
 ; blue 5
 ; Sirrus is guilty, lied to father and killed him
@@ -388,6 +475,7 @@ blue_dialog5:
 	.byte "FIND PATTERN 158 AND USE THE FIREPLACE",0
 	.byte 0,23
 	.byte "DON'T TOUCH THE GREEN BOOK!",0
+	.byte $80
 
 ; after
 ; Haha I am free!  I feel so alive!
@@ -403,3 +491,4 @@ blue_dialog6:
 	.byte "WHAT HAPPENS IF I RIP THESE PAGES OUT?",0
 	.byte 0,23
 	.byte "MAYBE SOMEONE WILL RESCUE YOU",0
+	.byte $80
