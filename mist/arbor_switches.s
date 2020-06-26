b66ae3b95c5dcf0302bd612ba466749546aa002e
Vince Weaver <vince@deater.net>
2020-06-26 01:34:32 -0400

    mist: arbor: can take elevator to the top

--- mist/arbor_switches.s
+++ mist/arbor_switches.s
@@ -51,8 +51,81 @@ hut_handle:
 	jmp	change_location
 
 
-	;===============================================
-	; update all backgrounds based on switch states
+
+;=============================
+;=============================
+; elevator2 handle pulled
+;=============================
+;=============================
+
+
+; FIXME: check for water power
+; FIXME: animate
+elevator2_handle:
+
+	; click speaker
+	bit	SPEAKER
+
+	; check for water power
+
+	lda	LOCATION
+	cmp	#ARBOR_INSIDE_ELEV2_CLOSED
+	beq	elev2_goto_top
+
+elev2_goto_bottom:
+	lda	#ARBOR_INSIDE_ELEV2_CLOSED
+	bne	done_elev2_handle
+
+elev2_goto_top:
+	lda	#ARBOR_IN_ELEV2_TOP_CLOSED
+
+done_elev2_handle:
+	sta	LOCATION
+	jmp	change_location
+
+
+;=========================
+;=========================
+; close elevator2 door
+;=========================
+;=========================
+
+elevator2_close_door:
+
+	lda	LOCATION
+	cmp	#ARBOR_INSIDE_ELEV2_OPEN
+	bne	elev2_close_top
+
+elev2_close_bottom:
+	lda	#ARBOR_INSIDE_ELEV2_CLOSED
+	bne	done_elev2_close_door
+
+elev2_close_top:
+	lda	#ARBOR_IN_ELEV2_TOP_CLOSED
+
+done_elev2_close_door:
+	sta	LOCATION
+	jmp	change_location
+
+
+
+;=========================
+;=========================
+; handle_top_gate
+;=========================
+;=========================
+
+handle_top_gate:
+
+
+	rts
+
+
+;===============================================
+;===============================================
+; update all backgrounds based on switch states
+;===============================================
+;===============================================
 
 update_arbor_state:
 
