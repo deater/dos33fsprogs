2860397c80b7e2223da73d715d5fa8cda33d80ee
Vince Weaver <vince@deater.net>
2020-07-08 15:26:51 -0400

    mist: can now get to all of the pillars

--- mist/mist_puzzles.s
+++ mist/mist_puzzles.s
@@ -1,3 +1,94 @@
+
+touch_pillar_eye:
+touch_pillar_snake:
+touch_pillar_bug:
+touch_pillar_anchor:
+touch_pillar_arrow:
+touch_pillar_leaf:
+touch_pillar_cross:
+touch_pillar_emu:
+	rts
+
+tree2_pillars:
+	lda	DIRECTION
+	cmp	#DIRECTION_E
+	beq	tree2_east
+	bne	tree2_west
+
+tree2_west:
+	lda	CURSOR_X
+	cmp	#22
+	bcs	goto_bug_pillar
+	bcc	goto_anchor_pillar
+
+tree2_east:
+	lda	CURSOR_X
+	cmp	#18
+	bcs	goto_arrow_pillar
+	bcc	goto_leaf_pillar
+
+pool_pillars:
+	lda	DIRECTION
+	cmp	#DIRECTION_E
+	beq	pool_east
+	bne	pool_west
+
+pool_west:
+	lda	CURSOR_X
+	cmp	#28
+	bcs	goto_eye_pillar
+	cmp	#12
+	bcs	goto_snake_pillar
+	bcc	goto_bug_pillar
+
+pool_east:
+	lda	CURSOR_X
+	cmp	#28
+	bcs	goto_leaf_pillar
+	cmp	#11
+	bcs	goto_cross_pillar
+	bcc	goto_emu_pillar
+
+
+goto_eye_pillar:
+	lda	#MIST_PILLAR_EYE
+	jmp	done_pillar
+
+goto_snake_pillar:
+	lda	#MIST_PILLAR_SNAKE
+	jmp	done_pillar
+
+goto_bug_pillar:
+	lda	#MIST_PILLAR_BUG
+	jmp	done_pillar
+
+goto_anchor_pillar:
+	lda	#MIST_PILLAR_ANCHOR
+	jmp	done_pillar
+
+
+goto_emu_pillar:
+	lda	#MIST_PILLAR_EMU
+	jmp	done_pillar
+
+goto_cross_pillar:
+	lda	#MIST_PILLAR_CROSS
+	jmp	done_pillar
+
+goto_leaf_pillar:
+	lda	#MIST_PILLAR_LEAF
+	jmp	done_pillar
+
+goto_arrow_pillar:
+	lda	#MIST_PILLAR_ARROW
+	jmp	done_pillar
+
+done_pillar:
+	sta	LOCATION
+	jmp	change_location
+
+
+
 ;=======================
 ; flip circuit breaker
 
