a4b3c54cafd5af5b1dc07ee23a0c01bdd8113c03
Vince Weaver <vince@deater.net>
2020-07-23 10:58:22 -0400

    mist: improved IIgs support
    thanks to 4am, this turns black backgrounds on and enables the
    PAGE2 workaround

--- mist/mist_title.s
+++ mist/mist_title.s
@@ -26,6 +26,41 @@ mist_start:
 	bit	FULLGR
 
 	;===================
+	; machine workarounds
+	;===================
+	; mostly IIgs
+	;===================
+	; thanks to 4am who provided this code from Total Replay
+
+	lda	ROM_MACHINEID
+	cmp	#$06
+	bne	not_a_iigs
+	sec
+	jsr	$FE1F			; check for IIgs
+	bcs	not_a_iigs
+
+	; gr/text page2 handling broken on early IIgs models
+	; this enables the workaround
+
+	jsr	ROM_TEXT2COPY		; set alternate display mode on IIgs
+	cli				; enable VBL interrupts
+
+	; also set background color to black instead of blue
+	lda	NEWVIDEO
+	and	#%00011111	; bit 7 = 0 -> IIgs Apple II-compat video modes
+				; bit 6 = 0 -> IIgs 128K memory map same as IIe
+				; bit 5 = 0 -> IIgs DHGR is color, not mono
+				; bits 0-4 unchanged
+	sta   NEWVIDEO
+	lda   #$F0
+	sta   TBCOLOR			; white text on black background
+	lda   #$00
+	sta   CLOCKCTL			; black border
+	sta   CLOCKCTL			; set twice for VidHD
+
+not_a_iigs:
+
+	;===================
 	; setup location
 	;===================
 
