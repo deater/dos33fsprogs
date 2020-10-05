22881013b2c786f7851dea1ce5bd378bc53ba11b
Vince Weaver <vince@deater.net>
2020-10-04 23:50:13 -0400

    twoliners: got autumn fitting

--- two-liners/convert_qkumba.c
+++ two-liners/convert_qkumba.c
@@ -1,14 +1,26 @@
-i = 0;
-e = 0;
+/* code by qkumba */
 
-do
-{
-enc[e++] = ((in[i + 2] & 3) << 4) + ((in[i + 1] & 3) << 2) + (in[i + 0] & 3) + 32;
-in[i + 0] = (in[i + 0] >> 2) + 32;
-in[i + 1] = (in[i + 1] >> 2) + 32;
-in[i + 2] = (in[i + 2] >> 2) + 32;
-write(o, in + i, 3);
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+
+	int i = 0;
+	int e = 0,filesize;
+	unsigned char in[1024];
+	unsigned char enc[1024];
+
+	filesize=read(0,in,1024);
+	do {
+		enc[e++] = ((in[i + 2] & 3) << 4) +
+			((in[i + 1] & 3) << 2) + (in[i + 0] & 3) + 32;
+		in[i + 0] = (in[i + 0] >> 2) + 32;
+		in[i + 1] = (in[i + 1] >> 2) + 32;
+		in[i + 2] = (in[i + 2] >> 2) + 32;
+		printf("%c%c%c",in[i],in[i+1],in[i+2]);//write(o, in + i, 3);
+	} while ((i += 3) < filesize);
+	enc[e]=0;
+	printf("%s\n",enc);
+
+	return 0;
 }
-while ((i += 3) < filesize);
-write(o, enc, sizeof(enc));
-
