19c1c1534c2178ba5ccf965fcc92055dc758b84c
Vince Weaver <vince@deater.net>
2020-10-12 00:42:40 -0400

    monkey: hookup voodoo room

--- monkey/monkey_voodoo1.s
+++ monkey/monkey_voodoo1.s
@@ -64,25 +64,374 @@ done_v1_adjust:
 	rts
 
 
-
-
-;draw_house:
-
-;	lda	#<wall_sprite
-;	sta	INL
-;	lda	#>wall_sprite
-;	sta	INH
-
-;	lda	#18
-;	sta	XPOS
-;	lda	#22
-;	sta	YPOS
-
-;	jsr	put_sprite_crop
-
-;	rts
-
-;house_sprite:
-
+	;==========================
+	;==========================
+	; voodoo1 check bounds
+	;==========================
+	;==========================
 voodoo1_check_bounds:
 	rts
+
+	;==========================
+	;==========================
+	; voodoo1 actions
+	;==========================
+	;==========================
+
+
+voodoo1_door_action:
+voodoo1_door_nothing:
+        lda     #VERB_WALK
+        sta     CURRENT_VERB
+        rts
+
+
+       ;=============================
+couch_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	couch_actions,Y
+	cmp	#$ff
+	beq	couch_nothing
+
+	sta	MESSAGE_L
+	lda	couch_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+couch_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+couch_actions:
+	.word   $FFFF		; give
+	.word   creepy_voodoo	; open
+	.word   creepy_voodoo	; close
+	.word   creepy_voodoo	; pick_up
+	.word   couch_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   couch_use	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+couch_look:	.byte 0,21,"LOOKS COMFORTABLE IN A CREEPY SORTA WAY",0
+couch_use:	.byte 1,21,"I CAN'T FALL ASLEEP IN STRANCE PLACES",0
+creepy_voodoo:	.byte 0,21,"I'D RATHER NOT TOUCH CREEPY VOODOO STUFF",0
+
+
+       ;=============================
+chickens_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	chickens_actions,Y
+	cmp	#$ff
+	beq	chickens_nothing
+
+	sta	MESSAGE_L
+	lda	chickens_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+chickens_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+chickens_actions:
+	.word   $FFFF		; give
+	.word   creepy_voodoo	; open
+	.word   creepy_voodoo	; close
+	.word   chickens_pickup	; pick_up
+	.word   chickens_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   creepy_voodoo	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+chickens_pickup:	.byte 14,21,"I BETTER NOT",0
+chickens_look:		.byte 12,21,"POOR CHICKENS...",0
+
+
+       ;=============================
+statue_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	statue_actions,Y
+	cmp	#$ff
+	beq	statue_nothing
+
+	sta	MESSAGE_L
+	lda	statue_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+statue_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+statue_actions:
+	.word   $FFFF		; give
+	.word   creepy_voodoo	; open
+	.word   creepy_voodoo	; close
+	.word   creepy_voodoo	; pick_up
+	.word   creepy_voodoo	; look_at
+	.word   $FFFF		; talk_to
+	.word   creepy_voodoo	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+
+       ;=============================
+voodoo_knicknacks_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	voodoo_knicknacks_actions,Y
+	cmp	#$ff
+	beq	voodoo_knicknacks_nothing
+
+	sta	MESSAGE_L
+	lda	voodoo_knicknacks_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+voodoo_knicknacks_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+voodoo_knicknacks_actions:
+	.word   $FFFF		; give
+	.word   creepy_voodoo	; open
+	.word   creepy_voodoo	; close
+	.word   creepy_voodoo	; pick_up
+	.word   voodoo_knicknacks_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   creepy_voodoo	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+voodoo_knicknacks_look:	.byte 3,21,"THERE'S A BAG OF BAT DRIPPINGS",0
+
+       ;=============================
+baskets_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	baskets_actions,Y
+	cmp	#$ff
+	beq	baskets_nothing
+
+	sta	MESSAGE_L
+	lda	baskets_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+baskets_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+baskets_actions:
+	.word   $FFFF		; give
+	.word   baskets_open	; open
+	.word   creepy_voodoo	; close
+	.word   creepy_voodoo	; pick_up
+	.word   baskets_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   creepy_voodoo	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+baskets_open:	.byte 10,21,"I'M NOT THAT CURIOUS",0
+baskets_look:	.byte 1,21,"GEE, I WONDER WHAT'S IN THESE BASKETS",0
+
+
+       ;=============================
+basket_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	basket_actions,Y
+	cmp	#$ff
+	beq	basket_nothing
+
+	sta	MESSAGE_L
+	lda	basket_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+basket_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+basket_actions:
+	.word   $FFFF		; give
+	.word   basket_open	; open
+	.word   creepy_voodoo	; close
+	.word   creepy_voodoo	; pick_up
+	.word   basket_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   creepy_voodoo	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+basket_open:	.byte 4,21,"SOMETHING WOULD PROBABLY JUMP OUT",0
+basket_look:	.byte 0,21,"HMMM.. I THINK I HEAR SLITHERING INSIDE",0
+
+
+       ;=============================
+bones_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	bones_actions,Y
+	cmp	#$ff
+	beq	bones_nothing
+
+	sta	MESSAGE_L
+	lda	bones_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+bones_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+bones_actions:
+	.word   $FFFF		; give
+	.word   creepy_voodoo	; open
+	.word   creepy_voodoo	; close
+	.word   creepy_voodoo	; pick_up
+	.word   bones_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   creepy_voodoo	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+bones_look:	.byte 3,21,"POOR LITTLE THING, WHATEVER IT WAS",0
+
+       ;=============================
+chalice_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	chalice_actions,Y
+	cmp	#$ff
+	beq	chalice_nothing
+
+	sta	MESSAGE_L
+	lda	chalice_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+chalice_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+chalice_actions:
+	.word   $FFFF		; give
+	.word   creepy_voodoo	; open
+	.word   creepy_voodoo	; close
+	.word   creepy_voodoo	; pick_up
+	.word   chalice_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   creepy_voodoo	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+chalice_look:	.byte 3,21,"NOW THIS IS THE CUP OF A CARPENTER",0
+
+       ;=============================
+trunk_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	trunk_actions,Y
+	cmp	#$ff
+	beq	trunk_nothing
+
+	sta	MESSAGE_L
+	lda	trunk_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+trunk_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+trunk_actions:
+	.word   $FFFF		; give
+	.word   creepy_voodoo	; open
+	.word   creepy_voodoo	; close
+	.word   creepy_voodoo	; pick_up
+	.word   trunk_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   creepy_voodoo	; use
+	.word   creepy_voodoo	; push
+	.word   creepy_voodoo	; pull
+
+trunk_look:	.byte 8,21,"PROBABLY HAS A BODY IN IT",0
+
+       ;=============================
+chicken_pulley_action:
+	lda	CURRENT_VERB
+	asl
+	tay
+
+	lda	chicken_pulley_actions,Y
+	cmp	#$ff
+	beq	chicken_pulley_nothing
+
+	sta	MESSAGE_L
+	lda	chicken_pulley_actions+1,Y
+	sta	MESSAGE_H
+
+	jmp	do_display_message
+
+chicken_pulley_nothing:
+	lda	#VERB_WALK
+	sta	CURRENT_VERB
+	rts
+
+chicken_pulley_actions:
+	.word   $FFFF		; give
+	.word   doesnt_open	; open
+	.word   doesnt_work	; close
+	.word   $FFFF		; pick_up
+	.word   chicken_pulley_look	; look_at
+	.word   $FFFF		; talk_to
+	.word   $FFFF		; use
+	.word   icant_move	; push
+	.word   icant_move	; pull
+
+chicken_pulley_look:	.byte 2,21,"A RUBBER CHICKEN WITH A PULLEY IN IT",0
+
+
