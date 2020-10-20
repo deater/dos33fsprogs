8fb76846a024bbdb939a58e5eb676aec76a7566d
Vince Weaver <vince@deater.net>
2020-10-20 02:01:54 -0400

    appleiibot: add bitmap example

---
+++ appleiibot/fouram.bas
@@ -0,0 +1,5 @@
+1REM_'> $^__XW?#__'\][C__!>W\Y_/&W:?\_SC=W/^?^1_]G_G_P=_Y_8><W?\/+, \/_S4&__S_T+C__\/]/P_/_3\_XYS_,X?@\\/K@/N$_CK"&OP_YO*B?^?^_*PA_'__;XX_#F9&$__1F9.X_?8F9&^_/8F9P_
+2HOME:FORY=0TO158:Z=PEEK(2054+Y)-32:FORI=0TO5:NORMAL:Q=INT(Z/2):IFQ*2<>ZTHENINVERSE
+3Z=Q:PRINT" ";:NEXT I,Y:GETA
+
+
---
+++ gr-utils/png2sixbitmap.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "loadpng.h"
+
+/* Converts a PNG to a shifted 6 bitmap */
+
+int main(int argc, char **argv) {
+
+	int row=0;
+	int col=0;
+
+	unsigned char ch;
+
+	unsigned char *image;
+	int xsize,ysize;
+
+	if (argc<2) {
+		fprintf(stderr,"Usage:\t%s INFILE\n\n",argv[0]);
+		exit(-1);
+	}
+
+	if (loadpng(argv[1],&image,&xsize,&ysize,PNG_WHOLETHING)<0) {
+		fprintf(stderr,"Error loading png!\n");
+		exit(-1);
+	}
+
+	fprintf(stderr,"Loaded image %d by %d\n",xsize,ysize);
+
+	ch=0;
+	for(row=0;row<24;row++) {
+		for(col=0;col<40;col++) {
+			if (image[row*xsize+col]) {
+				ch|=1<<5;
+			}
+			if ((row*xsize+col)%6==5) {
+				printf("%c",ch+32);
+				ch=0;
+			}
+			else {
+				ch>>=1;
+			}
+		}
+	}
+
+	printf("\n");
+
+	return 0;
+}
