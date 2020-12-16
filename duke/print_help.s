a149ade12128032aeda2489ef484ac46a13e2bdb
Vince Weaver <vince@deater.net>
2020-12-16 13:21:37 -0500

    duke: update help

--- duke/print_help.s
+++ duke/print_help.s
@@ -32,7 +32,9 @@ help_text:
 .byte 3, 6,   "A OR <-      : MOVE LEFT",0
 .byte 3, 7,   "D OR ->      : MOVE RIGHT",0
 .byte 3, 8,   "W OR UP      : ACTION, ENTER DOOR",0
-.byte 3,10,   "SPACEBAR     : JUMP",0
-.byte 3,11,   "RETURN       : SHOOT LASER",0
+.byte 3, 9,   "SPACEBAR     : JUMP",0
+.byte 3,10,   "RETURN       : SHOOT LASER",0
 .byte 3,12,   "ESC          : QUITS",0
+.byte 3,13,   "CONTROL-T    : TOGGLE SOUND",0
+.byte 3,14,   "J            : ENABLE JOYSTICK",0
 .byte 255
