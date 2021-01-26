a9219f0d4175fa9cb76f1aa4a412ede199406d42
Vince Weaver <vince@deater.net>
2021-01-25 20:37:13 -0500

    c64: get it small enough

--- graphics/hgr/c64/c64.s
+++ graphics/hgr/c64/c64.s
@@ -1,6 +1,17 @@
 ; silly c64
 
-; needs to be 146 bytes or less
+; 147 initial, top line
+; 145 beq instead of jmp, 0 is already in Y
+; 141 inline draw
+; 146 add READY
+; 136 optimize addr calculation
+; 150 printing READY and skipping line
+; 147 get FF by decrementing 0
+; 146 move increment before the draw
+; 144 have XPOS in A at beginning of loop
+; 142 realize shapes don't have to be aligned at 5
+
+; needs to be 144 bytes or less
 
 ; zero page locations
 WHICH		=	$00
@@ -50,50 +61,26 @@ dsr_demo:
 	lda	#$D5			; color blue
 	jsr	HCLR_COLOR
 
+	sty	WHICH		; = 0
+
+	dey			; $ff = white2
+	sty	HGR_COLOR
+
 	lda	#2
 	sta	HGR_SCALE
-
-	lda	#0
-	sta	WHICH
 
 	;=========================
 	; draw letters
 	;=========================
 
-
-	; LDA COLORTBL,X    GET COLOR PATTERN
-	lda	#$ff
-	sta	HGR_COLOR
-
-
 	lda	#8
 	sta	YPOS
 	lda	#23
-	sta	XPOS
+;	sta	XPOS
+
+	; XPOS in A coming in
 
 first_loop:
-	lda	WHICH
-	tay
-	lda	string,Y
-	clc
-	adc	#<shape_table
-	sta	shape_smc+1
-	lda	#>shape_table
-	sta	shape2_smc+1
-
-	jsr	draw
-
-	inc	WHICH
-
-	lda	XPOS
-	clc
-	adc	#8
-	sta	XPOS
-	cmp	#(28*8)+23
-	bne	first_loop
-
-end:
-	jmp	end
 
 
 draw:
@@ -101,25 +88,52 @@ draw:
 	; need to have co-ords set up with HPOSN
 	; rotate should be in A
 
+;	lda	XPOS
+	clc
+	adc	#8
+	sta	XPOS
+	tax
 
 	; setup X and Y co-ords
 
 	ldy	#0		; XPOSH always 0 for us
-	ldx	XPOS
+;	ldx	XPOS
 	lda	YPOS
 	jsr	HPOSN		; X= (y,x) Y=(a)
 
-shape_smc:
-	ldx	#<shape_table	; point to our shape
-shape2_smc:
-	ldy	#>shape_table	; code fits in one page so this doesn't change
+	ldy	WHICH
+	lda	string,Y
+	clc
+	adc	#<shape_table
+	tax
 
-rot_smc:
+;	ldx	#<shape_table	; point to our shape (low)
+	ldy	#>shape_table	; code fits in one page, same always
+
 	lda	#0		; set rotation
 
-	jmp	DRAW0		; XDRAW 1 AT X,Y
+	jsr	DRAW0		; XDRAW 1 AT X,Y
 				; Both A and X are 0 at exit
 
+
+	inc	WHICH
+	ldy	WHICH
+
+	lda	XPOS
+
+	cpy	#28
+	bne	no_adjust
+
+	sty	YPOS	; want 32, 28 is close
+	lda	#$ff
+	sta	XPOS
+
+no_adjust:
+	cpy	#33
+	bne	first_loop
+
+end:
+	beq	end
 
 
 shape_table:
@@ -130,7 +144,7 @@ shape_table:
 .byte	$3b,$24,$ad,$06,$00	; D		3
 .byte	$1f,$24,$35,$07,$00	; R		4
 .byte	$3f,$2c,$27,$2d,$00	; E		5
-.byte	$23,$24,$00,$00,$00	; I		6
+.byte	$23,$24,$00		; I		6
 .byte	$02,$00			; space		7
 
 ;.byte	$3b,$24,$ad,$06,$00	; 4		8
@@ -146,13 +160,14 @@ shape_table:
 
 string:
 ;       *  *  *  *     C  O  M  M  O  D  O  R  E
-.byte	10,10,10,10,35,0, 5,10,10, 5,15, 5,20,25,35
+.byte	10,10,10,10,33,0, 5,10,10, 5,15, 5,20,25,33
 ;	6  4     B  A  S  I  C   *  *  *  *
-.byte 	15,10,35,15,10,15,30,0, 35,10,10,10,10
+.byte 	15,10,33,15,10,15,30,0, 33,10,10,10,10
 
-;$3c,$2c,$3c,$3f,$36,$06,$00
-;$3f,$27,$2c,$27,$2d,$05,$00
+; 28
 
+;        R  E  A  D  Y
+.byte	20,25,10,15,30
 
-
+; 5
 
