ac2fc918f41e830c0b7887729ae70ca7b094e15f
Vince Weaver <vince@deater.net>
2021-01-31 01:25:34 -0500

    sierpinski: optimize

--- graphics/gr/sier/sier.s
+++ graphics/gr/sier/sier.s
@@ -2,8 +2,21 @@
 
 ; just plot X AND Y
 
-.include "zp.inc"
+;.include "zp.inc"
 .include "hardware.inc"
+
+GBASH	=	$27
+MASK	=	$2E
+COLOR	=	$30
+;XX	=	$F7
+YY	=	$F8
+T	=	$F9
+FACTOR1	=	$FA
+FACTOR2	=	$FB
+XX_T	=	$FC
+YY_T	=	$FD
+SAVED	=	$FE
+SAVED2	=	$FF
 
 	;================================
 	; Clear screen and setup graphics
@@ -13,44 +26,125 @@ sier:
 	jsr	SETGR		; set lo-res 40x40 mode
 	bit	FULLGR		; make it 40x48
 
+sier_outer:
 
-	lda	#39*2
-	sta	XX
+	lda	#47
+	sta	YY
+
+sier_yloop:
+
+	ldy	#39
+;	sta	XX
+
+	; calc YY_T
+
+	lda	YY
+	sta	FACTOR1		; T already in FACTOR2
+
+	lsr
+	bcc	even_mask
+	ldx	#$f0
+	bne	set_mask
+even_mask:
+	ldx	#$0f
+set_mask:
+	stx	MASK
+
+	jsr	GBASCALC	; take Y-coord/2 in A, put address in GBASL/H ( a trashed, C clear)
+
+	jsr	multiply	; finally finish mul from earlier
+	sta	YY_T
+
+	lda	GBASH
+draw_page_smc:
+	adc	#0
+	sta	GBASH
+
+
+
+
 
 sier_xloop:
 
-	lda	#47*2
-	sta	YY
-sier_yloop:
+	; want (YY-(XX*T)) & (XX+(YY*T)
+
+	clc
+	tya		; XX
+	adc	YY_T
+;	clc
+;	adc	XX
+	sta	SAVED
+
+	; calc XX_T
+
+	sty	FACTOR1		; XX, T already in FACTOR2
+	jsr	multiply
+	sta	XX_T
 
 	lda	YY
-	and	XX
-	and	#$FE
+	sec
+	sbc	XX_T
+
+	and	SAVED
+
+;	and	#$ff
 	beq	red
 
 black:
-	lda	#00
-	beq	do_color
-
+	lda	#00	; black
+	.byte	$2C	; bit trick
 red:
-	lda	#$11
-do_color:
+	lda	#$11	; red
+
 	sta	COLOR
 
-	lda	XX
-	lsr
-	tay
+;	ldy	XX
 
-	lda	YY
-	lsr
+	jsr	PLOT1		; PLOT AT (GBASL),Y
 
-	jsr	PLOT		; PLOT AT Y,A
+	dey
+	bpl	sier_xloop
 
 	dec	YY
-	bne	sier_yloop
+	bpl	sier_yloop
 
-	dec	XX
-	bne	sier_xloop
+	inc	mul_smc+1
+	inc	mul_smc+1
+	inc	mul_smc+1
+	inc	mul_smc+1
 
-done:
-	jmp	done
+flip_pages:
+	; X already 0
+
+	lda	draw_page_smc+1 ; DRAW_PAGE
+	beq	done_page
+	inx
+done_page:
+	ldy	PAGE0,X         ; set display page to PAGE1 or PAGE2
+
+	eor	#$4             ; flip draw page between $400/$800
+	sta	draw_page_smc+1 ; DRAW_PAGE
+
+	jmp	sier_outer
+
+
+	 ; factors in FACTOR1 and FACTOR2
+multiply:
+	lda	#0
+	ldx	#8
+	lsr	FACTOR1
+mul_loop:
+	bcc	mul_no_add
+	clc
+mul_smc:
+	adc	#$0		; T
+mul_no_add:
+	ror
+	ror	FACTOR1
+	dex
+	bne	mul_loop
+
+	; done, high result in A, low result in FACTOR1
+	; FACTOR2 preserved
+
+	rts
