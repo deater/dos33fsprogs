cc7339d7341c0a2826c7126cd8f0bfee022334fa
Vince Weaver <vince@deater.net>
2021-05-22 00:21:36 -0400

    hgr: entropytext: getting smaller

--- graphics/hgr/xdraw/entropy_textbot.s
+++ graphics/hgr/xdraw/entropy_textbot.s
@@ -37,7 +37,6 @@ DISP_PAGE       = $ED
 DRAW_PAGE       = $EE
 
 XHIGH           = $F1   ; credits
-LOGO_OFFSET     = $F2   ; credits
 
 
 HGR             = $F3E2
@@ -55,12 +54,13 @@ entropy_text:
 				; Y=0, A=0 after this call
 
 
-	sta	LOGO_OFFSET
+;	sta	LOGO_OFFSET
 	sta	FRAME
-
+	sta	XHIGH
 
 	iny			; default is 1
 	sty	HGR_SCALE
+
 
 	ldy	#60		; FOR Y=60 to 102 STEP 6
 logo_yloop:
@@ -68,29 +68,14 @@ logo_yloop:
 	ldx	#34		; FOR X=32 to 248 STEP 6
 logo_xloop:
 
-	stx	XREG		; save X
-	sty	YREG		; save Y
 
-	; setup X and Y co-ords
-	tya			; YPOS into A
-	ldy	#0		; XHIGH always #0
-
-	jsr	HPOSN		; X= (y,x) Y=(a)
-
-	ldx	LOGO_OFFSET
-	asl	desire_boxes,X
+logo_offset_smc:
+	asl	desire_boxes
 	bcc	skip_xdraw
 
-	ldx	#<shape_table	; point to our shape
-	ldy	#>shape_table
-	lda	#0		; ROT=0
-
-	jsr	XDRAW0		; XDRAW 1 AT X,Y
-				; Both A and X are 0 at exit
+	jsr	do_xdraw
 
 skip_xdraw:
-	jsr	RESTORE		; restore FLAGS/X/Y/A
-				; we saved X/Y earlier
 
 logo_nextx:				; NEXT X
 	inc	FRAME
@@ -98,7 +83,7 @@ logo_nextx:				; NEXT X
 	and	#$7
 	bne	no_inc_offset
 
-	inc	LOGO_OFFSET
+	inc	logo_offset_smc+1
 
 no_inc_offset:
 	txa
@@ -111,8 +96,6 @@ no_inc_offset:
 	bne	logo_xloop
 
 logo_nexty:
-
-;	inc	LOGO_OFFSET
 
 	clc
 	tya
@@ -148,40 +131,20 @@ xloop:
 
 
 random_scale:
-	jsr	random16
-	cmp	#20
-	bcs	done_scale	; bge
-
-	lda	SEEDL
+	lda	$F000
 	bmi	done_scale
+	cmp	#20
+	bcs	done_scale
 
 	inc	HGR_SCALE
 
 done_scale:
 
-	stx	XREG		; save X
-	sty	YREG		; save Y
-
-	; setup X and Y co-ords
-	tya			; YPOS into A
-	ldy	XHIGH		; XHIGH in Y
-				; XPOS already in X
-
-	jsr	HPOSN		; X= (y,x) Y=(a)
-
-
-	ldx	#<shape_table	; point to our shape
-	ldy	#>shape_table
-	lda	#0		; ROT=0
-
-	jsr	XDRAW0		; XDRAW 1 AT X,Y
-				; Both A and X are 0 at exit
-
-
-	jsr	RESTORE		; restore FLAGS/X/Y/A
-				; we saved X/Y earlier
+	jsr	do_xdraw
 
 nextx:				; NEXT X
+
+	inc	random_scale+1
 
 	; starting at 4 so hit 256, overflow to high bit
 	; finally end at 280 which is 24?
@@ -209,6 +172,37 @@ nexty:
 	bne	yloop		; if so, loop
 	beq	eloop
 
+
+
+do_xdraw:
+	stx	XREG		; save X
+	sty	YREG		; save Y
+
+
+	; setup X and Y co-ords
+	tya			; YPOS into A
+	ldy	XHIGH		; XHIGH in Y
+				; XPOS already in X
+
+	jsr	HPOSN		; X= (y,x) Y=(a)
+
+
+	ldx	#<shape_table	; point to our shape
+	ldy	#>shape_table
+	lda	#0		; ROT=0
+
+	jsr	XDRAW0		; XDRAW 1 AT X,Y
+				; Both A and X are 0 at exit
+
+
+	jmp	RESTORE		; restore FLAGS/X/Y/A
+				; we saved X/Y earlier
+
+
+
+
+
+
 shape_table:
 	.byte 18,63,36,36,45,45,54,54,63,0	; shape data (a square)
 
@@ -235,73 +229,3 @@ desire_boxes:
 .byte $42,$85,$0A,$80,$A0
 .byte $3A,$B9,$72,$7B,$B8
 .byte $00,$81,$00,$00,$00
-
-
-
-
-
-
-
-
-; 16-bit 6502 Random Number Generator
-
-; Linear feedback shift register PRNG by White Flame
-; http://codebase64.org/doku.php?id=base:small_fast_16-bit_prng
-
-; The Apple II KEYIN routine increments this field
-; while waiting for keypress
-
-;SEEDL = $4E
-;SEEDH = $4F
-
-XOR_MAGIC = $7657	; "vW"
-
-	;=============================
-	; random16
-	;=============================
-	; takes:
-	;	not 0, cc = 5+  = 27
-	;	not 0, cs = 5+12+19 = 36
-	;	$0000	  = 5+7+19 = 31
-	;	$8000     = 5+6+14 = 25
-	;	$XX00	  = 5+6+7+19 = 37
-random16:
-
-	lda	SEEDL							; 3
-	beq	lowZero		; $0000 and $8000 are special values	; 2
-
-	asl	SEEDL		; Do a normal shift			; 5
-	lda	SEEDH							; 3
-	rol								; 2
-	bcc	noEor							; 2
-
-doEor:
-				; high byte is in A
-
-
-	eor	#>XOR_MAGIC						; 2
-	sta	SEEDH							; 3
-	lda	SEEDL							; 3
-	eor	#<XOR_MAGIC						; 2
-	sta	SEEDL							; 3
-	rts								; 6
-
-lowZero:
-									; 1
-	lda	SEEDH							; 3
-	beq	doEor		; High byte is also zero		; 3
-				; so apply the EOR
-									; -1
-				; wasn't zero, check for $8000
-	asl								; 2
-	beq	noEor		; if $00 is left after the shift	; 2
-				; then it was $80
-	bcs	doEor		; else, do the EOR based on the carry	; 3
-
-noEor:
-									; 1
-	sta	SEEDH							; 3
-
-	rts								; 6
-
-
