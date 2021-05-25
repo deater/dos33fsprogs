489729bc169b52848de4511d6f98f2b07cf2fa6f
Vince Weaver <vincent.weaver@maine.edu>
2021-05-25 16:24:29 -0400

    appleiibot: gator for floppy day

--- basic/appleiibot/make_boxes.c
+++ basic/appleiibot/make_boxes.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
 
@@ -7,6 +8,11 @@ int main(int argc, char **argv) {
 	int color,x1,x2,y1,y2;
 	char output[1024];
 	int out_ptr=0;
+	int add=' ';
+
+	if (argc>1) {
+		add=atoi(argv[1]);
+	}
 
 	while(1) {
 
@@ -16,11 +22,11 @@ int main(int argc, char **argv) {
 		sscanf(buffer,"%d %d %d %d %d",
 			&color,&x1,&x2,&y1,&y2);
 
-		output[out_ptr]=color+32;
-		output[out_ptr+1]=x1+32;
-		output[out_ptr+2]=x2+32;
-		output[out_ptr+3]=y1+32;
-		output[out_ptr+4]=y2+32;
+		output[out_ptr]=color+add;
+		output[out_ptr+1]=x1+add;
+		output[out_ptr+2]=x2+add;
+		output[out_ptr+3]=y1+add;
+		output[out_ptr+4]=y2+add;
 		out_ptr+=5;
 	}
 	output[out_ptr]=0;
