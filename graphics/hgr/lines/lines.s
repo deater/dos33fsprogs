19ac3d9bf2bd008d26c8cad8cd87b33d405bea3b
Vince Weaver <vince@deater.net>
2021-07-01 01:39:13 -0400

    hgr: lines: urgh

--- graphics/hgr/lines/lines.s
+++ graphics/hgr/lines/lines.s
@@ -11,16 +11,21 @@ HGR_COLOR	= $E4
 HGR_PAGE	= $E6
 
 
-B_X1	= $F0
-B_Y1	= $F1
-B_X2	= $F2
-B_Y2	= $F3
-B_DX	= $F4
-B_DY	= $F5
-B_SX	= $F6
-B_SY	= $F7
-B_ERR	= $F8
-COUNT	= $F9
+B_X1_L	= $F0
+B_X1_H	= $F1
+B_Y1	= $F2
+B_X2_L	= $F3
+B_X2_H	= $F4
+B_Y2	= $F5
+B_DX_L	= $F6
+B_DX_H	= $F7
+B_DY_L	= $F8
+B_DY_H	= $F9
+B_SX	= $FA
+B_SY	= $FB
+B_ERR_L	= $FC
+B_ERR_H	= $FD
+COUNT	= $FE
 FRAME	= $FF
 
 ; soft-switches
@@ -33,6 +38,7 @@ KEYRESET	= $C010
 HGR2		= $F3D8		; set hires page2 and clear $4000-$5fff
 HGR		= $F3E2		; set hires page1 and clear $2000-$3fff
 HPLOT0		= $F457		; plot at (Y,X), (A)
+HGLIN           = $F53A         ; line to (X,A),(Y)
 HCOLOR1		= $F6F0		; set HGR_COLOR to value in X
 COLORTBL	= $F6F6
 PLOT		= $F800		; PLOT AT Y,A (A colors output, Y preserved)
@@ -44,42 +50,87 @@ WAIT		= $FCA8		; delay 1/2(26+27A+5A^2) us
 
 
 lines:
-
 	jsr	HGR2
 
-restart:
-	lda	#0
-	sta	COUNT
+reset:
+	ldx	#0
+	stx	COUNT
 
-lines_loop:
-	lda	FRAME
-	and	#$7
-;	lda	#$7f
-;	sta	HGR_COLOR
+	stx	B_X1_H
+	stx	B_X1_L
+	stx	B_Y1
 
+	stx	B_X2_H
+	stx	B_X2_L
 
-	lda	#0
-	sta	B_X1
-	lda	#36
+	lda	#191
 	sta	B_Y2
 
-	lda	COUNT
-	cmp	#10
-;end:
-	beq	restart
-
-	asl
-	asl
-	sta	B_Y1
-	sta	B_X2
+left_lines_loop:
 
 	jsr	draw_line
 
+	lda	B_Y1
+	clc
+	adc	#8
+	sta	B_Y1
+
+	lda	B_X2_L		; 280/24 = 140/12 = 70/6 = 11 
+	clc
+	adc	#11
+	sta	B_X2_L
+	bcc	noc
+	inc	B_X2_H
+noc:
 	inc	COUNT
 	inc	FRAME
 
-	jmp	lines_loop
+	lda	COUNT
+	cmp	#24
+end:
+	bne	left_lines_loop
 
+	;================
+	; right lines
+	;================
+	; at this point, count = 24
+	; X1, Y1 = 0,192
+	; X2, Y2 = 264,192
+	; want x1,y1 to go from 264,0 to 0,0
+	; want x2,y2 to go from 264,192 to 264,0
+
+	ldx	#0
+	stx	B_Y1
+
+	ldx	B_X2_L
+	stx	B_X1_L
+	ldx	B_X2_H
+	stx	B_X1_H
+
+right_lines_loop:
+
+	jsr	draw_line
+
+	lda	B_Y2
+	sec
+	sbc	#8
+	sta	B_Y2
+
+	lda	B_X1_L
+	sec
+	sbc	#11
+	sta	B_X1_L
+	bcs	noc2
+	dec	B_X1_H
+noc2:
+	dec	COUNT
+	inc	FRAME
+
+	lda	COUNT
+
+	beq	reset
+
+	bne	right_lines_loop
 
 
 	;============================
@@ -87,6 +138,16 @@ lines_loop:
 	;	from x1,y1 to x2,y2
 	;============================
 draw_line:
+
+
+;	lda	FRAME
+;	and	#$7
+;	tax
+	ldx	#7
+	jsr	HCOLOR1		; set color
+
+
+
 
 	; from wikipedia
 
@@ -101,54 +162,108 @@ init_bresenham:
 	; dx = abs(x2-x1)
 	; sx = x1<x2 ? 1 : -1
 
-	ldx	#0
-	jsr	do_abs
+
+do_abs_x:
+	ldy	#$ff
+
+	sec
+	lda	B_X1_L
+	sbc	B_X2_L			; A = x1 - x2
+	sta	B_DX_L
+	lda	B_X1_H
+	sbc	B_X2_H
+	sta	B_DX_H
+
+	bpl	xis_pos
+
+	ldy	#$1
+xneg:
+	lda	B_DX_L
+	eor	#$ff
+	clc
+	adc	#1
+	sta	B_DX_L
+	lda	B_DX_H
+	eor	#$ff
+	adc	#0
+	sta	B_DX_H
+
+xis_pos:
+	sty	B_SX
+
 
 	; dy = -abs(y2-y1)
 	; sy = y1 < y2 ? 1 : -1
 
-	inx
-	jsr	do_abs
-	jsr	neg			; dy = -abs(y2-y1)
+do_abs_y:
+	ldy	#$1
+
+	sec
+	lda	B_Y1
+	sbc	B_Y2			; A = y1 - y2
+	sta	B_DY_L
+	lda	#0
+	sbc	#0
+	sta	B_DY_H
+
+	bmi	yis_neg
+
+	ldy	#$ff
+yneg:
+	lda	B_DY_L
+	eor	#$ff
+	clc
+	adc	#1
+	sta	B_DY_L
+	lda	B_DY_H
+	adc	#0
+	sta	B_DY_H
+
+yis_neg:
+	sty	B_SY
+
+					; FIXME: just reverse earlier
+;	eor	#$ff			; dy = -abs(y2-y1)
+;	clc
+;	adc	#1
 
 	; err = dx+dy
-	; B_DY is in A already
+
 	clc
-	adc	B_DX
-	sta	B_ERR
+	lda	B_DY_L
+	adc	B_DX_L
+	sta	B_ERR_L
+	lda	B_DY_H
+	adc	B_DX_H
+	sta	B_ERR_H
 
 	;======================
 	; iterative plot points
 
 line_loop:
 
-;	tya
-;	pha
-;	txa
-;	pha
-
 	; hplot X1,Y1
 
-	ldx	B_X1
+	ldy	B_X1_H
+	ldx	B_X1_L
 	lda	B_Y1
-	ldy	#0
 
 	jsr	HPLOT0		; plot at (Y,X), (A)
 
-;	pla
-;	tax
-;	pla
-;	tay
-
 	; check if hit end
 
-	ldy	B_X1
-	cpy	B_X2
+	ldy	B_X1_H
+	cpy	B_X2_H
+	bne	line_no_end
+
+	ldy	B_X1_L
+	cpy	B_X2_L
 	bne	line_no_end
 
 	lda	B_Y1
 	cmp	B_Y2
 	beq	done_line
+
 line_no_end:
 
 	;========================
@@ -156,96 +271,77 @@ line_no_end:
 	;========================
 
 	; err2 = 2*err
-	lda	B_ERR
+	lda	B_ERR_L
 	asl
-	pha			; save err2 for later
-
-	ldx	#0		; setup for common_inc
+	rol	B_ERR_H
 
 	; if err2 >= dy:
 	;   err = err + dy
 	;   x1 = x1 + sx
 
+	; 16 bit signed compare
+	lda	B_ERR_L		; ERR2-DY
+	cmp	B_DY_L
+	lda	B_ERR_H
+	sbc	B_DY_H
+	bvc	blah
+	eor	#$80		; N eor V
+blah:
+	bmi	check_dx
 
-	; B_ERR already in A
-	cmp	B_DY		; check equal first
-	beq	do_x
+	; FIXME: convert ERR2 back to ERR
 
-	clc			; signed compare
-	sbc	B_DY
-	bvc	blah2
-	eor	#$80
-blah2:
-	bmi	skip_x		; ble
+	clc
+	lda	B_ERR_L
+	adc	B_DY_L
+	sta	B_ERR_L
+	lda	B_ERR_H
+	adc	B_DY_H
+	sta	B_ERR_H
 
-do_x:
-	lda	B_DY
-	jsr	common_inc
-skip_x:
+	lda	B_X1_L
+	adc	B_SX
+	sta	B_X1_L
+	lda	B_X1_H
+	adc	#0
+	sta	B_X1_H
 
-
-
-	inx			; setup common inc
-
+check_dx:
 	; if err2 <= dx:
 	;   err = err + dx
 	;   y1 = y1 + sy
-	pla			; restore err2
 
-	clc			; signed compare
-	sbc	B_DX
-	bvc	blah
-	eor	#$80
-blah:
-	bpl	skip_y
+	; 16 bit signed compare
+	lda	B_ERR_L		; ERR2-DX
+	cmp	B_DX_L
+	lda	B_ERR_H
+	sbc	B_DX_H
+	bvc	blah2
+	eor	#$80		; N eor V
+blah2:
+	bmi	skip_y
 
-do_y:
-	lda	B_DX
-	jsr	common_inc
+	clc
+	lda	B_ERR_L
+	adc	B_DX_L
+	sta	B_ERR_L
+	lda	B_ERR_H
+	adc	B_DX_H
+	sta	B_ERR_H
+
+	lda	B_Y1
+	adc	B_SY
+	sta	B_Y1
+;	lda	B_Y1_H
+;	adc	#0
+;	sta	B_Y1_H
+
 
 skip_y:
 
 	jmp	line_loop
 
-
-	;=====================================
-	; common increment
-	;=====================================
-common_inc:
-
-	clc
-	adc	B_ERR
-	sta	B_ERR
-
-	lda	B_X1,X
-	clc
-	adc	B_SX,X
-	sta	B_X1,X
 done_line:
-	rts
-
-
-	;=====================================
-	; init, do the abs and sx calculations
-	;	x=0, for X
-	;	x=1, for Y
-	;=====================================
-do_abs:
-	ldy	#$ff
-	sec
-	lda	B_X1,X
-	sbc	B_X2,X			; A = x1 - x2
-
-	bpl	is_pos
-
-	ldy	#$1
-neg:
-	eor	#$ff
-	clc
-	adc	#1
-is_pos:
-	sty	B_SX,X
-	sta	B_DX,X
 	rts
 
 
