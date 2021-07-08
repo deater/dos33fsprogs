25223da438d75345e0a43a04b977d1d99240fc41
Vince Weaver <vince@deater.net>
2021-07-08 00:49:17 -0400

    mist_hgr: more file adds

---
+++ games/mist_hgr/init_state.s
@@ -0,0 +1,28 @@
+; init state
+
+; initial state at start of game is for all vars to be 0
+
+init_state:
+
+	; global game state
+	lda	#0
+	sta	JOYSTICK_ENABLED
+
+	; game state in saves init
+
+	lda	#$0
+	ldy	#WHICH_LOAD
+init_state_loop:
+	sta	0,Y
+	iny
+	cpy	#END_OF_SAVE
+	bne	init_state_loop
+
+
+	; testing
+
+;	lda	#$ff
+;	sta	MARKER_SWITCHES
+
+	rts
+
