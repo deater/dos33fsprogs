ee82b57505d7b1b4177ce5bdd2c8fed1148e9bec
Vince Weaver <vince@deater.net>
2021-07-17 16:16:18 -0400

    vgi: add test program

---
+++ graphics/hgr/vgi/test_vgi.s
@@ -0,0 +1,47 @@
+; VGI Tester
+
+.include "zp.inc"
+.include "hardware.inc"
+
+
+vgi_test:
+
+	jsr	SETGR
+	jsr	HGR
+	bit	FULLGR
+
+	jsr	vgi_make_tables
+
+	; get pointer to image data
+
+	lda	#<new_data
+	sta	VGIL
+	lda	#>new_data
+	sta	VGIH
+
+	jsr	play_vgi
+
+	jsr	wait_until_keypress
+
+
+done:
+	jmp	done
+
+.include "vgi_common.s"
+
+.include "new.data"
+
+
+	;============================
+	; WAIT UNTIL KEYPRESS
+	;============================
+
+wait_until_keypress:
+
+	lda	KEYPRESS
+	bpl	wait_until_keypress
+
+	bit	KEYRESET
+
+	rts
+
