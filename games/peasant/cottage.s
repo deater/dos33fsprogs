253590a26ddec27526ae0eabcf1ebeb404eb91aa
Vince Weaver <vince@deater.net>
2021-08-12 20:03:11 -0400

    peasant: add some more walking sprites
    not the best, but HGR is hard

--- games/peasant/cottage.s
+++ games/peasant/cottage.s
@@ -246,9 +246,6 @@ display_cottage_text3:
 
 	rts
 
-
-
-
 	;============================
 	; draw peasant
 	;============================
@@ -258,9 +255,49 @@ draw_peasant:
 	lda	PEASANT_Y
 	sta	CURSOR_Y
 
+	lda	PEASANT_DIR
+	cmp	#PEASANT_DIR_RIGHT
+	beq	peasant_right
+
+	;=====================
+	; up up up up
+peasant_up:
+
+	lda	FRAME
+	and	#1
+	beq	peasant_up1
+
+
+peasant_up2:
+	lda	#<peasant_up2_sprite
+	sta	INL
+	lda	#>peasant_up2_sprite
+	jmp	done_pick_draw
+
+peasant_up1:
+	lda	#<peasant_up1_sprite
+	sta	INL
+	lda	#>peasant_up1_sprite
+	jmp	done_pick_draw
+
+
+peasant_right:
+	lda	CURSOR_X
+	and	#1
+	bne	draw_right1
+
+draw_right2:
+	lda	#<peasant_right2_sprite
+	sta	INL
+	lda	#>peasant_right2_sprite
+	jmp	done_pick_draw
+
+draw_right1:
 	lda	#<peasant_right1_sprite
 	sta	INL
 	lda	#>peasant_right1_sprite
+
+done_pick_draw:
 	sta	INH
 
 	jsr	hgr_draw_sprite_7x30
