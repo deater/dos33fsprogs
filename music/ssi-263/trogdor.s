ca67d5e21d75af266ad220382c21f5c473a985c3
Vince Weaver <vincent.weaver@maine.edu>
2021-09-14 11:02:47 -0400

    ssi-263: sample trogdor speech

--- music/ssi-263/trogdor.s
+++ music/ssi-263/trogdor.s
@@ -27,11 +27,11 @@ speech_test:
 
 speech_loop:
 
-	; hello
+	; trogdor
 
-	lda	#<hello
+	lda	#<trogdor
 	sta	SPEECH_PTRL
-	lda	#>hello
+	lda	#>trogdor
 	sta	SPEECH_PTRH
 
 	jsr	ssi263_speak
@@ -54,23 +54,125 @@ wait_until_keypress:
 
 .include "ssi263_simple_speech.s"
 
+	; the document
+	; "Phonetic Speech Dictionary for the SC-01 Speech Synthesizer"
+	; sc01-dictionary.pdf
+	; was very helpful here
 
-.byte 34,"I can honestly say it'll",13
-        .byte "be a pleasure and an honor",13
-        .byte "to burninate you, Rather",13
-        .byte "Dashing.",0
-
-
-
-	; unrefined hello from Programming Guide
-hello:
+trogdor:
 	.byte PHONEME_PAUSE	; PA
 	.byte PHONEME_PAUSE	; PA
-	.byte PHONEME_HF	; HF
-	.byte PHONEME_EH	; EH
+	.byte PHONEME_AH1	; AH1	; I
+	.byte PHONEME_Y		; Y
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_K		; K	; Can
+	.byte PHONEME_AE1	; AE1
+	.byte PHONEME_N		; N
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_AH	; AH	; Honestly
+	.byte PHONEME_N		; N
+	.byte PHONEME_EH1	; EH1
+	.byte PHONEME_S		; S
+	.byte PHONEME_T		; T
 	.byte PHONEME_L		; L
-	.byte PHONEME_O		; O
+	.byte PHONEME_E1	; E1
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_S		; S	; Say
+	.byte PHONEME_A		; A
+	.byte PHONEME_AY	; AY
+	.byte PHONEME_Y		; Y
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_I		; I1	; it'll
+;	.byte PHONEME_I3	; I3
+	.byte PHONEME_T		; T
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_I		; I
+	.byte PHONEME_L		; L
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_B		; B	; be
+	.byte PHONEME_E1	; E1
+	.byte PHONEME_Y		; Y
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_A		; A	; A
+	.byte PHONEME_AY	; AY
+	.byte PHONEME_Y		; Y
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_P		; P	; pleasure
+	.byte PHONEME_L		; L
+	.byte PHONEME_EH1	; EH1
+	.byte PHONEME_SCH	; SCH
+	.byte PHONEME_ER	; ER
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_AE1	; AE1	; and
+	.byte PHONEME_EH	; EH
+	.byte PHONEME_N		; N
+	.byte PHONEME_D		; D
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_AE1	; AE1	; an
+	.byte PHONEME_EH	; EH
+	.byte PHONEME_N		; N
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_AH	; AH	; honor
+	.byte PHONEME_N		; N
+	.byte PHONEME_ER	; ER
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_T		; T	; to
+	.byte PHONEME_IU	; IU
+	.byte PHONEME_U1	; U1
+	.byte PHONEME_U1	; U1
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_B		; B	; burninate
+	.byte PHONEME_ER	; ER
+	.byte PHONEME_R		; R
+	.byte PHONEME_N		; N
+	.byte PHONEME_I		; I
+	.byte PHONEME_N		; N
+	.byte PHONEME_A		; A
+	.byte PHONEME_A		; A
+	.byte PHONEME_Y		; Y
+	.byte PHONEME_T		; T
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_Y		; Y	; you
+	.byte PHONEME_IU	; IU
+	.byte PHONEME_U1	; U1
+	.byte PHONEME_U1	; U1
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA	; ,
+
+	.byte PHONEME_R		; R	; Rather
+	.byte PHONEME_AE1	; AE1
+	.byte PHONEME_AE1	; AE1
+	.byte PHONEME_EH	; EH
+	.byte PHONEME_TH	; TH
+	.byte PHONEME_ER	; ER
+	.byte PHONEME_R		; R
+	.byte PHONEME_PAUSE	; PA
+	.byte PHONEME_PAUSE	; PA
+
+	.byte PHONEME_D		; D	; Dashing
+	.byte PHONEME_AE1	; AE1
+	.byte PHONEME_EH	; EH
+	.byte PHONEME_SCH	; SCH
+	.byte PHONEME_I		; I
+	.byte PHONEME_NG	; NG
+
+
 	.byte PHONEME_PAUSE	; PA
 	.byte PHONEME_PAUSE	; PA
 	.byte $FF
+
 
