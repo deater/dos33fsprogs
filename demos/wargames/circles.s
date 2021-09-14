338152eef69c7acfb6da6f2a12987066326ae0e0
Vince Weaver <vincent.weaver@maine.edu>
2021-09-14 16:33:37 -0400

    wargames: move missiles

---
+++ demos/wargames/circles.s
@@ -0,0 +1,315 @@
+; VGI_Circles
+
+TEMP0 = $80
+TEMP1 = $81
+TEMP2 = $82
+TEMP3 = $83
+TEMP4 = $84
+TEMP5 = $85
+
+P0 = $86
+P1 = $87
+P2 = $88
+P3 = $89
+
+
+XX		= TEMP0
+MINUSXX		= TEMP1
+YY		= TEMP2
+MINUSYY		= TEMP3
+D		= TEMP4
+COUNT		= TEMP5
+
+	;========================
+	; VGI circle
+	;========================
+
+;	VGI_CCOLOR	= P0
+;	VGI_CX		= P1
+;	VGI_CY		= P2
+;	VGI_CR		= P3
+
+vgi_circle:
+	ldx	VGI_CCOLOR
+	lda	COLORTBL,X
+	sta	HGR_COLOR
+
+	;===============================
+	; draw circle
+	;===============================
+	; draw circle at (CX,CY) of radius R
+	; signed 8-bit math so problems if R > 64?
+
+	; XX=0 YY=R
+	; D=3-2*R
+	; GOTO6
+
+	lda	#0
+	sta	XX
+
+	lda	VGI_CR
+	sta	YY
+
+	lda	#3
+	sec
+	sbc	VGI_CR
+	sbc	VGI_CR
+	sta	D
+
+	jmp	do_plots
+
+circle_loop:
+	; X=X+1
+
+	inc	XX
+
+	; IF D>0 THEN Y=Y-1:D=D+4*(X-Y)+10
+	lda	D
+	bmi	else
+
+	dec	YY
+
+	lda	XX
+	sec
+	sbc	YY
+	asl
+	asl
+	clc
+	adc	D
+	adc	#10
+	jmp	store_D
+
+else:
+	; ELSE D=D+4*X+6
+	lda	XX
+	asl
+	asl
+	clc
+	adc	D
+	adc	#6
+store_D:
+	sta	D
+
+do_plots:
+	; setup constants
+
+	lda	XX
+	eor	#$FF
+	sta	MINUSXX
+	inc	MINUSXX
+
+	lda	YY
+	eor	#$FF
+	sta	MINUSYY
+	inc	MINUSYY
+
+	; HPLOT CX+X,CY+Y
+	; HPLOT CX-X,CY+Y
+	; HPLOT CX+X,CY-Y
+	; HPLOT CX-X,CY-Y
+	; HPLOT CX+Y,CY+X
+	; HPLOT CX-Y,CY+X
+	; HPLOT CX+Y,CY-X
+	; HPLOT CX-Y,CY-X
+
+	; calc X co-ord
+
+	lda	#7
+	sta	COUNT
+pos_loop:
+	lda	COUNT
+	and	#$4
+	lsr
+	tay
+
+	lda	COUNT
+	lsr
+	bcc	xnoc
+	iny
+xnoc:
+	lda	VGI_CX
+	clc
+	adc	XX,Y
+	tax
+
+	; calc y co-ord
+
+	lda	COUNT
+	lsr
+	eor	#$2
+	tay
+
+	lda	VGI_CY
+	clc
+	adc	XX,Y
+
+	ldy	#0
+
+	jsr	HPLOT0		; plot at (Y,X), (A)
+
+	dec	COUNT
+	bpl	pos_loop
+
+
+	; IFY>=XTHEN4
+	lda	YY
+	cmp	XX
+	bcs	circle_loop
+
+	rts
+
+
+
+
+	;========================
+	; VGI circle
+	;========================
+	; VGI_CCOLOR	= P0
+	; VGI_CX	= P1
+	; VGI_CY	= P2
+	; VGI_CR	= P3
+
+vgi_filled_circle:
+	ldx	VGI_CCOLOR
+	lda	COLORTBL,X
+	sta	HGR_COLOR
+
+	;===============================
+	; draw filled circle
+	;===============================
+	; draw filled circle at (CX,CY) of radius R
+	; signed 8-bit math so problems if R > 64?
+
+
+	; XX=0 YY=R
+	; D=3-2*R
+	; GOTO6
+
+	lda	#0
+	sta	XX
+
+	lda	VGI_CR
+	sta	YY
+
+	lda	#3
+	sec
+	sbc	VGI_CR
+	sbc	VGI_CR
+	sta	D
+
+	jmp	do_filled_plots
+
+filled_circle_loop:
+	; X=X+1
+
+	inc	XX
+
+	; IF D>0 THEN Y=Y-1:D=D+4*(X-Y)+10
+	lda	D
+	bmi	filled_else
+
+	dec	YY
+
+	lda	XX
+	sec
+	sbc	YY
+	asl
+	asl
+	clc
+	adc	D
+	adc	#10
+	jmp	store_filled_D
+
+filled_else:
+	; ELSE D=D+4*X+6
+	lda	XX
+	asl
+	asl
+	clc
+	adc	D
+	adc	#6
+store_filled_D:
+	sta	D
+
+do_filled_plots:
+	; setup constants
+
+	lda	XX
+	eor	#$FF
+	sta	MINUSXX
+	inc	MINUSXX
+
+	lda	YY
+	eor	#$FF
+	sta	MINUSYY
+	inc	MINUSYY
+
+	; HPLOT CX+X,CY+Y
+	; HPLOT CX-X,CY+Y
+	; HPLOT CX+X,CY-Y
+	; HPLOT CX-X,CY-Y
+	; HPLOT CX+Y,CY+X
+	; HPLOT CX-Y,CY+X
+	; HPLOT CX+Y,CY-X
+	; HPLOT CX-Y,CY-X
+
+
+
+
+	lda	#3
+	sta	COUNT
+filled_pos_loop:
+
+	; calc left side
+
+	; calc X co-ord
+
+	lda	COUNT
+	ora	#$1
+	eor	#$2
+	tay
+	lda	VGI_CX
+	clc
+	adc	XX,Y
+	tax
+
+	; calc y co-ord
+
+	ldy	COUNT
+	lda	VGI_CY
+	clc
+	adc	XX,Y
+
+	ldy	#0
+
+;	pha			; save Y value for later
+
+	jsr	HPLOT0		; plot at (Y,X), (A)
+
+
+	; calc right side
+	lda	COUNT
+	and	#$2
+	eor	#$2
+	tay
+	lda	XX,Y
+	asl
+
+	ldy	#0
+	ldx	#0
+
+	jsr	HLINRL		; plot relative (X,A), (Y)
+				; so in our case (0,XX*2),0
+
+
+
+	dec	COUNT
+	bpl	filled_pos_loop
+
+
+	; IFY>=XTHEN4
+	lda	YY
+	cmp	XX
+	bcs	filled_circle_loop
+
+	rts
--- demos/wargames/wargames.s
+++ demos/wargames/wargames.s
@@ -5,10 +5,206 @@
 
 .include "hardware.inc"
 
+VGI_CCOLOR      = P0
+VGI_CX          = P1
+VGI_CY          = P2
+VGI_CR          = P3
+
+NIBCOUNT	= $09
+
+HGR_COLOR	= $E4
+
+MISSILE_LOW	= $FD
+MISSILE_HIGH	= $FE
+FRAME		= $FF
+
+STATUS_WAITING		= $00
+STATUS_MOVING		= $01
+STATUS_EXPLODING	= $02
+STATUS_DONE		= $FF
+
+MISSILE_STATUS = 0
+MISSILE_START_FRAME = 1
+MISSILE_X = 2
+MISSILE_X_FRAC = 3
+MISSILE_Y = 4
+MISSILE_Y_FRAC = 5
+MISSILE_DX_H = 6
+MISSILE_DX_L = 7
+MISSILE_DY_H = 8
+MISSILE_DY_L = 9
+MISSILE_DEST_X = 10
+MISSILE_DEST_Y = 11
+MISSILE_RADIUS = 12
+
+
 .include "ssi263.inc"
 
 
-speech_test:
+wargames:
+
+	jsr	HGR
+
+	lda	#<map_lzsa
+	sta	getsrc_smc+1
+	lda	#>map_lzsa
+	sta	getsrc_smc+2
+
+	lda	#$20
+
+	jsr	decompress_lzsa2_fast
+
+
+	lda	#0
+	sta	FRAME
+missile_loop:
+
+	lda	#<missiles
+	sta	MISSILE_LOW
+	lda	#>missiles
+	sta	MISSILE_HIGH
+
+	ldy	#0
+	lda	(MISSILE_LOW),Y
+
+	; see if totally done
+	bpl	keep_going
+	jmp	done_missile_loop
+keep_going:
+	cmp	#1
+	beq	missile_ready
+	cmp	#2
+	beq	missile_explode
+
+	; else 0, see if match
+
+	ldy	#MISSILE_START_FRAME
+	lda	(MISSILE_LOW),Y
+	cmp	FRAME
+	beq	missile_activate
+	jmp	done_missile_loop		; not ready
+
+missile_activate:
+
+	; make it ready
+	lda	#STATUS_MOVING
+	ldy	#MISSILE_STATUS
+	sta	(MISSILE_LOW),Y
+
+
+missile_ready:
+
+missile_draw:
+
+	ldy	#MISSILE_X
+	lda	(MISSILE_LOW),Y
+	tax
+
+	ldy	#MISSILE_Y
+	lda	(MISSILE_LOW),Y
+
+	ldy	#0
+
+	jsr	HPLOT0		; plot at (Y,X), (A)
+
+missile_move:
+
+	; add X
+	clc
+	ldy	#MISSILE_DX_L
+	lda	(MISSILE_LOW),Y
+	ldy	#MISSILE_X_FRAC
+	adc	(MISSILE_LOW),Y
+	sta	(MISSILE_LOW),Y
+
+	ldy	#MISSILE_DX_H
+	lda	(MISSILE_LOW),Y
+	ldy	#MISSILE_X
+	adc	(MISSILE_LOW),Y
+	sta	(MISSILE_LOW),Y
+
+	; add Y
+	clc
+	ldy	#MISSILE_DY_L
+	lda	(MISSILE_LOW),Y
+	ldy	#MISSILE_Y_FRAC
+	adc	(MISSILE_LOW),Y
+	sta	(MISSILE_LOW),Y
+
+	ldy	#MISSILE_DY_H
+	lda	(MISSILE_LOW),Y
+	ldy	#MISSILE_Y
+	adc	(MISSILE_LOW),Y
+	sta	(MISSILE_LOW),Y
+
+
+	; see if at end
+	ldy	#MISSILE_Y
+	lda	(MISSILE_LOW),Y
+	ldy	#MISSILE_DEST_Y
+	cmp	(MISSILE_LOW),Y
+	bne	not_match
+
+	ldy	#MISSILE_X
+	lda	(MISSILE_LOW),Y
+	ldy	#MISSILE_DEST_X
+	cmp	(MISSILE_LOW),Y
+	bne	not_match
+
+is_match:
+	lda	#STATUS_EXPLODING
+	ldy	#MISSILE_STATUS
+	sta	(MISSILE_LOW),Y
+not_match:
+	jmp	done_missile_loop
+
+missile_explode:
+
+	lda	#7
+	sta	VGI_CCOLOR
+
+	ldy	#MISSILE_RADIUS
+	lda	(MISSILE_LOW),Y
+	clc
+	adc	#1
+	sta	(MISSILE_LOW),Y
+
+	sta	VGI_CR
+
+	cmp	#15
+	bcc	not_done_explosion
+
+	lda	#STATUS_DONE
+	ldy	#MISSILE_STATUS
+	sta	(MISSILE_LOW),Y
+
+not_done_explosion:
+
+
+	ldy	#MISSILE_X
+	lda	(MISSILE_LOW),Y
+	sta	VGI_CX
+
+	ldy	#MISSILE_Y
+	lda	(MISSILE_LOW),Y
+	sta	VGI_CY
+
+	jsr	vgi_filled_circle
+
+
+done_missile_loop:
+
+	lda	#50
+	jsr	WAIT
+
+	inc	FRAME
+	beq	done_missiles
+
+	jmp	missile_loop
+
+done_missiles:
+
+	;=========================================
 
 	jsr	HOME
 
@@ -52,6 +248,10 @@ wait_until_keypress:
 .include "ssi263_detect.s"
 
 .include "ssi263_simple_speech.s"
+
+.include "decompress_fast_v2.s"
+
+.include "circles.s"
 
 	; the document
 	; "Phonetic Speech Dictionary for the SC-01 Speech Synthesizer"
@@ -199,4 +399,40 @@ trogdor:
 
 	.byte $FF
 
+
+map_lzsa:
+	.incbin "map.lzsa"
+
+
+header:
+	.byte "STRATEGY:                WINNER:",0
+	.byte "USSR FIRST STRIKE",0
+	.byte "U.S. FIRST STRIKE",0
+	.byte "NATO / WARSAW PACT",0
+	.byte "FAR EAST STRATEGY",0
+
+
+ending:
+	.byte "GREETINGS PROFESSOR FALKEN",0
+	.byte "A STRANGE GAME",0
+	.byte "THE ONLY WINNING MOVE IS"
+	.byte "NOT TO PLAY.",0
+
+
+code:
+	.byte "CPE1704TKS",0
+
+
+
+
+missiles:
+	.byte $00	; status
+	.byte $10	; start frame
+	.byte 10,$10	; x-location
+	.byte 50,$10	; y-location
+	.byte $01,$00	; deltax
+	.byte $00,$00	; deltay
+	.byte 100,50	; destination
+	.byte $00	; radius
+	.byte $00,$00,$00	; padding
 
