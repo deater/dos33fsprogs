857cfd8cd3e50ada8c3c6bda8f1ab8d440903cde
Vince Weaver <vince@deater.net>
2021-11-07 14:05:46 -0500

    peasant: update archery

--- games/peasant/archery.s
+++ games/peasant/archery.s
@@ -11,7 +11,7 @@
 .include "inventory.inc"
 .include "parse_input.inc"
 
-LOCATION_BASE	= LOCATION_ARCHERY ; (20)
+LOCATION_BASE	= LOCATION_ARCHERY ; (28)
 
 archery:
 	lda	#0
@@ -91,23 +91,32 @@ game_loop:
 
 	inc	FRAME
 
-	jsr	check_keyboard
+	bit	KEYRESET
 
-	lda	LEVEL_OVER
-	bmi	oops_new_location
-	bne	game_over
+	jsr	wait_until_keypress
+
+	lda	#LOCATION_ARCHERY
+	jmp	update_map_location
+
+
+
+;	jsr	check_keyboard
+
+;	lda	LEVEL_OVER
+;	bmi	oops_new_location
+;	bne	game_over
 
 
 	; delay
 
-	lda	#200
-	jsr	wait
+;	lda	#200
+;	jsr	wait
 
 
-	jmp	game_loop
+;	jmp	game_loop
 
-oops_new_location:
-	jmp	new_location
+;oops_new_location:
+;	jmp	new_location
 
 
 	;************************
@@ -120,47 +129,21 @@ game_over:
 
 
 
-.include "draw_peasant.s"
+;.include "draw_peasant.s"
 
-.include "gr_copy.s"
+;.include "gr_copy.s"
 
 .include "new_map_location.s"
 
-.include "peasant_move.s"
+;.include "peasant_move.s"
 
-;.include "parse_input.s"
+;.include "score.s"
 
-;.include "inventory.s"
-
-.include "score.s"
-
-.include "keyboard.s"
+;.include "keyboard.s"
 
 .include "wait.s"
 .include "wait_a_bit.s"
 
-.include "version.inc"
-
-
-
-; Moved to qload
-;.include "decompress_fast_v2.s"
-;.include "hgr_font.s"
-;.include "draw_box.s"
-;.include "hgr_rectangle.s"
-;.include "hgr_1x28_sprite_mask.s"
-;.include "hgr_1x5_sprite.s"
-;.include "hgr_partial_save.s"
-;.include "hgr_input.s"
-;.include "hgr_tables.s"
-;.include "hgr_text_box.s"
-;.include "clear_bottom.s"
-;.include "hgr_hgr2.s"
-;.include "loadsave_menu.s"
-;.include "wait_keypress.s"
-
 .include "graphics_archery/archery_graphics.inc"
-
-;.include "graphics_cliff/priority_cliff.inc"
 
 
