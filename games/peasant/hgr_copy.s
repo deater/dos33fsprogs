f33a338c72c449c658b6c76c831d6d3e56b08e77
Vince Weaver <vince@deater.net>
2021-12-30 13:56:09 -0500

    peasant: make hgr_copy artificially slow to give the windowblind effect

--- games/peasant/hgr_copy.s
+++ games/peasant/hgr_copy.s
@@ -3,38 +3,38 @@
 	;=========================================================
 	; copy $2000 to $4000
 
-	; would be faster if we unroll it, but much bigger
-
-	; 14+ ((14*256)+20)*32 + 5 = 115347 = 8.6fps
-
-	; theoretical unrolled, 30*6 bytes bigger (180 bytes?)
-	; 2 + ((9*32)+5)*256 + 5 = 75015 = 13.3 fps
+	; intentionally slow for the miniblind effect
 
 hgr_copy:
+	lda	$0
+	sta	INL
+	sta	OUTL
 
-	ldx	#0				; 2
-	lda	#$20				; 2
-	sta	hgr_copy_smc+2			; 4
-	lda	#$40				; 2
-	sta	hgr_copy_smc+5			; 4
-
-hgr_copy_column:
-
-hgr_copy_smc:
-	lda	$2000,X				; 4
-	sta	$4000,X				; 5
-
-	dex					; 2
-	bne	hgr_copy_column			; 2nt/3t
+	lda	#$20
+	sta	INH
+	lda	#$40
+	sta	OUTH
 
 
+	ldy	#0
+hgr_copy_outer:
 
-	inc	hgr_copy_smc+2			; 6
-	inc	hgr_copy_smc+5			; 6
+hgr_copy_inner:
+	lda	#1
+	jsr	wait
 
-	lda	hgr_copy_smc+2			; 4
-	cmp	#$40				; 2
-	bne	hgr_copy_column			; 2/3
+	lda	(INL),Y
+	sta	(OUTL),Y
+	iny
+	bne	hgr_copy_inner
 
-	rts					; 6
+	inc	INH
+	inc	OUTH
+	lda	OUTH
+	cmp	#$60
+	bne	hgr_copy_outer
+
+	rts
+
+
 
