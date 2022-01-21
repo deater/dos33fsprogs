b6518e69ab7664395cbec21a6dadafe296df7bce
Vince Weaver <vince@deater.net>
2022-01-20 22:48:42 -0500

    tiny_gr: update to be more robust

--- demos/l/tinygr_8/tiny_gr.s
+++ demos/l/tinygr_8/tiny_gr.s
@@ -4,6 +4,8 @@
 
 ; trying to make small graphics in 8 bytes for Apple II
 
+; drops into the middle of the CHRGET get next token routine
+; that the Applesoft interpreter copies to $B1 - $C8 on boot
 
 ; zero page locations
 
@@ -15,7 +17,30 @@ COUT1	= $FDF0                 ; output A to screen
 
 tiny_gr:
 
+;chrget	B1:	E6 B8		inc	txtptrl
+;	B3:	D0 02		bne	chrgot
+;	B5:	E6 B9		inc	txtptrh
+;chrgot	B7:	AD 00 08	lda	$800	txtprtrl/txtptrh
+;	BA:	C9 3A		cmp	#$3A
+;	BC:	B0 0A		bcs	end
+;	BE:	C9 20		cmp	#$20
+;	C0:	F0 EF		beq	chrget
+;	C2:	38		sec
+;	C3:	E9 30		sbc	#$30
+;	C5:	38		sec
+;	C6:	E9 D0		sbc	#$D0
+;	C8:	60		rts
+
+; we drop this at B9
+
+;chrget	B1:	E6 B8		inc	txtptrl
+;	B3:	D0 02		bne	chrgot
+;	B5:	E6 B9		inc	txtptrh
+;chrgot	B7:	AD 00 08	lda	$800
 tiny_loop:
-	lda	#$20
-	bit	$C050
-	jsr	COUT
+;	lda	$7800
+.byte 	$78	; sei		; B9
+	bit	$C050		; BA BB BC set LORES
+	jsr	COUT1		; BD BE BF output A to stdout, with scroll
+.byte	$50 ; bvc (bra we hope)	; C0
+;		chrget
