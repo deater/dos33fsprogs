7e9b9e90f25a8e0a6620b09970b09fefe1970d92
Vince Weaver <vincent.weaver@maine.edu>
2022-03-15 15:33:50 -0400

    lemm: was too big, load with qboot
    bit of a hack but seems to work

---
+++ games/lemm/qboot_sector.s
@@ -0,0 +1,244 @@
+;	fast seek/multi-read
+;	copyright (c) Peter Ferrie 2015-16
+
+	; Paramaters for loading QLOAD
+
+	sectors   = 14		; user-defined
+	firsttrk  = 1		; user-defined, first track to read
+	firstsec  = 0		; user-defined, first sector to read
+	address   = $12		; user-defined
+	entry     = $1200	; user-defined
+	version   = 1
+
+        ;memory usage:
+        ;256 bytes ($200-2ff) static table
+        grouped   = $200
+
+	; stay away from interrupt vectors at $3fe !!!
+
+        ;106 bytes ($300-369) static table
+	preshift	= $300
+        zvalue		= $fd         ; only during init
+        znibble		= $fe         ; only during init
+        zmask		= $ff         ; only during init
+
+	WHICH_SLOT	= $DA
+
+; $26/$27	sector read location (ROM)
+; $3D		sector number (ROM)
+
+
+; at entry (at least on AppleWin) A=1, X=60 (slot<<4), Y=0
+; qkumba says cffa cards leave Y at $10
+;	26/27 =	00/09
+;	3D = 1
+
+	; For Disk II booting, the firmware loads track0/sector0
+	; to $800 and then jumps to $801
+
+.org $800
+	.byte	1		; number of sectors for ROM to load
+
+boot_entry:
+	; this code loads two sectors up to $10/$11
+
+	; assume A=1 coming in here
+
+	lsr			; check sector number
+				; A=0, carry=1
+	tay			; Y=0
+	adc	#$0f		; A=$10 (destintation)
+
+	sta	$27		; set or update address as needed
+	cmp	#$12
+				; 10  11  12  (1       1     1)
+				; be, bf, c0  (1011 1011 1100)
+				; so if hit $c000 we are done
+
+	beq	done_load_2	; branch if loaded 2
+
+	inc	$3d		; increment sector (faster to find)
+
+	; call to the read routine in proper slot
+	; using rts to jump indirect to
+	; $CX5C
+
+	; this routine reads sector in $3D on track in $41
+	;	to address in $26/$27
+	; when it's done it jumps back to $801
+	stx	WHICH_SLOT	; save for later
+
+	txa			; x is slot# << 4
+	lsr
+	lsr
+	lsr
+	lsr
+	ora	#$c0		; slot to PROM base
+	pha
+	lda     #$5b            ;read-1
+	pha
+	rts			; return used to call $CX5C in DISK II ROM
+
+done_load_2:
+
+	; patch self modifying code for Q6L read
+
+	txa
+	ora	#$8c		; slot to Q6L
+				; Q6L?
+				; if slot 6, after this A is $EC
+	; Y should be 2 here
+patch_loop:
+	iny
+	ldx	patchtbl-3, Y
+	sta	code_begin, X   ; replace placeholders with Q6L
+				; BE02 = EC? lda c0ec
+				; so sets to c08c (Q6L)
+
+	bne	patch_loop
+
+	; patch self-modifying code for turning motor off
+
+	and	#$f8            ; MOTOROFF (c088) -> c0e8
+	sta	slotpatch7+1
+
+	; patch self-modifying code for turning motor on
+	clc
+	adc	#1            ; MOTORON (c089) -> c0e9
+	sta	slotpatch9+1
+
+	; patch self-modifying code for phase off
+
+	eor	#9              ; PHASEOFF (c080)
+	sta	slotpatch8+1
+
+	ldx	#$3f
+	stx	zmask
+	inx
+	ldy	#$7f
+
+	bne	skip_ahead	; branch always
+
+	; pad with zeros until $839
+	; $839 is the entry point
+	;	adjusts address at $8FE to be entry point
+	;	jumps to boot 2
+;.res	$839-*
+
+;	lda	#>(entry-1)
+;	pha
+;	lda	#<(entry-1)
+;	pha
+;	jsr	preread
+;	jmp	$1000		; stage2 entry point
+
+patchtbl:
+	.byte   <(slotpatch1+1), <(slotpatch2+1), <(slotpatch3+1)
+	.byte	<(slotpatch4+1), <(slotpatch5+1), <(slotpatch6+1)
+indextbl:	;the 0 also terminates the patchtbl list!
+	.byte   0, 2, 1, 3
+
+
+        ;construct denibbilisation table
+        ;pre-shifted for interleave read
+
+skip_ahead:
+loopaa:
+	sty	znibble
+	tya
+	asl
+	bit	znibble
+	beq	loopz
+	ora	znibble
+	eor	#$ff
+	and	#$7e
+loopa:
+	bcs	loopz
+	lsr
+	bne	loopa
+	dex
+	txa
+	asl
+	asl
+	sta	preshift-$16, Y
+loopz:
+	dey
+	bne	loopaa
+
+        ;construct 2-bit group table
+
+	sty     zvalue
+loopbb:
+	lsr     zmask
+	lsr     zmask
+loopb:
+	lda	indextbl, X
+	sta	grouped, Y
+	inc	zvalue
+	lda	zvalue
+	and	zmask
+	bne	loopy
+	inx
+	txa
+	and	#3
+	tax
+loopy:
+	iny
+	iny
+	iny
+	iny
+	cpy	#3
+	bcs	loopb
+	iny
+	cpy	#3
+	bcc	loopbb
+	lda	#>(entry-1)
+	pha
+	lda	#<(entry-1)
+	pha
+	jsr	preread
+
+	; seek backward support
+;	sty	startsec+1
+;	sta	tmpadr+1
+;	stx	total+1
+
+	jmp	seekread
+
+preread:
+
+;copy post-read if necessary
+;push post-read address here
+;        pla
+;        tax
+;        pla
+;        tay
+;        lda     #>(postread-1)
+;        pha
+;        lda     #<(postread-1)
+;        pha
+;        tya
+;        pha
+;        txa
+;        pha
+
+	lda	#<(firsttrk*2)
+	sta	phase+1
+	ldx	#sectors
+	lda	#address
+	ldy	#firstsec
+	rts
+
+
+
+end_code:
+
+.res	$8fe-*
+
+; traditionally, entry point to jump to at end of loading
+;	$1000 in this case
+;*=$8fe
+	.byte   $10, $00
+
+
+.include "qboot_stage2.s"
---
+++ games/lemm/qload.s
@@ -0,0 +1,250 @@
+; Loader for MIST
+
+.include "zp.inc"
+.include "hardware.inc"
+
+;.include "common_defines.inc"
+.include "qboot.inc"
+
+LOAD_TEXT_TITLE = 0
+
+qload_start:
+
+	; init the write code
+;	lda	WHICH_SLOT
+;	jsr	popwr_init
+
+	; first time entry
+	; start by loading text title
+
+	lda	#LOAD_TEXT_TITLE	; load title
+	sta	WHICH_LOAD
+
+	lda	#1
+	sta	CURRENT_DISK		; current disk number
+
+	jsr	load_file
+
+	jsr	$6000
+.if 0
+	lda	#LOAD_TITLE		; load title
+	sta	WHICH_LOAD
+
+main_game_loop:
+	jsr	load_file
+
+	lda	WHICH_LOAD
+	bne	not_title
+
+start_title:
+	jsr	$4000
+	jmp	main_game_loop
+
+not_title:
+	jsr	$2000
+	jmp	main_game_loop
+.endif
+
+	;====================================
+	; loads file specified by WHICH_LOAD
+	;====================================
+load_file:
+	ldx	WHICH_LOAD
+
+	lda	which_disk_array,X
+	cmp	CURRENT_DISK
+	bne	change_disk
+
+load_file_no_diskcheck:
+	lda	load_address_array,X
+	sta	load_address
+
+	lda	track_array,X
+	sta	load_track
+
+	lda	sector_array,X
+	sta	load_sector
+
+	lda	length_array,X
+	sta	load_length
+
+	jsr	load_new
+
+	rts
+
+	;===================================================
+	;===================================================
+	; change disk
+	;===================================================
+	;===================================================
+
+change_disk:
+.if 0
+	; turn off disk drive light
+
+	jsr	driveoff
+
+	jsr	TEXT
+	jsr	HOME
+
+	lda	#<error_string
+	sta	OUTL
+	lda	#>error_string
+	sta	OUTH
+
+	ldx	WHICH_LOAD
+	lda	which_disk_array,X
+	clc
+	adc	#48
+
+	ldy	#19
+	sta	(OUTL),Y
+
+	ldy	#0
+
+quick_print:
+	lda	(OUTL),Y
+	beq	quick_print_done
+	jsr	COUT1
+	iny
+	jmp	quick_print
+
+quick_print_done:
+
+fnf_keypress:
+	lda	KEYPRESS
+	bpl	fnf_keypress
+	bit	KEYRESET
+
+	;==============================================
+	; actually verify proper disk is there
+	; read T0:S0 and verify proper disk
+
+	lda	WHICH_LOAD
+	pha
+
+	ldx	#LOAD_FIRST_SECTOR	; load track 0 sector 0
+	stx	WHICH_LOAD
+
+	jsr	load_file_no_diskcheck
+
+	pla
+	sta	WHICH_LOAD
+	tax
+
+	; first sector now in $c00
+	;	offset 59
+	;		disk1 = $0a
+	;		disk2 = $32 ('2')
+	;		disk3 = $33 ('3')
+
+	lda	$c59
+	cmp	#$0a
+	beq	is_disk1
+	cmp	#$32
+	beq	is_disk2
+	cmp	#$33
+	beq	is_disk3
+	bne	change_disk		; unknown disk
+
+is_disk1:
+	lda	#1
+	bne	disk_compare
+
+is_disk2:
+	lda	#2
+	bne	disk_compare
+
+is_disk3:
+	lda	#3
+
+disk_compare:
+	cmp	which_disk_array,X
+	bne	change_disk		; disk mismatch
+
+	;==============================================
+	; all good, retry original load
+
+	jsr	HOME
+
+	ldx	WHICH_LOAD
+	lda	which_disk_array,X
+	sta	CURRENT_DISK
+
+	jmp	load_file
+
+; offset for disk number is 19
+error_string:
+.byte "PLEASE INSERT DISK 1, PRESS RETURN",0
+
+.endif
+
+which_disk_array:
+	.byte 1,1,3,3		; MIST_TITLE,MIST,MECHE,SELENA
+	.byte 1,1,3,2		; OCTAGON,VIEWER,STONEY,CHANNEL
+	.byte 2,1,2,2		; CABIN,DENTIST,ARBOR,NIBEL
+	.byte 1,1,1,3		; SHIP,GENERATOR,D'NI,SUB
+	.byte 1			; TEXT_TITLE
+	.byte 1,1,1,1,1		; SAVE1,SAVE2,SAVE3,SAVE4,SAVE5
+	.byte $f		; FIRST_SECTOR
+
+load_address_array:
+        .byte $60,$20,$20,$20	; MIST_TITLE,MIST,MECHE,SELENA
+	.byte $20,$20,$20,$20	; OCTAGON,VIEWER,STONEY,CHANNEL
+	.byte $20,$20,$20,$20	; CABIN,DENTIST,ARBOR,NIBEL
+	.byte $20,$20,$20,$20	; SHIP,GENERATOR,D'NI,SUB
+	.byte $08		; TEXT_TITLE
+	.byte $0E,$0E,$0E,$0E
+	.byte $0E		; SAVE1,SAVE2,SAVE3,SAVE4,SAVE5
+	.byte $0C		; FIRST_SECTOR
+
+track_array:
+        .byte  3, 8, 1,21	; MIST_TITLE,MIST,MECHE,SELENA
+	.byte 18,31,11, 1	; OCTAGON,VIEWER,STONEY,CHANNEL
+	.byte 27,26,10,20	; CABIN,DENTIST,ARBOR,NIBEL
+	.byte 30,32,28,30	; SHIP,GENERATOR,D'NI,SUB
+	.byte  0		; TEXT_TITLE
+	.byte  0, 0, 0, 0, 0	; SAVE1,SAVE2,SAVE3,SAVE4,SAVE5
+	.byte  0		; FIRST_SECTOR
+
+sector_array:
+        .byte  0, 0, 0, 0	; MIST_TITLE,MIST,MECHE,SELENA
+	.byte  0, 8, 0, 0	; OCTAGON,VIEWER,STONEY,CHANNEL
+	.byte  0, 0, 0, 0	; CABIN,DENTIST,ARBOR,NIBEL
+	.byte  0,13, 0, 1	; SHIP,GENERATOR,D'NI,SUB
+	.byte  6		; TEXT_TITLE
+	.byte 11,12,13,14,15	; SAVE1,SAVE2,SAVE3,SAVE4,SAVE5
+	.byte  0		; FIRST_SECTOR
+
+length_array:
+        .byte  88,159,157,145	; MIST_TITLE,MIST,MECHE,SELENA
+	.byte 128, 20,158,135	; OCTAGON,VIEWER,STONEY,CHANNEL
+	.byte  61, 31,159,109	; CABIN,DENTIST,ARBOR,NIBEL
+	.byte  20, 33, 27, 78	; SHIP,GENERATOR,D'NI,SUB
+	.byte   3		; TEXT_TITLE
+	.byte   1,1,1,1,1	; SAVE1,SAVE2,SAVE3,SAVE4,SAVE5
+	.byte   1		; FIRST_SECTOR
+
+;        .include        "audio.s"
+;	.include	"linking_noise.s"
+ ;       .include        "decompress_fast_v2.s"
+  ;      .include        "draw_pointer.s"
+   ;     .include        "end_level.s"
+;	.include        "gr_copy.s"
+;        .include        "gr_fast_clear.s"
+ ;       .include        "gr_offsets.s"
+ ;       .include        "gr_pageflip.s"
+ ;       .include        "gr_putsprite_crop.s"
+ ;       .include        "keyboard.s"
+ ;       .include        "text_print.s"
+;	.include	"loadstore.s"
+;	.include	"joystick.s"
+
+ ;       .include        "page_sprites.inc"
+;	.include        "common_sprites.inc"
+
+;	.include	"qkumba_popwr.s"
+
+qload_end:
+
+.assert (>qload_end - >qload_start) < $e , error, "loader too big"
