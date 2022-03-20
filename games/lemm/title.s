1815e74976b3e6b6969afc46679c8a819cc08834
Vince Weaver <vince@deater.net>
2022-03-20 11:47:10 -0400

    lemm: more complete credits

--- games/lemm/title.s
+++ games/lemm/title.s
@@ -20,7 +20,7 @@ show_title:
 
 	; print the text
 
-	ldx	#12
+	ldx	#16
 title_loop:
 
 	jsr	move_and_print
@@ -34,16 +34,20 @@ title_loop:
 
 
 title_text:
-.byte  0, 0,"LOADING LEMM V0.01",0
+.byte  0, 0,"LOADING LEMM V0.02",0
 .byte  0, 1,"  LEMM PROOF-OF-CONCEPT DEMAKE",0
-.byte  0, 4,"BASED ON LEMMINGS BY DMA DESIGN",0
-.byte  0, 7,"APPLE II PORT: VINCE WEAVER",0
-.byte  0, 8,"DISK CODE    : QKUMBA",0
-.byte  0, 9,"LZSA CODE    : EMMANUEL MARTY",0
-.byte  0,11,"   WASD/ARROWS MOVE",0
-.byte  0,12,"   ENTER ACTION",0
-.byte  0,13,"   J TOGGLES JOYSTICK",0
+.byte  0, 3,"BASED ON LEMMINGS BY DMA DESIGN",0
+.byte  0, 5,"  APPLE II PORT: VINCE WEAVER",0
+.byte  0, 6,"  DISK : QKUMBA    B2D  : B. BUCKELS",0
+.byte  0, 7,"  LZSA : E. MARTY  AUDIO: O. SCHMIDT",0
+.byte  0, 8,"  MUSIC: J. SCHARVONA",0
+.byte  0,10,"DIRECTIONS:",0
+.byte  0,11,"   WASD/ARROWS   : MOVE POINTER",0
+.byte  0,12,"   ENTER/SPACE   : ACTION",0
+.byte  0,13,"   1...8         : FAST JOB SELECT",0
+.byte  0,14,"   J             : ENABLE JOYSTICK",0
 .byte  0,16,"       ______",0
 .byte  0,17,"     A \/\/\/ SOFTWARE PRODUCTION",0
 .byte  0,19," HTTP://WWW.DEATER.NET/WEAVE/VMWPROD",0
+.byte  4,21,"PRESS 1-5 TO CHOOSE START LEVEL",0
 
