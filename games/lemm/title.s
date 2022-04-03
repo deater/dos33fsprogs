e057eae1aeb459f6b6c09a846e730addecb20834
Vince Weaver <vince@deater.net>
2022-04-03 01:08:43 -0400

    lemm: bump to version 0.7

--- games/lemm/title.s
+++ games/lemm/title.s
@@ -34,7 +34,7 @@ title_loop:
 
 
 title_text:
-.byte  0, 0,"LOADING LEMM V0.06 (31 MAR 2022)",0
+.byte  0, 0,"LOADING LEMM V0.07 (2 APR 2022)",0
 .byte  0, 1,"  LEMM PROOF-OF-CONCEPT DEMAKE",0
 .byte  0, 3,"BASED ON LEMMINGS BY DMA DESIGN",0
 .byte  0, 5,"  APPLE II PORT: VINCE WEAVER",0
@@ -42,10 +42,10 @@ title_text:
 .byte  0, 7,"  LZSA : E. MARTY  AUDIO: O. SCHMIDT",0
 .byte  0, 8,"  MUSIC: J. SCHARVONA",0
 .byte  0,10,"DIRECTIONS:",0
-.byte  0,11,"   WASD/ARROWS   : MOVE POINTER",0
-.byte  0,12,"   ENTER/SPACE   : ACTION",0
-.byte  0,13,"   1...8         : FAST JOB SELECT",0
-.byte  0,14,"   J             : ENABLE JOYSTICK",0
+.byte  0,11,"   WASD/ARROWS  : MOVE POINTER",0
+.byte  0,12,"   ENTER/SPACE  : ACTION",0
+.byte  0,13,"   1...8        : FAST JOB SELECT",0
+.byte  0,14,"   J,!          : ENABLE JOYSTICK,CHEAT",0
 .byte  0,16,"       ______",0
 .byte  0,17,"     A \/\/\/ SOFTWARE PRODUCTION",0
 .byte  0,19," HTTP://WWW.DEATER.NET/WEAVE/VMWPROD",0
