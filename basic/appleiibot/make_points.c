b0c3736813957e6adbbea810974885734c025b76
Vince Weaver <vincent.weaver@maine.edu>
2022-09-09 18:34:51 -0400

    basic: add mystery

--- basic/appleiibot/make_points.c
+++ basic/appleiibot/make_points.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//#define LINE_VALUE	279
+//#define STOP_VALUE	0
+//#define MODE_VALUE	3
+
+#define LINE_VALUE	3
+#define STOP_VALUE	6
+#define MODE_VALUE	9
+
+
 static int debug=1;
 
 /* if div=3 */
@@ -45,7 +54,7 @@ int main(int argc, char **argv) {
 		sscanf(buffer,"%d %d %d %d %d",
 			&a1,&a2,&a3,&a4,&a5);
 		if (debug) fprintf(stderr,"%d %d %d %d %d\n",a1,a2,a3,a4,a5);
-		if (a1==279) {
+		if (a1==LINE_VALUE) {
 			output[out_ptr]=((a1-xadjust)/div)+add;
 			output[out_ptr+1]=((a2-xadjust)/div)+add;
 			output[out_ptr+2]=((a3-xadjust)/div)+add;
@@ -53,11 +62,11 @@ int main(int argc, char **argv) {
 			output[out_ptr+4]=((a5-xadjust)/div)+add;
 			out_ptr+=5;
 		}
-		else if (a1==3) {
+		else if (a1==MODE_VALUE) {
 			output[out_ptr]=((a1-xadjust)/div)+add;
 			out_ptr+=1;
 		}
-		else if (a1==0) {
+		else if (a1==STOP_VALUE) {
 			output[out_ptr]=((a1-xadjust)/div)+add;
 			out_ptr+=1;
 			break;
