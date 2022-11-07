28fb6d13ba8d3d015dd6493cd30544954825cd14
Vince Weaver <vince@deater.net>
2022-11-06 23:31:27 -0500

    tiny_tracker: working on things

---
+++ music/tiny_tracker6/d4.s
@@ -0,0 +1,63 @@
+; Apple II graphics/music in 1k
+
+; by deater (Vince Weaver) <vince@deater.net>
+
+; Zero Page
+	.include "zp.inc"
+	.include "hardware.inc"
+
+; for a 256 entry we need to fit in 252 bytes
+
+; 310 bytes -- initial
+; 268 bytes -- strip out interrupts
+; 262 bytes -- simplify init
+; 261 bytes -- optimize init more
+; 253 bytes -- optimize var init
+; 252 bytes -- bne vs jmp
+; 250 bytes -- song only has 16 notes so can never be negative
+; 249 bytes -- make terminating value $80 instead of $FF
+; 247 bytes -- combine note loop.  makes song a bit faster
+; 245 bytes -- try to optimize writing out volume
+; 255 bytes -- add in some visualization
+; 252 bytes -- re-arrange decode code
+d2:
+
+	;===================
+	; music Player Setup
+
+tracker_song = peasant_song
+
+	; assume mockingboard in slot#4
+
+	; inline mockingboard_init
+
+.include "mockingboard_init.s"
+
+.include "tracker_init.s"
+
+	jsr	SETGR			; enable lo-res graphics
+
+game_loop:
+	; typically A=0, X=FF, Y=0 here
+
+	; play a frame of music
+
+.include "play_frame.s"
+.include "ay3_write_regs.s"
+
+	; delay 20Hz, or 1/20s = 50ms
+
+	lda	#140
+	jsr	WAIT
+
+	lda	#140
+	jsr	WAIT
+
+	lda	#140
+	jsr	WAIT
+
+	beq	game_loop
+
+; music
+.include	"mA2E_4.s"
+
---
+++ music/tiny_tracker6/play_frame.s
@@ -0,0 +1,78 @@
+play_frame:
+
+	;============================
+	; see if still counting down
+
+	lda	SONG_COUNTDOWN
+	bpl	done_update_song
+
+set_notes_loop:
+
+	;==================
+	; load next byte
+
+	ldy	SONG_OFFSET
+	lda	tracker_song,Y
+
+	;==================
+	; see if hit end
+
+	; this song only 16 notes so valid notes always positive
+;	cmp	#$80
+	bpl	not_end
+
+	;====================================
+	; if at end, loop back to beginning
+
+	asl			; reset song offset to 0
+	sta	SONG_OFFSET
+	beq	set_notes_loop
+
+not_end:
+
+	; NNNNNNEC -- c=channel, e=end, n=note
+
+	pha				; save note
+
+	and	#1
+	tax
+	ldy	#$0E
+	sty	AY_REGS+8,X		; $08 set volume A,B
+
+	asl
+	tax				; put channel offset in X
+
+
+	pla				; restore note
+	tay
+	and	#$2
+	sta	SONG_COUNTDOWN		; always 2 long?
+
+	tya
+	lsr
+	lsr				; get note in A
+
+	tay				; lookup in table
+
+	lda	frequencies_high,Y
+	sta	AY_REGS+1,X
+;	sta	$500,X
+
+	lda	frequencies_low,Y
+	sta	AY_REGS,X		; set proper register value
+
+	; visualization
+blah_urgh:
+	sta	$400,Y
+	inc	blah_urgh+1
+
+
+	;============================
+	; point to next
+
+	; assume less than 256 bytes
+	inc	SONG_OFFSET
+
+done_update_song:
+	dec	SONG_COUNTDOWN
+	bmi	set_notes_loop
