02e36c7980a2a5627b5d3b825951ac4f290fa0ec
Vince Weaver <vince@deater.net>
2023-01-03 23:59:57 -0500

    zero_wing: some more graphics

--- music/zero_wing/zw.s
+++ music/zero_wing/zw.s
@@ -12,12 +12,50 @@ hires_start:
 	;===================
 	jsr	HOME
 
-	jsr	HGR
+
 
 	;===================
 	; Load graphics
 	;===================
 load_loop:
+
+	jsr	HGR
+
+	;=============================
+	; In AD...
+	;=============================
+
+	jsr	HOME
+
+	lda	#<inad_text
+	sta	OUTL
+	lda	#>inad_text
+	sta	OUTH
+
+	jsr	move_and_print
+
+	jsr	wait_until_keypress
+
+	;=============================
+	; War was beginning
+	;=============================
+
+
+	jsr	HOME
+
+	lda	#<ship_data
+	sta	zx_src_l+1
+
+	lda	#>ship_data
+	sta	zx_src_h+1
+
+	lda	#$20
+
+	jsr	zx02_full_decomp
+
+	jsr	move_and_print
+
+	jsr	wait_until_keypress
 
 	;=============================
 	; OPERATOR
@@ -111,21 +149,59 @@ load_loop:
 	.include	"text_print.s"
 	.include	"gr_offsets.s"
 
-cats_data:
-	.incbin "graphics/cats.hgr.zx02"
 
+inad_text:
+	.byte 14,21,"IN A.D. 2101",0
+war_text:
+	.byte 11,21,"WAR WAS BEGINNING.",0
+what_text:
+	.byte 0,21,"CAPTAIN: WHAT HAPPEN ?",0	; scroll
+bomb_text:
+	.byte 0,21,"MECHANIC: SOMEBODY SET UP US",0
+	.byte 10,21,         "THE BOMB.",0
+operator_text:
+	.byte 0,20,"OPERATOR: WE GET SIGNAL.",0
+	.byte 1,22, "CAPTAIN: WHAT !",0
+main_text:
+	.byte 0,20,"OPERATOR: MAIN SCREEN TURN ON.",0
+you_text:
+	.byte 0,20,"CAPTAIN: IT'S YOU !!",0
 cats_text:
 	.byte 0,21,"CATS: HOW ARE YOU GENTLEMEN !!",0
+base_text:
+	.byte 0,21,"CATS: ALL YOUR BASE ARE BELONG",0
+	.byte 6,22,      "TO US.",0
+destruction_text:
+	.byte 0,21,"CATS: YOU ARE ON THE WAY TO",0
+	.byte 6,22,      "DESTRUCTION.",0
+captain_text:
+	.byte 0,21,"CAPTAIN: WHAT YOU SAY !!",0
+survive_text:
+	.byte 0,21,"CATS: YOU HAVE NO CHANCE TO",0
+	.byte 6,22,      "SURVIVE MAKE YOUR TIME.",0
+ha_text:
+	.byte 0,21,"CATS: HA HA HA HA ....",0
+op_cap_text:
+	.byte 0,21,"OPERATOR: CAPTAIN !!",0
+zig_text:
+	.byte 0,21,"CAPTAIN: TAKE OFF EVERY 'ZIG'!!",0
+doing_text:
+	.byte 0,21,"CAPTAIN: YOU KNOW WHAT YOU DOING.",0
+move_zig_text:
+	.byte 0,21,"CAPTAIN: MOVE 'ZIG'.",0
+justice_text:
+	.byte 0,21,"CAPTAIN: FOR GREAT JUSTICE.",0
+
+cats_data:
+	.incbin "graphics/cats.hgr.zx02"
 
 captain_data:
 	.incbin "graphics/captain.hgr.zx02"
 
-captain_text:
-	.byte 0,21,"CAPTAIN: WHAT YOU SAY !!",0
-
 operator_data:
 	.incbin "graphics/operator.hgr.zx02"
 
-operator_text:
-	.byte 0,20,"OPERATOR: WE GET SIGNAL.",0
-	.byte 1,22,"CAPTAIN: WHAT !",0
+ship_data:
+	.incbin "graphics/ship.hgr.zx02"
+
+
