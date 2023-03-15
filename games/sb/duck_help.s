5ec950a48ac1a6edeff1443f6fc3fc94315845da
Vince Weaver <vince@deater.net>
2023-03-15 02:07:52 -0400

    sb: add text info

---
+++ games/sb/duck_help.s
@@ -0,0 +1,18 @@
+;    **********************************
+;    *    DUCK POND SIMULATOR 3.0     *
+;    *                                *
+;    *        COPYRIGHT 1981          *
+;    **********************************
+;
+;               INSTRUCTIONS:
+;       "F" : THROWS FOOD
+;       OTHER KEYS DO OTHER THINGS
+;
+;  PRESS Q NOW TO QUIT
+; PRESS SPACEBAR TO RESUME
+
+; ###                    ###              ***
+; #####                ***###           ###***
+;  #####                  ##               **
+;  #### #                 ########         #####***
+;  #### #                 #######          ######*
