5d4e0b066b116724f3cd2aa79f487e8b2259babe
Vince Weaver <vince@deater.net>
2023-05-31 01:15:43 -0400

    sb: forgot a file

---
+++ games/sb/purple.s
@@ -0,0 +1,35 @@
+PURPLE = $D000
+PURPLE_LENGTH = 28
+
+play_purple:
+	; only avail if language card
+
+	lda	SOUND_STATUS
+	and	#SOUND_IN_LC
+	beq	done_play_purple
+
+	; switch in language card
+	; read/write RAM $d000 bank 1
+
+	 bit     $C083
+	 bit     $C083
+
+
+	; call the btc player
+
+	lda	#<PURPLE
+	sta	BTC_L
+	lda	#>PURPLE
+	sta	BTC_H
+
+	ldx	#PURPLE_LENGTH			; 28 pages long???
+	jsr	play_audio
+
+        ; read/write RAM, $d000 bank 2
+
+	bit     $c08B
+	bit     $c08B
+
+done_play_purple:
+	rts
+
