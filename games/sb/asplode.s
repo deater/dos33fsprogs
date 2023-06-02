b29f0334ae5c595b4fb7e85e6a99687c2824804e
Vince Weaver <vincent.weaver@maine.edu>
2023-06-02 15:40:42 -0400

    sb: add sbzone files

---
+++ games/sb/asplode.s
@@ -0,0 +1,92 @@
+; Yet Another HR project
+;
+; by deater (Vince Weaver) <vince@deater.net>
+
+
+.include "zp.inc"
+.include "hardware.inc"
+
+
+hires_start:
+
+	;===================
+	; set graphics mode
+	;===================
+	jsr	HOME
+
+	bit	HIRES
+	bit	FULLGR
+	bit	SET_GR
+	bit	PAGE1
+
+
+
+	;==========================
+	; Load Title
+	;===========================
+
+load_title:
+	lda	#<title_data
+	sta	ZX0_src
+	lda	#>title_data
+	sta	ZX0_src+1
+
+	lda	#$20
+
+	jsr	full_decomp
+
+	jsr	wait_until_keypress
+
+	;===================
+	; Load graphics
+	;===================
+load_loop:
+
+	;=============================
+
+
+	;==========================
+	; Load Image
+	;===========================
+
+load_image:
+
+	; size in ldsizeh:ldsizel (f1/f0)
+
+	lda	#<comp_data
+	sta	ZX0_src
+	lda	#>comp_data
+	sta	ZX0_src+1
+
+
+	lda	#$20
+
+
+	jsr	full_decomp
+
+	jsr	wait_until_keypress
+
+which_ok:
+	lda	#0
+	sta	WHICH_LOAD
+	rts
+
+
+
+
+wait_until_keypress:
+	lda	KEYPRESS				; 4
+	bpl	wait_until_keypress			; 3
+	bit	KEYRESET	; clear the keyboard buffer
+	rts
+
+
+	.include	"zx02_optim.s"
+
+title_data:
+	.incbin "asplode_graphics/sb_title.hgr.zx02"
+
+comp_data:
+	.incbin "asplode_graphics/sb_zone.hgr.zx02"
+
+
