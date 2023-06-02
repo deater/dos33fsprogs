34401d1dac0979e69930e34003233df895f70482
Vince Weaver <vincent.weaver@maine.edu>
2023-06-02 16:37:01 -0400

    sb: start using big sprites

--- games/sb/asplode.s
+++ games/sb/asplode.s
@@ -1,3 +1,5 @@
+; Strongbad Zone
+;
 ; Yet Another HR project
 ;
 ; by deater (Vince Weaver) <vince@deater.net>
@@ -19,7 +21,13 @@ hires_start:
 	bit	SET_GR
 	bit	PAGE1
 
+	;====================
+	; set up tables
+	;====================
 
+	lda	#$20
+	sta	HGR_PAGE
+	jsr	hgr_make_tables
 
 	;==========================
 	; Load Title
@@ -38,18 +46,14 @@ load_title:
 	jsr	wait_until_keypress
 
 	;===================
-	; Load graphics
+	; setup game
 	;===================
-load_loop:
-
-	;=============================
-
 
 	;==========================
-	; Load Image
+	; Load Background
 	;===========================
 
-load_image:
+load_background:
 
 	; size in ldsizeh:ldsizel (f1/f0)
 
@@ -64,9 +68,88 @@ load_image:
 
 	jsr	full_decomp
 
-	jsr	wait_until_keypress
+	lda	#16
+	sta	STRONGBAD_X
+	sta	PLAYER_X
 
-which_ok:
+	;==========================
+	; main loop
+	;===========================
+main_loop:
+
+	;==========================
+	; draw head
+	;===========================
+
+	lda	#<head_sprite
+	sta	INL
+	lda	#>head_sprite
+	sta	INH
+	lda	STRONGBAD_X
+	sta	SPRITE_X
+	lda	#36
+	sta	SPRITE_Y
+	jsr	hgr_draw_sprite
+
+	;==========================
+	; draw player
+	;===========================
+
+	lda	#<player_sprite
+	sta	INL
+	lda	#>player_sprite
+	sta	INH
+	lda	PLAYER_X
+	sta	SPRITE_X
+	lda	#138
+	sta	SPRITE_Y
+	jsr	hgr_draw_sprite
+
+check_keypress:
+	lda     KEYPRESS
+	bpl	check_keypress
+	bit     KEYRESET		; clear the keyboard strobe
+
+	; clear high bit
+	and	#$7f
+
+	; FIXME: adjust for lowercase too
+	cmp	#'Q'
+	beq	done_game
+
+	cmp	#27		; escape
+	beq	done_game
+
+	cmp	#'A'		; shield left
+	cmp	#'S'		; shield center
+	cmp	#'D'		; shield right
+
+	cmp	#8		; left
+	beq	move_left
+
+        cmp	#$15
+        beq	move_right	; right
+
+
+done_keyboard_check:
+	jmp	check_keypress
+
+move_left:
+	dec	PLAYER_X
+	jmp	main_loop
+
+move_right:
+	inc	PLAYER_X
+	jmp	main_loop
+
+
+
+
+	;==========================
+	; done game
+	;==========================
+
+done_game:
 	lda	#0
 	sta	WHICH_LOAD
 	rts
@@ -80,8 +163,11 @@ wait_until_keypress:
 	bit	KEYRESET	; clear the keyboard buffer
 	rts
 
-
+	.include	"hgr_tables.s"
 	.include	"zx02_optim.s"
+	.include	"hgr_sprite_big.s"
+
+	.include	"asplode_graphics/sb_sprites.inc"
 
 title_data:
 	.incbin "asplode_graphics/sb_title.hgr.zx02"
