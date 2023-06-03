6104eb40ba9ee372c02488ab686b9f855b7e7449
Vince Weaver <vince@deater.net>
2023-06-02 23:46:25 -0400

    sb: improve color cycle

--- games/sb/cycle_colors.s
+++ games/sb/cycle_colors.s
@@ -6,13 +6,13 @@ cycle_colors:
 	lda	FRAME
 	lsr
 	lsr
-	lsr
+;	lsr
 	and	#$3
 	tax
 
-	lda	color_opcodes,X
-	sta	color_change1_smc
-	sta	color_change2_smc
+;	lda	color_opcodes,X
+;	sta	color_change1_smc
+;	sta	color_change2_smc
 
 	lda	color_mask_odd,X
 	sta	color_change1_smc+1
@@ -21,7 +21,7 @@ cycle_colors:
 	sta	color_change2_smc+1
 
 
-	ldx	#50
+	ldx	#77
 color_loop:
 
 	lda	hposn_high,X
@@ -37,6 +37,14 @@ color_loop:
 
 color_inner_loop:
 
+	cpx	#59
+	bcc	no_were_good
+	cpy	#10
+	bcc	no_were_good
+	cpy	#24
+	bcc	skip_area
+
+no_were_good:
 
 	lda	(INL),Y
 color_change1_smc:
@@ -48,6 +56,8 @@ color_change1_smc:
 color_change2_smc:
 	and	#$55
 	sta	(OUTL),Y
+
+skip_area:
 	dey
 
 
@@ -60,10 +70,16 @@ color_change2_smc:
 	rts
 
 
+; green = 1   (10)	and $29/$55/$2A
+; purple= 2   (01)	and $29/$2A/$55
+; white1= 3             and $29/$7f/$7f
+; orange= 5   (10)	and $29/$D5/$AA
+; blue=   6   (01)	and $29/$AA/$D5
+; white2= 7             and $29/$FF/$FF
 
-color_opcodes:
-	.byte	$29,$29,$09,$29		; and = $29  ora=$09
+;color_opcodes:
+;	.byte	$29,$29,$29,$29		; and = $29  ora=$09
 color_mask_odd:
-	.byte	$AA,$7f,$00,$D5
+	.byte	$55,$D5,$FF,$7F
 color_mask_even:
-	.byte	$D5,$7f,$00,$AA
+	.byte	$2A,$AA,$FF,$7F
