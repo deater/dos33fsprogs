f1cf8122b9a21ea48afae5b329dbc2189dcb1abc
Vince Weaver <vincent.weaver@maine.edu>
2023-07-31 14:16:49 -0400

    text: update program to dump a 1k file to the text screen

--- utils/gr-sim/text/text.c
+++ utils/gr-sim/text/text.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "gr-sim.h"
 
@@ -8,6 +9,8 @@ int main(int argc, char **argv) {
 
 	int ch,i;
 	char output[BUFSIZ];
+	unsigned char data[1024];
+	FILE *fff;
 
 	grsim_init();
 
@@ -17,15 +20,29 @@ int main(int argc, char **argv) {
 
 	text();
 
-	basic_htab(10);
 
-	basic_vtab(10);
+	if (argc<2) {
 
-	basic_print("HELLO WORLD!\r\r");
+		basic_htab(10);
 
-	for(i=0;i<128;i++) {
-		sprintf(output,"%c",i);
-		basic_print(output);
+		basic_vtab(10);
+
+		basic_print("HELLO WORLD!\r\r");
+
+
+		for(i=0;i<128;i++) {
+			sprintf(output,"%c",i);
+			basic_print(output);
+		}
+	}
+	else {
+		fff=fopen(argv[1],"r");
+		if (fff==NULL) {
+			fprintf(stderr,"Error opening %s\n",argv[1]);
+			exit(1);
+		}
+		fread(data,1024,1,fff);
+		memcpy(ram+1024,data,1024);
 	}
 
 	while(1) {
