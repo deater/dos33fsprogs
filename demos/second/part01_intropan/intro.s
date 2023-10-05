1fabebdd3a5f598c597b3a6934e0446b0109ab2f
Vince Weaver <vincent.weaver@maine.edu>
2023-10-05 16:11:26 -0400

    second: hook up more graphics

--- demos/second/part01_intropan/intro.s
+++ demos/second/part01_intropan/intro.s
@@ -22,6 +22,40 @@ load_loop:
 	bit	FULLGR
 	bit	PAGE1
 
+	; left logo
+
+	lda	#<intro_left_data
+	sta	zx_src_l+1
+
+	lda	#>intro_left_data
+	sta	zx_src_h+1
+
+	lda	#$20
+
+	jsr	zx02_full_decomp
+
+	jsr	wait_until_keypress
+
+
+	; right logo
+
+	lda	#<intro_right_data
+	sta	zx_src_l+1
+
+	lda	#>intro_right_data
+	sta	zx_src_h+1
+
+	lda	#$20
+
+	jsr	zx02_full_decomp
+
+	jsr	wait_until_keypress
+
+
+
+
+	; fc logo
+
 	lda	#<fc_sr_logo_data
 	sta	zx_src_l+1
 
@@ -34,11 +68,17 @@ load_loop:
 
 	jsr	wait_until_keypress
 
+blah:
+	jmp	blah
 
 .align $100
 	.include	"../wait_keypress.s"
 	.include	"../zx02_optim.s"
 
+intro_left_data:
+	.incbin "graphics/igl.hgr.zx02"
+intro_right_data:
+	.incbin "graphics/igr.hgr.zx02"
 fc_sr_logo_data:
 	.incbin "graphics/fc_sr_logo.hgr.zx02"
 
