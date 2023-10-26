8f72d2d4a91997e9944e44a1705889bda70c533b
Vince Weaver <vince@deater.net>
2023-10-26 15:16:05 -0400

    second: update credits

--- demos/second/part20_credits/credits.s
+++ demos/second/part20_credits/credits.s
@@ -72,10 +72,12 @@ credits_logo_outer_outer:
 	tya
 	pha
 
-        lda     #<credit_message
-        ldy     #>credit_message
+	ldx	COUNT
 
-	jsr	draw_font_1x10
+        lda     credits_list_low,X
+        ldy     credits_list_high,X
+
+	jsr	draw_font_1x10_multiple
 
 	pla
 	tay
@@ -96,14 +98,14 @@ credits_logo_outer:
 
 	; setup output pointer
 
-	lda	hposn_low+32,X			; adjust X
+	lda	hposn_low+16,X			; adjust X
 	clc
 	adc	#15				; center on screen
 	sta	OUTL
 
 	; setup high
 
-	lda	hposn_high+32,X
+	lda	hposn_high+16,X
 	sta	OUTH
 
 
@@ -157,8 +159,11 @@ credits_logo_inner:
 	lda	#0
 skip_summary2:
 	cmp	#21
-	bne	credits_logo_outer_outer
+	beq	done_credits_logo
 
+	jmp	credits_logo_outer_outer
+
+done_credits_logo:
 
 	;=======================
 	; scroll job
@@ -249,8 +254,152 @@ summary1_data:
 summary2_data:
 	.incbin "graphics/summary2_invert.hgr.zx02"
 
-credit_message:
-	.byte 14,100,"Deater / DsR",0
+
+credits_list_low:
+	.byte <credit_message1,<credit_message2,<credit_message3
+	.byte <credit_message4,<credit_message5,<credit_message6
+	.byte <credit_message7,<credit_message8,<credit_message9
+	.byte <credit_message10,<credit_message11,<credit_message12
+	.byte <credit_message13,<credit_message14,<credit_message15
+	.byte <credit_message16,<credit_message17,<credit_message18
+	.byte <credit_message19,<credit_message20,<credit_message21
+
+credits_list_high:
+	.byte >credit_message1,>credit_message2,>credit_message3
+	.byte >credit_message4,>credit_message5,>credit_message6
+	.byte >credit_message7,>credit_message8,>credit_message9
+	.byte >credit_message10,>credit_message11,>credit_message12
+	.byte >credit_message13,>credit_message14,>credit_message15
+	.byte >credit_message16,>credit_message17,>credit_message18
+	.byte >credit_message19,>credit_message20,>credit_message21
+
+
+
+
+credit_message1:	; intro
+credit_message2:	; intro ships
+credit_message3:	; intro explosion
+	.byte 11, 96,"GRAPHICS -- MARVEL",13
+	.byte 14,108,"A2 -- DEATER",13
+	.byte 13,128,"MUSIC - SKAVEN",13
+	.byte 14,140,"AY - DEATER",13
+	.byte 13,160,"CODE -- DEATER",0
+;credit_message2:	; intro ships
+;	.byte 11, 96,"GRAPHICS -- MARVEL",13
+;	.byte 14,108,"A2 -- DEATER",13
+;	.byte 13,128,"MUSIC - SKAVEN",13
+;	.byte 14,140,"AY - DEATER",13
+;	.byte 13,160,"CODE -- DEATER",0
+;credit_message3:	; intro explosion
+;	.byte 11, 96,"GRAPHICS -- MARVEL",13
+;	.byte 14,108,"A2 -- DEATER",13
+;	.byte 13,128,"MUSIC - SKAVEN",13
+;	.byte 14,140,"AY - DEATER",13
+;	.byte 13,160,"CODE -- DEATER",0
+credit_message4:	; logo
+	.byte 12, 96,"GRAPHICS - PIXEL",13
+	.byte 14,108,"A2 -- DEATER",0
+credit_message5:	; chessboard
+credit_message9:	; ape
+credit_message10:	; leaves
+credit_message16:	; transmission
+	.byte 12, 96,"GRAPHICS -- PIXEL",13
+	.byte 14,108,"A2 -- DEATER",13
+	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+	.byte 15,140,"AY -- Z00M",13
+	.byte 13,160,"CODE -- DEATER",0
+credit_message6:	; tunnel
+credit_message7:	; circles
+credit_message8:	; interference
+credit_message13:	; plasma
+credit_message14:	; cube
+credit_message15:	; balls
+credit_message17:	; voxel
+	.byte  9, 96,"MUSIC -- PURPLE MOTION",13
+	.byte 15,108,"AY -- Z00M",13
+	.byte 13,128,"CODE -- DEATER",0
+;credit_message7:	; circles
+;	.byte  9, 96,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,108,"AY -- Z00M",13
+;	.byte 13,128,"CODE -- DEATER",0
+;credit_message8:	; interference
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+;credit_message9:	; ape
+;	.byte 12, 96,"GRAPHICS -- PIXEL",13
+;	.byte 14,108,"A2 -- DEATER",13
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+;credit_message10:	; leaves
+;	.byte 12, 96,"GRAPHICS -- PIXEL",13
+;	.byte 14,108,"A2 -- DEATER",13
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+credit_message11:	; lens
+credit_message12:	; roto
+	.byte  7, 96,"GRAPHICS -- PIXEL / SKAVEN",13
+	.byte 14,108,"A2 -- DEATER",13
+	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+	.byte 15,140,"AY -- Z00M",13
+	.byte 13,160,"CODE -- DEATER",0
+;credit_message12:	; roto
+;	.byte 12, 96,"GRAPHICS -- PIXEL",13	; / SKAVEN
+;	.byte 14,108,"A2 -- DEATER",13
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+;credit_message13:	; plasma
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+;credit_message14:	; cube
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+;credit_message15:	; balls
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+;credit_message16:	; transmission
+;	.byte 12, 96,"GRAPHICS -- PIXEL",13
+;	.byte 14,108,"A2 -- DEATER",13
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+;credit_message17:	; voxel
+;	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+;	.byte 15,140,"AY -- Z00M",13
+;	.byte 13,160,"CODE -- DEATER",0
+credit_message18:	; polar
+	.byte 11, 96,"GRAPHICS -- MARVEL",13
+	.byte 14,108,"A2 -- DEATER",13
+	.byte  9,128,"MUSIC -- PURPLE MOTION",13
+	.byte 15,140,"AY -- Z00M",13
+	.byte 13,160,"CODE -- DEATER",0
+credit_message19:	; 3D
+	.byte 14, 96,"WORLD - TRUG",13
+	.byte 14,108,"A2 -- DEATER",13
+	.byte 13,128,"MUSIC - SKAVEN",13
+	.byte 14,140,"AY -- DEATER",13
+	.byte 13,160,"CODE -- DEATER",0
+credit_message20:	; NUTS
+credit_message21:	; CREDITS
+	.byte 12, 96,"GRAPHICS -- PIXEL",13
+	.byte 14,108,"A2 -- DEATER",13
+	.byte 13,128,"MUSIC - SKAVEN",13
+	.byte 14,140,"AY -- DEATER",13
+	.byte 13,160,"CODE -- DEATER",0
+;credit_message21:	; CREDITS
+;	.byte 12, 96,"GRAPHICS -- PIXEL",13
+;	.byte 14,108,"A2 -- DEATER",13
+;	.byte 13,128,"MUSIC - SKAVEN",13
+;	.byte 14,140,"AY -- DEATER",13
+;	.byte 13,160,"CODE -- DEATER",0
+
+
 
 apple_message:
 	.byte "Apple ][ Forever",0
