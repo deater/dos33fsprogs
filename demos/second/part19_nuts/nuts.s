0e15f04fda5d4078bcd2ced5ba8017cb3b5cecfb
Vince Weaver <vince@deater.net>
2023-10-28 01:28:48 -0400

    second: work on animation

--- demos/second/part19_nuts/nuts.s
+++ demos/second/part19_nuts/nuts.s
@@ -31,9 +31,9 @@ load_loop:
 
 	lda	#0
 	jsr	hgr_page1_clearscreen
+	jsr	hgr_page2_clearscreen
 
-;	jsr	hgr_make_tables
-
+	bit	PAGE2
 
 	; fc logo
 
@@ -41,10 +41,62 @@ load_loop:
 	sta	zx_src_l+1
 	lda	#>fc_iipix_data
 	sta	zx_src_h+1
-	lda	#$20
+	lda	#$60
 	jsr	zx02_full_decomp
-	jsr	wait_until_keypress
 
+
+	lda	#0
+	sta	COUNT
+	sta	DRAW_PAGE
+
+ship_sprite_loop:
+
+	lda	#$60
+	jsr	hgr_copy
+
+	ldx	COUNT
+
+	lda	ship_coords_x,X
+	cmp	#$FF
+	beq	done_ship_sprite_loop
+
+	sta	SPRITE_X
+	lda	ship_coords_y,X
+	sta     SPRITE_Y
+
+	lda	ship_size,X
+	tax
+
+	lda	ship_lookup_low,X
+	sta     INL
+	lda	ship_lookup_high,X
+	sta	INH
+
+	jsr	hgr_draw_sprite_big
+
+	lda	DRAW_PAGE
+	beq	ship_sprite_flip
+
+	lda	#0
+	sta	DRAW_PAGE
+	bit	PAGE2
+	jmp	done_ship_sprite_flip
+
+ship_sprite_flip:
+	lda	#$20
+	sta	DRAW_PAGE
+	bit	PAGE1
+
+done_ship_sprite_flip:
+
+;	jsr	wait_until_keypress
+
+	inc	COUNT
+	bne	ship_sprite_loop		; bra
+
+done_ship_sprite_loop:
+
+	bit	PAGE1
 
 	; nuts4 logo
 
@@ -65,10 +117,36 @@ nuts_done:
 	.include	"../zx02_optim.s"
 ;	.include	"../hgr_table.s"
 	.include	"../hgr_clear_screen.s"
-
+	.include	"hgr_sprite_big.s"
+	.include	"hgr_copy_fast.s"
 
 fc_iipix_data:
 	.incbin "graphics/fc_iipix.hgr.zx02"
 
 nuts4_data:
 	.incbin "graphics/nuts4.hgr.zx02"
+
+
+	.include "graphics/ship_sprites.inc"
+
+ship_coords_x:
+	.byte	28, 28, 28, 27, 26, 25, 24
+	.byte   23, 22, 21, 20, 19, 18
+	.byte	16, 14, 12, 10,  7,  3, 0,$FF
+
+ship_coords_y:
+	.byte	91, 97 ,103,109,111,112,112
+	.byte   108,113,110,108,107,106
+	.byte	102,101, 95, 85, 77, 68,59
+
+ship_size:
+	.byte	0,0,0,0,0,0,0
+	.byte	1,1,1,1,1,1
+	.byte	2,2,2,2,2,2,2
+
+ship_lookup_low:
+	.byte <small_ship_sprite,<medium_ship_sprite,<large_ship_sprite
+
+ship_lookup_high:
+	.byte >small_ship_sprite,>medium_ship_sprite,>large_ship_sprite
+
--- demos/second/start2.s
+++ demos/second/start2.s
@@ -136,16 +136,16 @@ load_loop:
 	;=======================
 	; run NUTS
 	;============================================
-	; copy NUTS from AUX $2000 to MAIN $6000
+	; copy NUTS from AUX $2000 to MAIN $8000
 
 	lda	#$20		; AUX src $2000
-	ldy	#$60		; MAIN dest $6000
+	ldy	#$80		; MAIN dest $8000
 	ldx	#32		; 32 pages
 	jsr	copy_aux_main
 
 	; run nuts
 
-	jsr	$6000
+	jsr	$8000
 
 
 	;=======================
