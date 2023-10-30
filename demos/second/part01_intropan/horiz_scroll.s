2beb1d66c0480c0114a52079ac332a5d04b08f92
Vince Weaver <vincent.weaver@maine.edu>
2023-10-30 16:02:34 -0400

    second: update horizontal scroll

--- demos/second/part01_intropan/horiz_scroll.s
+++ demos/second/part01_intropan/horiz_scroll.s
@@ -16,37 +16,56 @@ pan_outer_loop:
 	lda	hposn_high,X
 	sta	pil_smc1+2
 	sta	pil_smc2+2
-	sta	pil_smc4+2
-	eor	#$60
 	sta	pil_smc3+2
 
-	lda	hposn_low,X
-	sta	pil_smc2+1
-	sta	pil_smc4+1
+;	sta	pil_smc4+2
+;	eor	#$60
 
+
+	lda	hposn_low,X
 	sta	pil_smc1+1
-	inc	pil_smc1+1
-	clc
-	adc	COUNT
+	sta	pil_smc2+1
 	sta	pil_smc3+1
+	inc	pil_smc3+1
+
+;	clc
+;	adc	COUNT
+;	sta	pil_smc3+1
+;	sta	pil_smc4+1
+	stx	XSAVE
 
 
 	ldy	#0
 pan_inner_loop:
 
-pil_smc1:
-	lda	$2000+1,Y
-pil_smc2:
-	sta	$2000,Y
+	; original: 36*39 = ??
+	; updated:  34*39
 
-	iny
-	cpy	#39
-	bne	pan_inner_loop
+pil_smc1:
+	ldx	$2000,Y					; 4+
+	lda	left_lookup_main,X			; 4+
+	sta	TEMPY					; 3
 
 pil_smc3:
-	lda	$4000
-pil_smc4:
-	sta	$2000,Y
+	ldx	$2000+1,Y				; 4+
+	lda	left_lookup_next,X			; 4+
+	ora	TEMPY					; 3
+
+pil_smc2:
+	sta	$2000,Y					; 5
+
+	iny						; 2
+	cpy	#39					; 2
+	bne	pan_inner_loop				; 2/3
+
+; leftover
+
+;pil_smc3:
+;	lda	$4000
+;pil_smc4:
+;	sta	$2000,Y
+
+	ldx	XSAVE
 
 	dex
 	cpx	#$ff
@@ -56,8 +75,10 @@ pil_smc4:
 
 	inc	COUNT
 	lda	COUNT
-	cmp	#39
+	cmp	#139
 
 	bne	pan_outer_outer_loop
 
 	rts
+
+.include "scroll_tables.s"
