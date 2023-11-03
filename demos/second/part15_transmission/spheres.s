ba83a62c365cefab409ea778cef75ce19e8ba722
Vince Weaver <vince@deater.net>
2023-11-02 20:09:10 -0400

    second: sword transmission

--- demos/second/part15_transmission/spheres.s
+++ demos/second/part15_transmission/spheres.s
@@ -85,11 +85,48 @@ spheres_start:
 	cli
 
 
+	;=============================
+	; wait a bit
+	;=============================
+	; "10 seconds to transmission"
+	; do we wait 10s?
+
+	lda	#4
+	jsr	wait_seconds
 
 
-;	lda	#0
-;	sta	COUNT
-;	sta	DRAW_PAGE
+	lda	#0
+	sta	XMISSION_COUNT
+
+	;==========================
+	; transmit sword
+	;==========================
+
+	lda	#<long_sword1
+	sta	BASE_SPRITEL
+	lda	#>long_sword1
+	sta	BASE_SPRITEH
+
+move_sword_loop:
+        jsr     gr_copy_to_current
+
+	jsr	draw_sword
+
+	jsr	page_flip
+
+	lda	#25
+	jsr	wait_ticks
+
+	lda	KEYPRESS
+	bmi	done_sword_loop
+
+	inc	XMISSION_COUNT
+	lda	XMISSION_COUNT
+	cmp	#60
+	bne	move_sword_loop
+
+done_sword_loop:
+	bit	KEYRESET
 
 
 spheres_loop:
@@ -97,17 +134,101 @@ spheres_loop:
 	jsr	wait_for_pattern
 	bcc	spheres_loop
 
+
 spheres_done:
 	rts
 
 
-	.include	"../wait_keypress.s"
-;	.include	"../zx02_optim.s"
-;	.include	"../hgr_table.s"
-;	.include	"../hgr_clear_screen.s"
-;	.include	"../hgr_copy_fast.s"
 
-	.include	"../gr_pageflip.s"
+
+	;==========================
+	; draw skewed sword
+	;==========================
+draw_sword:
+
+	lda	BASE_SPRITEL
+	sta	CURRENT_SPRITEL
+	lda	BASE_SPRITEH
+	sta	CURRENT_SPRITEH		; copy start for sprite
+
+	ldx	#0
+
+	lda	#3
+	sta	COUNT
+
+sword_loop:
+	lda	gr_offsets,X
+	clc
+	adc	COUNT
+	sta	GBASL
+
+	lda	gr_offsets+1,X
+	clc
+	adc	DRAW_PAGE
+	sta	GBASH
+
+	ldy	#5
+sword_inner_loop:
+	lda	(CURRENT_SPRITEL),Y
+	beq	skip_pixel
+	sta	(GBASL),Y
+skip_pixel:
+	dey
+	bpl	sword_inner_loop
+
+	inc	COUNT
+
+	clc
+	lda	CURRENT_SPRITEL
+	adc	#6
+	sta	CURRENT_SPRITEL
+	lda	#0
+	adc	CURRENT_SPRITEH
+	sta	CURRENT_SPRITEH
+
+	inx
+	inx
+	cpx	#28
+	bne	sword_loop
+
+	; move to next input line
+
+	clc
+	lda	BASE_SPRITEL
+	adc	#6
+	sta	BASE_SPRITEL
+	lda	#0
+	adc	BASE_SPRITEH
+	sta	BASE_SPRITEH
+
+	rts
+
+
+        ;==========
+        ; page_flip
+        ;==========
+
+page_flip:
+        lda     DRAW_PAGE                                               ; 3
+        beq     page_flip_show_1                                        ; 2nt/3
+page_flip_show_0:
+	; show page2, draw page1
+        bit     PAGE2                                                   ; 4
+        lda     #0                                                      ; 2
+        sta	DRAW_PAGE
+        rts                                                             ; 6
+
+page_flip_show_1:
+	; show page1, draw page2
+	bit	PAGE1                                                   ; 4
+	lda	#4
+	sta	DRAW_PAGE
+	rts
+
+
+	.include	"../wait_keypress.s"
+
+;	.include	"../gr_pageflip.s"
 	.include	"../gr_copy.s"
 
 	.include	"../gr_offsets.s"
@@ -121,3 +242,5 @@ spheres_data:
 
 transmission_data:
 	.incbin "audio/transmission.btc.zx02"
+
+	.include "graphics/sword_sprite.inc"
