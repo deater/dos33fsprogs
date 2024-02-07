8a504fb5c8037c5e57e7fcf9ce2c65c6e617f4a4
Vince Weaver <vince@deater.net>
2024-02-07 00:30:52 -0500

    lovebyte2024: work on plasma

--- demos/lovebyte2024/plasma_mask_1k/plasma_mask.s
+++ demos/lovebyte2024/plasma_mask_1k/plasma_mask.s
@@ -1,26 +1,14 @@
-; Plasma Tiny
+; Plasma Mask
 
-; originally based on Plasmagoria code by French Touch
+; for Lovebyte 2024
 
-; trying to see how small I can get it
+; by Vince `deater` Weaver / Dsr
 
+; originally based on Plasmagoria (GPL3) code by French Touch
 
-; note can use $F000 (or similar) for color lookup to get passable
-;	effect on fewer bytes
-
-; 347 bytes -- initial with FAC
-; 343 bytes -- optimize init
-; 340 bytes -- move Table1 + Table2 to zero page
-; 331 bytes -- init not necessary
-; 319 bytes -- inline precalc + display + init lores colors
-; 316 bytes -- fallthrough in make_tiny
 
 .include "hardware.inc"
 .include "zp.inc"
-
-;Table1	=	$8000
-;Table2	=	$8000+64
-
 
 lores_colors_fine=$8100
 
@@ -28,12 +16,25 @@ lores_colors_fine=$8100
 	; start of code
 	;======================================
 
-plasma_tiny:
+plasma_mask:
+
 	jsr	HGR		; have table gen appear on hgr page1
 	bit	FULLGR
 
 
-
+	ldx	#0
+load_graphics_loop:
+	lda	graphics_src_l,X
+	sta	zx_src_l+1
+	lda	graphics_src_h,X
+	sta	zx_src_h+1
+	lda	graphics_loc,X
+	stx	XSAVE
+	jsr	zx02_full_decomp
+	ldx	XSAVE
+	inx
+	cpx	#4
+	bne	load_graphics_loop
 
 	;=================
         ; init music
@@ -42,7 +43,7 @@ plasma_tiny:
         sta     FRAME
         sta     WHICH_TRACK
 
-;===================
+	;===================
         ; music Player Setup
 
 tracker_song = peasant_song
@@ -57,7 +58,7 @@ tracker_song = peasant_song
 
 	cli	; start music
 
-	jsr	make_tables
+;	jsr	make_tables
 
 	bit	LORES			; set lo-res
 
@@ -96,19 +97,13 @@ done_init_lores_colors:
 do_plasma:
 	; init
 
-;	lda	#02
-;	ldx	#5
-;init_loop:
-;	sta	COMPT1,X
-;	dex
-;	bne	init_loop
 
 BP3:
 
 ; ============================================================================
 ; Precalculate some values (inlined)
-; ROUTINES PRE CALCUL
 ; ============================================================================
+
 precalc:
 	lda	PARAM1		; self modify various parts
 	sta	pc_off1+1
@@ -151,11 +146,8 @@ pc_off4:
 
 ; ============================================================================
 ; Display Routines
-; ROUTINES AFFICHAGES
 ; ============================================================================
 
-; Display "Normal"
-; AFFICHAGE "NORMAL"
 
 display_normal:
 
@@ -166,6 +158,20 @@ display_line_loop:
 	txa
 	jsr	GBASCALC
 
+	; set up pointer for mask
+
+	ldy	WHICH_TRACK	; CURRENT_EFFECT
+
+	lda	GBASL
+        sta     INL
+	lda	GBASH
+
+	clc
+	adc	graphics_loc,Y
+	sec				; blurgh
+	sbc	#4			;
+        sta     INH
+
 	ldy	#39			; col 0-39
 
 	lda	Table2,X		; setup base sine value for row
@@ -174,6 +180,10 @@ display_col_loop:
 	lda	Table1,Y		; load in column sine value
 display_row_sin_smc:
 	adc	#00			; add in row value
+
+	; MASKING happens HERE
+	and	(INL),Y
+
 	sta	display_lookup_smc+1	; patch in low byte of lookup
 display_lookup_smc:
 	lda	lores_colors_fine	; attention: must be aligned
@@ -197,20 +207,51 @@ display_lookup_smc:
 
 
 
+;lores_colors_lookup = $F000
+
 lores_colors_lookup:
 .byte $00,$88,$55,$99,$ff,$bb,$33,$22,$66,$77,$44,$cc,$ee,$dd,$99,$11
 
 
-.include "make_tables.s"
+;.include "make_tables.s"
+
 
 .include "interrupt_handler.s"
 .include "mockingboard_constants.s"
 
 .include "zx02_optim.s"
 
+
+graphics_src_l:
+	.byte	<dsr_empty,<dsr_small,<dsr_big,<dsr_big2
+graphics_src_h:
+	.byte	>dsr_empty,>dsr_small,>dsr_big,>dsr_big2
+
+graphics_loc:
+	.byte	$A0,$A4,$A8,$AC
+
 ; graphics
 dsr_big:
 .incbin		"graphics/dsr_big.gr.zx02"
+dsr_big2:
+.incbin		"graphics/dsr_big2.gr.zx02"
+dsr_small:
+.incbin		"graphics/dsr_small.gr.zx02"
+dsr_empty:
+.incbin		"graphics/dsr_empty.gr.zx02"
 
 ; music
 .include        "mA2E_2.s"
+
+
+
+;sin1    =       $2000
+;sin2    =       $2100
+;sin3    =       $2200
+save    =       $2300
+
+.align $100
+sin1:
+	.incbin	"precalc/tables"
+sin2=sin1+$100
+sin3=sin1+$200
