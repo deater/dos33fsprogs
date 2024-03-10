6552127256c1b41b6671b6048b34acea4ad166b1
Vince Weaver <vince@deater.net>
2024-03-10 01:26:40 -0500

    duke: autogen sprites

--- games/duke/duke_level1.s
+++ games/duke/duke_level1.s
@@ -224,7 +224,7 @@ done_with_duke:
 	.include	"level_end.s"
 
 	.include	"draw_duke.s"
-	.include	"duke_sprites.inc"
+	.include	"sprites/duke_sprites.inc"
 	.include	"move_duke.s"
 	.include	"handle_laser.s"
 	.include	"draw_tilemap.s"
