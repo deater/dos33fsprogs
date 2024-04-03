c766a3bdffc8cf372319d65fc18672effd92a5d6
Vince Weaver <vince@deater.net>
2024-04-03 00:28:18 -0400

    keen: more work on keen sounds

--- music/redbook_sound/test_keen.s
+++ music/redbook_sound/test_keen.s
@@ -1,19 +1,34 @@
 
-SOUND_OFFSET = $F0
+SOUND_OFFSET	= $F0
+WHICH_SOUND	= $F1
+INL		= $FE
+INH		= $FF
 
-KEYPRESS        =       $C000
-KEYRESET        =       $C010
+KEYPRESS        = $C000
+KEYRESET        = $C010
 
 test_keen:
+	lda	#0
+	sta	WHICH_SOUND
+
+next_sound:
+	ldy	WHICH_SOUND
+	lda	sounds_low,Y
+	sta	INL
+	lda	sounds_high,Y
+	cmp	#$FF
+	beq	test_keen		; reset
+
+	sta	INH
 
 	ldy	#0
 	sty	SOUND_OFFSET
 play_loop:
 	ldy	SOUND_OFFSET
-	lda	LVLDONESND,Y
+	lda	(INL),Y
 	sta	speaker_frequency
 	iny
-	lda	LVLDONESND,Y
+	lda	(INL),Y
 	cmp	#$FF
 	beq	play_done
 
@@ -23,6 +38,10 @@ play_loop:
 
 	sta	speaker_duration
 	iny
+	bne	no_wrap
+	inc	INH
+no_wrap:
+
 	sty	SOUND_OFFSET
 
 	jsr	speaker_tone
@@ -36,10 +55,38 @@ play_done:
 
 	bit	KEYRESET
 
-	jmp	test_keen
+	inc	WHICH_SOUND
+
+	jmp	next_sound
 
 
 
 .include "redbook_sound.s"
-.include "lvldone.inc"
+.include "ck1_sounds.inc"
 
+
+sounds_low:
+	.byte <WLDWALKSND,<WLDBLOCKSND<WLDENTERSND,<KEENWALKSND
+	.byte <KEENBLOKSND,<KEENJUMPSND,<KEENLANDSND,<KEENDIESND
+	.byte <GOTBONUSSND,<GOTITEMSND,<GOTPARTSND,<KEENFIRESND
+	.byte <KEENPOGOSND,<POGOJUMPSND,<LVLDONESND,<GAMEOVERSND
+	.byte <HISCORESND,<TELEPORTSND,<CHUNKSMASH,<GOINDOORSND
+	.byte <BUMPHEADSND,<USEKEYSND,<CANNONFIRE,<SLAMSND
+	.byte <CLICKSND,<CRYSTALSND,<PLUMMETSND,<EXTRAMANSND
+	.byte <YORPBUMPSND,<KEENWLK2SND,<YORPBOPSND,<GETCARDSND
+	.byte <DOOROPENSND,<YORPSCREAM,<GARGSCREAM,<GUNCLICK
+	.byte <SHOTHIT,<TANKFIRE,<vortscream,<keencicle
+	.byte <keensleft,<EARTHPOW,$FF
+
+sounds_high:
+	.byte >WLDWALKSND,>WLDBLOCKSND>WLDENTERSND,>KEENWALKSND
+	.byte >KEENBLOKSND,>KEENJUMPSND,>KEENLANDSND,>KEENDIESND
+	.byte >GOTBONUSSND,>GOTITEMSND,>GOTPARTSND,>KEENFIRESND
+	.byte >KEENPOGOSND,>POGOJUMPSND,>LVLDONESND,>GAMEOVERSND
+	.byte >HISCORESND,>TELEPORTSND,>CHUNKSMASH,>GOINDOORSND
+	.byte >BUMPHEADSND,>USEKEYSND,>CANNONFIRE,>SLAMSND
+	.byte >CLICKSND,>CRYSTALSND,>PLUMMETSND,>EXTRAMANSND
+	.byte >YORPBUMPSND,>KEENWLK2SND,>YORPBOPSND,>GETCARDSND
+	.byte >DOOROPENSND,>YORPSCREAM,>GARGSCREAM,>GUNCLICK
+	.byte >SHOTHIT,>TANKFIRE,>vortscream,>keencicle
+	.byte >keensleft,>EARTHPOW,$FF
