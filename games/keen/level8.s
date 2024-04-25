8fd766a34ab3fc66a4b2dbcc66ca34fd5267700d
Vince Weaver <vince@deater.net>
2024-04-25 00:43:15 -0400

    keen: update level8

--- games/keen/level8.s
+++ games/keen/level8.s
@@ -5,19 +5,19 @@
 ; at $6000
 
 level8_data:
-.byte	116	;MAX_TILE_X 		= 116	; 116 wide
-.byte	20	;MAX_TILE_Y 		= 16	; 20 tall
+.byte	76	;MAX_TILE_X
+.byte	56	;MAX_TILE_Y
 
-.byte	4	;START_KEEN_TILEX	= 4
-.byte	16	;START_KEEN_TILEY	= 16
+.byte	66	;START_KEEN_TILEX
+.byte	52	;START_KEEN_TILEY
 
-.byte	0	;START_TILEMAP_X	= 0
-.byte	8	;START_TILEMAP_Y	= 8
+.byte	55	;START_TILEMAP_X
+.byte	43	;START_TILEMAP_Y
 
-.byte	0	;NUM_ENEMIES		= 8
+.byte	0	;NUM_ENEMIES
 
-.byte	32	;HARDTOP_TILES   	= 32	; start at 32
-.byte	40	;ALLHARD_TILES   	= 40	; start at 40
+.byte	40	;HARDTOP_TILES
+.byte	48	;ALLHARD_TILES
 
 
 .align	$100
@@ -25,32 +25,30 @@ level8_data:
 ; at $6100
 enemy_data:
 
-enemy_data_out:         .byte 1,     0,   0,    0,    0,    0,   0,    0
-enemy_data_exploding:   .byte 0,     0,   0,    0,    0,    0,   0,    0
-enemy_data_type:        .byte YORP,  YORP, YORP, YORP, YORP, YORP,YORP,YORP
-enemy_data_direction:   .byte RIGHT, LEFT, LEFT, RIGHT, LEFT, RIGHT,LEFT,LEFT
-enemy_data_tilex:       .byte 16,    19,   23,   14,   14,   16,  42,  70
-enemy_data_tiley:       .byte 9,     9,    9,    45,   38,   43,  44,  34
-enemy_data_x:           .byte 0,     0,    0,    0,    0,    0,   0,   0
-enemy_data_y:           .byte 0,     0,    0,    0,    0,    0,   0,   0
-enemy_data_state:       .byte 0,     0,    0,    0,    0,    0,   0,   0
-enemy_data_count:       .byte 8,     8,    8,    8,    8,    8,   8,   8
+.byte	$0
 
-; FIXME: this is not correct
-; enemy1: yorp @16,9, right
-; enemy2: yorp @19,9  left
-; enemy3: yorp @23,9  left
-; enemy4: yorp @14,45 right
-; enemy5: yorp @14,38 left
-; enemy6: yorp @16,43 right
-; enemy7: yorp @42,44 left
-; enemy8: yorp @70,34 left
+; enemy0: robot @22,5 left
+; enemy1: robot @9,5 right
+; enemy1: robot @70,22 left
+; enemy1: robot @56,52 left
 
-; enemy9: garg @50,31 right
-; enemy10: garg @63,45 right
+; enemy: butler robot @9,9 right
+; enemy: butler robot @23,12 right
+; enemy: butler robot @21,24 right
+; enemy: butler robot @7,18 left
+; enemy: butler robot @6,30 right
+; enemy: butler robot @67,44 left
+; enemy: butler robot @69,44 left
 
-; enemy?: butler robot@27,7 left
-; enemy?: vorticon  @53,8 right
+; enemy?: vorticon  @56,13 left
+
+; enemy?: cannon @64,22,left
+; enemy?: cannon @55,22,right
+; enemy?: cannon @18,47,right
+
+; enemy9: garg @54,35 right
+; enemy10: garg @37,42 right
+
 
 .align	$100
 
