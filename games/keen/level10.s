f60cb2c1d696e1fb30eddb253090907bef830e11
Vince Weaver <vince@deater.net>
2024-04-26 00:25:32 -0400

    keen: update level10

--- games/keen/level10.s
+++ games/keen/level10.s
@@ -3,19 +3,19 @@
 ; at $6000
 
 level10_data:
-.byte	20	;MAX_TILE_X 		= 116	; 116 wide
-.byte	26	;MAX_TILE_Y 		= 16	; 16 tall
+.byte	50	;MAX_TILE_X
+.byte	36	;MAX_TILE_Y
 
-.byte	1	;START_KEEN_TILEX	= 1
-.byte	13	;START_KEEN_TILEY	= 13
+.byte	1	;START_KEEN_TILEX
+.byte	32	;START_KEEN_TILEY
 
-.byte	0	;START_TILEMAP_X	= 0
-.byte	5	;START_TILEMAP_Y	= 5
+.byte	0	;START_TILEMAP_X
+.byte	24	;START_TILEMAP_Y
 
-.byte	0	;NUM_ENEMIES		= 8
+.byte	0	;NUM_ENEMIES
 
-.byte	32	;HARDTOP_TILES   	= 32	; start at 32
-.byte	40	;ALLHARD_TILES   	= 40	; start at 40
+.byte	40	;HARDTOP_TILES
+.byte	48	;ALLHARD_TILES
 
 
 .align	$100
@@ -24,14 +24,19 @@ level10_data:
 enemy_data:
 .byte $0
 
+; big robot@10,6 left
+; butler robot @8,13 left
+; garg @15,19 right
+; garg @ 17,19 right
+
 .align	$100
 
 ; at $6200
 oracle_message:
 	;      012345678901234567890123456789012345678
-	.byte 2,21,"YOU HEAR IN YOUR MIND:",0
-	.byte 2,22,"IT IS TOO BAD THAT YOU CANNOT READ",0
-	.byte 2,23,"THE STANDARD GALACTIC ALPHABET, HUMAN",0
+	.byte 2,21,"YOU SEE THESE WORDS IN YOUR HEAD:",0
+	.byte 2,22,"YOU WILL NEED A RAYGUN IN THE END,",0
+	.byte 2,23,"BUT NOT TO SHOOT THE VORTICON...",0
 
 .align $100
 
