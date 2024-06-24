4f8c9fd2cca85b88e90b8dae5791f8e327e589ef
Vince Weaver <vince@deater.net>
2024-06-23 23:14:38 -0400

    riven: add stairs

---
+++ games/riven_hgr/disk41_files/level_stairs.s
@@ -0,0 +1,107 @@
+; Riven -- Jungle Island -- Stairs
+
+; by deater (Vince Weaver) <vince@deater.net>
+
+; Zero Page
+	.include "../zp.inc"
+	.include "../hardware.inc"
+	.include "../common_defines.inc"
+	.include "../qload.inc"
+	.include "disk41_defines.inc"
+
+riven_jungle_tunnel2:
+
+	;===================
+	; init screen
+	;===================
+
+;	jsr	TEXT
+;	jsr	HOME
+	bit	KEYRESET
+
+	bit	SET_GR
+	bit	PAGE1
+	bit	HIRES
+	bit	FULLGR
+
+	;========================
+	; set up location
+	;========================
+
+	lda	#<locations
+	sta	LOCATIONS_L
+	lda	#>locations
+	sta	LOCATIONS_H
+
+	lda	#0
+	sta	DRAW_PAGE
+	sta	LEVEL_OVER
+
+	lda	#0
+	sta	JOYSTICK_ENABLED
+	sta	UPDATE_POINTER
+
+	lda	#1
+	sta	CURSOR_VISIBLE
+
+	lda	#20
+	sta	CURSOR_X
+	sta	CURSOR_Y
+
+
+
+
+	;===================================
+	; init
+	;===================================
+
+	jsr	change_location
+
+	jsr     save_bg_14x14           ; save old bg
+
+game_loop:
+
+	;===================================
+	; draw pointer
+	;===================================
+
+	jsr	draw_pointer
+
+	;===================================
+	; handle keypress/joystick
+	;===================================
+
+	jsr	handle_keypress
+
+	;===================================
+	; increment frame count
+	;===================================
+
+	inc	FRAMEL
+	bne	frame_no_oflo
+
+	inc	FRAMEH
+frame_no_oflo:
+
+	;====================================
+	; check level over
+	;====================================
+
+	lda	LEVEL_OVER
+	bne	really_exit
+
+	jmp	game_loop
+
+really_exit:
+
+	rts
+
+
+	;==========================
+	; includes
+	;==========================
+
+
+.include "graphics_stairs/stairs_graphics.inc"
+
+.include "leveldata_stairs.inc"
