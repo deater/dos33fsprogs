091893747a62b072341d283c6af8a97867bf089b
Vince Weaver <vince@deater.net>
2024-06-25 09:32:34 -0400

    riven: more missing files

---
+++ games/riven_hgr/disk41_files/disk41.s
@@ -0,0 +1,58 @@
+.include "../zp.inc"
+
+which_disk:
+	.byte	$41		; BCD
+
+load_address_array:
+	.byte	$40,$40,$40,$40	; TITLE, TUNNEL, STAIRS, COVE
+	.byte	$40,$20,$00,$00 ; STAIRS2,MOVIE_COVE
+
+track_array:
+	.byte	 1, 2,10,17	; TITLE, TUNNEL, STAIRS, COVE
+	.byte	24, 31, 0, 0	; STAIRS2,MOVIE_COVE
+
+sector_array:
+	.byte	9, 0, 0, 0	; TITLE, TUNNEL, STAIRS, COVE
+	.byte	0,0,0,0		; STAIRS2,MOVIE_COVE
+
+length_array:
+	.byte	8, 128,128,116	; TITLE, TUNNEL, STAIRS, COVE
+	.byte	112,48,0,0	; STAIRS2,MOVIE_COVE
+
+disk_exit_disk:	; note: BCD (yes I'm lazy)
+	.byte	$40		; TUNNEL (DISK40)
+	.byte	$43		; CART   (DISK43)
+	.byte	$00
+	.byte	$00
+
+disk_exit_dni_h:
+	.byte	$01	; 40 = 25*1 + 3*5 + 0
+	.byte	$01	; 43 = 24*1 + 3*5 + 3
+	.byte	$00
+	.byte	$00
+
+disk_exit_dni_l:
+	.byte	$30
+	.byte	$33
+	.byte	$00
+	.byte	$00
+
+	; load_tunnel / tunnel_4 / n
+	; load bridge / mid_bridge / w
+disk_exit_load:
+	.byte	3			; LOAD_TUNNEL
+	.byte	2			; LOAD_BRIDGE
+	.byte	0
+	.byte	0
+
+disk_exit_level:
+	.byte	$03			; TUNNEL_4
+	.byte	$00			; MID_BRIDGE
+	.byte	$00
+	.byte	$00
+
+disk_exit_direction:
+	.byte	DIRECTION_N		;
+	.byte	DIRECTION_W
+	.byte	$00
+	.byte	$00
