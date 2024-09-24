467b51644d661172fc35ce8e8ad634fe8b979136
Vince Weaver <vincent.weaver@maine.edu>
2024-09-24 13:07:56 -0400

    peasant: spread the files on disk3 around a bit

--- games/peasant/qload.s
+++ games/peasant/qload.s
@@ -29,9 +29,11 @@ qload_start:
 which_disk_array:
 	.byte 1,1,1,1		; VID_LOGO, TITLE, INTRO. COPY_CHECK
 	.byte 2,2,2,2		; PEASANT1, PEASANT2, PEASANT3, PEASANT4
-	.byte 3,3,1,3		; TROGDOR, ENDING, MUSIC, CLIFF
+	.byte 3,3,1,3		; TROGDOR, ENDING, MUSIC, CLIFF_BASE
 	.byte 1,1,1,3		; GAME_OVER, INVENTORY, PARSE_INPUT, INN
-	.byte 3,3,3		; INSIDE, ARCHERY, MAP
+	.byte 3,3,3,3		; INSIDE, ARCHERY, MAP, CLIMB
+	.byte 3			; HEIGHTS
+
 	.byte 1,1,1		; SAVE1, SAVE2, SAVE3
 	.byte $f		; disk detect
 
@@ -40,34 +42,42 @@ load_address_array:
 	.byte $60,$60,$60,$60	; PEASANT1, PEASANT2, PEASANT3, PEASANT4
 	.byte $60,$60,$D0,$60	; TROGDOR, ENDING, MUSIC, CLIFF
 	.byte $60,$D0,$20,$60	; GAME_OVER, INVENTORY, PARSE_INPUT, INN
-	.byte $60,$60,$60	; INSIDE, ARCHERY, MAP
+	.byte $60,$60,$60,$60	; INSIDE, ARCHERY, MAP, CLIMB
+	.byte $60		; HEIGHTS
+
 	.byte $BC,$BC,$BC	; SAVE1, SAVE2, SAVE3
 	.byte $BC		; disk detect
 
 track_array:
         .byte  4, 6, 9,1	; VID_LOGO, TITLE, INTRO, COPY_CHECK
 	.byte 15,20,25,30	; PEASANT1, PEASANT2, PEASANT3, PEASANT4
-	.byte 19,24, 3,29	; TROGDOR, ENDING, MUSIC, CLIFF
-	.byte 15,14,13,14	; GAME_OVER, INVENTORY, PARSE_INPUT, INN
-	.byte  9,6,4		; INSIDE, ARCHERY, MAP
+	.byte 15,19, 3,24	; TROGDOR, ENDING, MUSIC, CLIFF_BASE
+	.byte 15,14,13,11	; GAME_OVER, INVENTORY, PARSE_INPUT, INN
+	.byte  7,3,1,26		; INSIDE, ARCHERY, MAP, CLIMB
+	.byte 30		; HEIGHTS
+
 	.byte  0, 0, 0		; SAVE1, SAVE2, SAVE3
 	.byte  0		; disk detect
 
 sector_array:
         .byte  0, 0, 0, 0	; VID_LOGO, TITLE, INTRO, COPY_CHECK
 	.byte  0, 0, 0, 0	; PEASANT1, PEASANT2, PEASANT3, PEASANT4
-	.byte  0, 0, 0, 0	; TROGDOR, ENDING, MUSIC, CLIFF
+	.byte  0, 0, 0, 0	; TROGDOR, ENDING, MUSIC, CLIFF_BASE
 	.byte  0, 0, 0, 0	; GAME_OVER, INVENTORY, PARSE_INPUT, INN
-	.byte  0, 0, 0		; INSIDE, ARCHERY, MAP
+	.byte  0, 0, 0, 0	; INSIDE, ARCHERY, MAP, CLIMB
+	.byte  0		; HEIGHTS
+
 	.byte  11,12,13		; SAVE1, SAVE2, SAVE3
 	.byte  0		; disk detect
 
 length_array:
         .byte  32, 50, 60, 20	; VID_LOGO, TITLE, INTRO, COPY_CHECK
 	.byte  80, 88, 88, 80	; PEASANT1, PEASANT2, PEASANT3, PEASANT4
-	.byte  80, 80, 16, 80	; TROGDOR, ENDING, MUSIC, CLIFF
-	.byte  16, 16, 16, 80	; GAME_OVER, INVENTORY, PARSE_INPUT, INN
-	.byte  80, 64, 16	; INSIDE, ARCHERY, MAP
+	.byte  64, 80, 16, 32	; TROGDOR, ENDING, MUSIC, CLIFF_BASE
+	.byte  16, 16, 16, 64	; GAME_OVER, INVENTORY, PARSE_INPUT, INN
+	.byte  64, 64, 32, 64	; INSIDE, ARCHERY, MAP, CLIMB
+	.byte  48		; HEIGHTS
+
 	.byte   1,1,1		; SAVE1, SAVE2, SAVE3
 	.byte   1		; disk detect
 
