0f0f6c947a42775ad912f3c5978c2572eb666381
Vince Weaver <vincent.weaver@maine.edu>
2024-11-05 14:55:04 -0500

    driven: have large font going

--- demos/driven/part05_atrus/atrus.s
+++ demos/driven/part05_atrus/atrus.s
@@ -53,6 +53,23 @@ atrus_opener:
 	;=================================
 	; scroller
 	;=================================
+	bit	FULLGR
+
+	lda	#<atrus10_graphics
+	sta	zx_src_l+1
+	lda	#>atrus10_graphics
+	sta	zx_src_h+1
+	lda	#$40				; on both pages
+	jsr	zx02_full_decomp
+
+	jsr	do_scroll
+
+	jsr	wait_until_keypress
+
+
+	;=================================
+	; book start
+	;=================================
 
 	lda	#<atrus10_graphics
 	sta	zx_src_l+1
@@ -60,7 +77,6 @@ atrus_opener:
 	sta	zx_src_h+1
 	lda	#$20
 	jsr	zx02_full_decomp
-
 
 	jsr	wait_until_keypress
 
@@ -109,3 +125,79 @@ atrus_text:
 .include "plasma.s"
 
 .include "font/large_font.inc"
+
+
+do_scroll:
+
+	ldy	#0
+	sty	SCROLL_OFFSET	; FIXME: SCROLL_OFFSET
+do_scroll_loop:
+	ldx	SCROLL_OFFSET
+	lda	scroll_text,X
+	sec
+	sbc	#'@'
+	asl
+	tax
+do_scroll_col_loop:
+	; row1
+	lda	large_font_row0,X
+	sta	$22D0,Y
+	; row2
+	lda	large_font_row1,X
+	sta	$26D0,Y
+	; row3
+	lda	large_font_row2,X
+	sta	$2AD0,Y
+	; row4
+	lda	large_font_row3,X
+	sta	$2ED0,Y
+	; row5
+	lda	large_font_row4,X
+	sta	$32D0,Y
+	; row6
+	lda	large_font_row5,X
+	sta	$36D0,Y
+	; row7
+	lda	large_font_row6,X
+	sta	$3AD0,Y
+	; row8
+	lda	large_font_row7,X
+	sta	$3ED0,Y
+	; row9
+	lda	large_font_row8,X
+	sta	$2350,Y
+	; row10
+	lda	large_font_row9,X
+	sta	$2750,Y
+	; row11
+	lda	large_font_row10,X
+	sta	$2B50,Y
+	; row12
+	lda	large_font_row11,X
+	sta	$2F50,Y
+	; row13
+	lda	large_font_row12,X
+	sta	$3350,Y
+	; row14
+	lda	large_font_row13,X
+	sta	$3750,Y
+	; row15
+	lda	large_font_row14,X
+	sta	$3B50,Y
+	; row16
+	lda	large_font_row15,X
+	sta	$3F50,Y
+
+	inx
+	iny
+	tya
+	and	#1
+	bne	do_scroll_col_loop
+
+	inc	SCROLL_OFFSET
+	cpy	#40
+	bne	do_scroll_loop
+	rts
+
+scroll_text:
+	.byte "THE@QUICK@BROWN@FOX@JUMPED@OVER@THE@LAZY@DOG"
