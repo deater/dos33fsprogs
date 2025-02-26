0fdb229b9286554301b69b975782be845bc25af4
Vince Weaver <vince@deater.net>
2025-02-25 20:21:26 -0500

    starpath: add ampersand version

--- graphics/gr/starpath/andor.s
+++ graphics/gr/starpath/andor.s
@@ -1,9 +1,26 @@
-.org 300
+; calculates &X,Y,Z,C
+;  C = (X|Y)&Z
+
+
+; Routine for calling from &
+;	when called next char/token in A
+;	can't return anything unless you look up a variable by name
+;	and write to it
+
+; USER() can take one argument, returns value
+
+; dump with od -t u1 ./ANDOR
+
+VARPNTL = $83
+VARPNTH = $84
+
 
 FRMEVL = $DD7B
 GETADDR= $E752
 CHKCOM = $DEBE
 SNGFLT = $E301
+PTRGET = $DFE3	; returns pointer to variable in VARPNT and (Y,A)
+MOVMF  = $EB2B
 
 andor:
 	jsr	FRMEVL		; 20 7B DD  (or FRMNUM) 20 67 DD   value->FAC
@@ -22,9 +39,16 @@ andor:
 	jsr	GETADDR		; 20 52 E7  FAC-> LINNUM ($50/$51)
 	jsr	CHKCOM		; 20 BE DE  move past comma
 
-	lda	$50		; A5 50
-	ora	$24		; 05 24
-	and	$25		; 25 25
-	tay			; A8
-	jmp	SNGFLT		; 20 01 E3  unsigned Y -> FAC
+	lda	$24		; A5 24
+	ora	$25		; 05 25
+	and	$50		; 25 50
+	sta	$24
+
+	jsr	PTRGET		; 20 E3 DF
+	ldy	$24		; A8
+	jsr	SNGFLT		; 20 01 E3  unsigned Y -> FAC
+
+	ldx	VARPNTL		;VARPNT
+	ldy	VARPNTH		;VARPNT+1
+	jmp	MOVMF
 
