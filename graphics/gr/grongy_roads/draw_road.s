24545e1042dd2a17cd4c8d31f07be6444fafb635
Vince Weaver <vincent.weaver@maine.edu>
2025-05-12 12:42:50 -0400

    roads: update code comments

--- graphics/gr/grongy_roads/draw_road.s
+++ graphics/gr/grongy_roads/draw_road.s
@@ -1,51 +1,62 @@
+	;=================================================
+	; 194 frames to draw.  This is an annoying number
+	;=================================================
+	; this runs in the interrupt handler?
+	;=================================================
+	; 8 frames at a time are decompressed to a buffer
+	;   this code copies the next one to the proper lo-res page
+	;   if it is frame #7 (the last frame) then it starts decompressing
+	;   the next one.  The hope is it's done the 1st 1k (1 frame)
+	;   before it is time to display that one
 
 draw_road:
+	;==============================
+	; copy in frame
 
-	;================================
-	; copy in MAIN graphics
-
-	ldy	ROAD_COUNT
-	ldx	animation_main,Y
-	jsr	copy_to_400_main
+	ldy	ROAD_COUNT			; get current frame count
+	ldx	animation_main,Y		; get offset to copy from
+	jsr	copy_to_400_main		; copy to proper page
 
 	;==============================
 	; move to next animation frame
 
-	lda	ROAD_FILE
-	cmp	#24
+	lda	ROAD_FILE			; check which of 0..24 we're in
+	cmp	#24				; if not 24 normal handling
 	bne	normal_road
 
-	; urgh only 2 here
+	; File #24 (the 25th one) is "special" and only has two frames
 
-	inc	ROAD_COUNT
+	inc	ROAD_COUNT			; increment
 	lda	ROAD_COUNT
-	and	#$1
+	and	#$1				; wrap at 2
 	jmp	road_common
 
 normal_road:
-	inc	ROAD_COUNT
+	inc	ROAD_COUNT			; increment
 	lda	ROAD_COUNT
-	and	#$7
+	and	#$7				; wrap at 8
 road_common:
-	sta	ROAD_COUNT	; wrap at 8
+	sta	ROAD_COUNT			; store updated value
 
-	bne	no_oflo
+	bne	no_oflo				; if 0 on to next file
 
+	;=============================
+	; next file: start decompress
 
-	; start decompress
-
-
-	lda	#1
-	sta	START_DECOMPRESS
+	lda	#1				; trigger decompress in
+	sta	START_DECOMPRESS		; main thread
 
 no_oflo:
+
+	;===============================
+	; can't VBLANK, waiting up to 1/60s will take too long
 
 ;	jsr	wait_vblank
 
 	;============================
 	; page flip
 
-	jsr	gr_flip_page
+	jsr	gr_flip_page			; page flip
 
 	rts
 
