b7803fef03e67470f4f3dc74fb7b4e466d20f5f9
Vince Weaver <vincent.weaver@maine.edu>
2025-06-04 17:06:01 -0400

    peasant: work on loading peasant sprites

---
+++ games/peasant/sprites_peasant/peasant_sprite_common.s
@@ -0,0 +1,27 @@
+; Note there are various we have to handle
+
+; note, depending what order you do some tasks the outfit might be
+;	temporarily wrong.  From what I gather the real game has this problem
+;	too
+
+; 0 = initial short pants peasant
+; 1 = robe  (with on fire variant)
+; 2 = mud
+; 3 = pot on head
+; 4 = haystack
+
+; ones only on last disk
+; robe+on fire
+; ? = robe + helm
+; ? = robe + shield
+; ? = robe + sword
+
+
+peasant_original_zx02:
+	.incbin "walking_sprites.zx02"
+
+peasant_robe_zx02:
+	.incbin "robe_sprites.zx02"
+
+
+
