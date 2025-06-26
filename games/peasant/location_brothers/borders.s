1179379b378c0f07911b7ad9254c771f9937c87a
Vince Weaver <vincent.weaver@maine.edu>
2025-06-26 13:24:12 -0400

    peasant: fix the archery area a bit with regards to leaving area
    so hopefully don't get stuck in unwalkable positions

--- games/peasant/location_brothers/borders.s
+++ games/peasant/location_brothers/borders.s
@@ -1,4 +1,57 @@
 ; check borders when leaving
 
-; nothing for now
+	;======================================
+	;======================================
+	; special case leaving-level borders
+	;======================================
+	;======================================
+
+check_borders:
+
+	;=====================
+	; check going south
+	;	brothers -> lake_w
+	; FIXME: in real game no issue. In ours can end up in lake on right side?
+
+check_south:
+
+	lda	MAP_LOCATION
+	cmp	#LOCATION_LAKE_WEST
+	bne	check_east
+
+	lda	PEASANT_X
+	cmp	#25
+	bcc	check_south_ok
+
+	lda	#25			; don't let us walk on water
+	sta	PEASANT_X
+
+check_south_ok:
+
+	;=====================
+	; check going east
+	;	brothers -> river
+	; walking east it toward bottom will put you up mid-screen
+check_east:
+	lda	MAP_LOCATION
+	cmp	#LOCATION_RIVER_STONE
+	bne	check_west
+
+	lda	PEASANT_Y
+	cmp	#80
+	bcc	check_west		; blt
+
+	lda	#80
+	sta	PEASANT_Y
+
+check_west:
+
+	;=====================
+	; check going north
+	;	brothers -> well
+	; only exit should be through gap.  Handled by collision though?
+check_north:
+
+check_done:
+
 
--- games/peasant/location_well/borders.s
+++ games/peasant/location_well/borders.s
@@ -1,16 +1,12 @@
 
-	; special case if leaving with baby in well
 
-	; trouble though, by this point MAP_LOCATION is the new one?
+	; check if left baby in the well
 
-	lda	PREVIOUS_LOCATION
-	cmp	#LOCATION_OLD_WELL
-	bne	skip_level_specific
+check_baby_in_well:
 
-at_old_well:
 	lda	GAME_STATE_0
 	and	#BABY_IN_WELL
-	beq	skip_level_specific
+	beq	baby_not_in_well
 
 	ldx	#<well_leave_baby_in_well_message
 	ldy	#>well_leave_baby_in_well_message
@@ -23,7 +19,71 @@ at_old_well:
 	sta	LEVEL_OVER
 	jmp	level_over
 
-skip_level_specific:
+baby_not_in_well:
 
+
+	;======================================
+	;======================================
+	; special case leaving-level borders
+	;======================================
+	;======================================
+
+check_borders:
+
+	;=====================
+	; check going south
+	;	well->brothers
+check_south:
+	; in actual game, walking down center pops you out gap in trees
+	; at archery
+        ; walking to the left side of screen pops you out the trees on left
+	; but you can't go back that way
+
+	lda	MAP_LOCATION
+	cmp	#LOCATION_ARCHERY
+	bne	check_east
+
+	; be sure we're in range
+	lda	PEASANT_X
+	cmp	#6
+	bcs	exit_mid_left
+
+exit_through_trees:
+
+	lda	#8
+	bne	border_update_x			; bra
+
+	;=======================
+	; if 8..25 exit at 25
+	; if 30-40 exit at 29
+	; else exit as-is
+
+exit_mid_left:
+	cmp	#25
+	bcs	exit_mid_right
+	lda	#25
+	bne	border_update_x			; bra
+
+exit_mid_right:
+	cmp	#30
+	bcc	exit_straight_through
+
+	lda	#29
+
+exit_straight_through:
+
+border_update_x:
+	sta	PEASANT_X
+	jmp	check_done
+
+					; fine if at far left
+
+not_goint_to_archery:
+
+check_east:
+check_west:
+check_north:
+
+check_done:
 
 
