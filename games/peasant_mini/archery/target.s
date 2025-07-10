4fc647cc00ee484eff2f4501783feaa60b390f72
Vince Weaver <vince@deater.net>
2025-07-10 19:16:53 -0400

    target: can move string as well

--- games/peasant_mini/archery/target.s
+++ games/peasant_mini/archery/target.s
@@ -100,70 +100,48 @@ bg_data:
 	;=======================
 draw_bow:
 
-	;=======================
+	lda	#0
+	sta	BOW_INDEX
+
+	; check if odd or even
 
 	lda	BOW_X
-	sta	CURSOR_X
-	lda	#159
-	sta	CURSOR_Y
+	and	#$1
+	eor	#$1			; our sprite X is off by one
+	asl
+	asl
+	sta	draw_bow_smc+1
 
-	lda	#<bow_sprite0
-	sta	INL
-	lda	#>bow_sprite0
-	sta	INH
+draw_bow_loop:
+	clc
+	lda	BOW_INDEX
+draw_bow_smc:
+	adc	#0
+	tax
 
-	jsr	hgr_draw_sprite
-
-	;==========================
+	; set xpos,ypos
 
 	clc
 	lda	BOW_X
-	adc	#8
-
+	adc	bow_sprite_offsets,X
 	sta	CURSOR_X
 	lda	#159
 	sta	CURSOR_Y
 
-	lda	#<bow_sprite1
+	; set up sprites
+
+	lda	bow_sprites_l,X
 	sta	INL
-	lda	#>bow_sprite1
+	lda	bow_sprites_h,X
 	sta	INH
 
 	jsr	hgr_draw_sprite
 
-	;====================
+	inc	BOW_INDEX
+	lda	BOW_INDEX
+	cmp	#4
+	bne	draw_bow_loop
 
-	clc
-	lda	BOW_X
-	adc	#15
-
-	sta	CURSOR_X
-	lda	#159
-	sta	CURSOR_Y
-
-	lda	#<bow_sprite2
-	sta	INL
-	lda	#>bow_sprite2
-	sta	INH
-
-	jsr	hgr_draw_sprite
-
-	;====================
-
-	clc
-	lda	BOW_X
-	adc	#22
-
-	sta	CURSOR_X
-	lda	#159
-	sta	CURSOR_Y
-
-	lda	#<bow_sprite3
-	sta	INL
-	lda	#>bow_sprite3
-	sta	INH
-
-	jsr	hgr_draw_sprite
 
 	rts
 
@@ -174,21 +152,48 @@ draw_bow:
 	;=======================
 draw_string:
 
-.if 0
-	;=======================
+	lda	#0
+	sta	BOW_INDEX
+
+	; check if odd or even
 
 	lda	BOW_X
+	and	#$1
+	eor	#$1			; our sprite X is off by one
+	asl
+	sta	draw_string_smc+1
+
+draw_string_loop:
+	clc
+	lda	BOW_INDEX
+draw_string_smc:
+	adc	#0
+	tax
+
+	; set xpos,ypos
+
+	clc
+	lda	BOW_X
+	adc	string_sprite_offsets,X
 	sta	CURSOR_X
-	lda	#159
+	lda	#183
 	sta	CURSOR_Y
 
-	lda	#<bow_sprite0
+	; set up sprites
+
+	lda	string_sprites_l,X
 	sta	INL
-	lda	#>bow_sprite0
+	lda	string_sprites_h,X
 	sta	INH
 
 	jsr	hgr_draw_sprite
-.endif
+
+	inc	BOW_INDEX
+	lda	BOW_INDEX
+	cmp	#2
+	bne	draw_string_loop
+
+
 	rts
 
 
@@ -198,3 +203,31 @@ draw_string:
 draw_arrow:
 
 	rts
+
+bow_sprite_offsets:
+	.byte 0,8,15,22,0,8,15,22
+
+bow_sprites_l:
+	.byte <bow_sprite_odd0,<bow_sprite_odd1
+	.byte <bow_sprite_odd2,<bow_sprite_odd3
+	.byte <bow_sprite_even0,<bow_sprite_even1
+	.byte <bow_sprite_even2,<bow_sprite_even3
+
+bow_sprites_h:
+	.byte >bow_sprite_odd0,>bow_sprite_odd1
+	.byte >bow_sprite_odd2,>bow_sprite_odd3
+	.byte >bow_sprite_even0,>bow_sprite_even1
+	.byte >bow_sprite_even2,>bow_sprite_even3
+
+
+string_sprite_offsets:
+	.byte 3,14,3,14
+
+string_sprites_l:
+	.byte <string_sprite_odd0,<string_sprite_odd1
+	.byte <string_sprite_even0,<string_sprite_even1
+
+string_sprites_h:
+	.byte >string_sprite_odd0,>string_sprite_odd1
+	.byte >string_sprite_even0,>string_sprite_even1
+
