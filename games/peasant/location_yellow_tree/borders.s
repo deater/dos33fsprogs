09b7bd1c5eda94ede1add82f0ffc90f6cc576cbd
Vince Weaver <vincent.weaver@maine.edu>
2025-07-22 15:50:02 -0400

    peasant: fix issue where could get stuck on waterfall page

--- games/peasant/location_yellow_tree/borders.s
+++ games/peasant/location_yellow_tree/borders.s
@@ -1,4 +1,31 @@
-; check borders when leaving
+	;======================================
+	; special case leaving-level borders
 
-; nothing for now
+
+check_borders:
+
+check_south:
+
+	;=====================
+	; check going east
+	;	yellow_tree->wterfall
+
+check_east:
+
+	; possible to get caught on edge of river
+
+	lda	PEASANT_Y
+	cmp	#$54
+	bcc	east_fine
+
+	lda	#$54
+	sta	PEASANT_Y
+
+east_fine:
+
+check_west:
+check_north:
+
+check_done:
+
 
