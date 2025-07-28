fc5f241caabd55ec012ff7ce0665130efb2e54b4
Vince Weaver <vince@deater.net>
2025-07-28 01:49:49 -0400

    peasant: work on remove pot sprites

---
+++ games/peasant/location_outside_inn/draw_remove_pot.s
@@ -0,0 +1,12 @@
+; 0 (wide arms up)
+; 1 (touch pot)
+; 2 (push pot up)
+; 1 (touch pot)
+; 2 (push potup)
+; 1 (touch pot)
+; 3 (pop off head) (2 frames?)
+; 4 to side
+; 5 lower
+; 6 last
+; message
+
