a89138e031faa349b948d527dd0fd2b8fac76ab2
Vince Weaver <vince@deater.net>
2025-10-18 23:54:30 -0400

    hgr: font_drop: semi-decent framerate

--- graphics/hgr/hgr_font_collapse/font_drop.s
+++ graphics/hgr/hgr_font_collapse/font_drop.s
@@ -9,11 +9,16 @@ drop_yadd	= $1400
 hposn_low	= $1800
 hposn_high	= $1900
 
-; Optimization (move_text_screen #0 to #1) $6613:
+; Note the fps values are going to change depending how many chars on screen
+
+; Optimization (move_text_screen #0 to #1) (later draw_text_screen #1 to #2):
 ; $21391	= initial implementation	136081 = ~7.3 fps
 ; $1F0A9	= optimize move to use X,Y	127145 = ~7.9 fps
 ; $1E0A9	= make move fixed loop over 1k	123049 = ~8.1 fps
-; $1DFC8	= inline put_char, optimize	122824 = ~8.1 fps]
+; $1DFC8	= inline put_char, optimize	122824 = ~8.1 fps
+; $1A9BB	= optimize some more		108987 = ~9.2 fps
+; $14E03	= inline move routine		 85507 = ~11.7 fps
+
 
 font_drop:
 
@@ -33,14 +38,12 @@ font_drop:
 	;====================
 
 reinit:
-	;================
-	; set up Y table
 
 	ldx	#0
 setup_drop_y_outer:
 	lda	gr_offsets_l,X
-	sta	OUTL
-	sta	INL
+	sta	OUTL				; OUTL is drop_y
+	sta	INL				; INL is drop_yadd
 
 	lda	gr_offsets_h,X
 	clc
@@ -52,19 +55,19 @@ setup_drop_y_outer:
 
 	ldy	#0
 
-	txa		; ypos = X*8
+	txa					; ypos = X*8
 	asl
 	asl
 	asl
 setup_drop_y_inner:
 
-	sta	(OUTL),Y
+	sta	(OUTL),Y			; store out ypos
 
 	pha
 
-	jsr	random8
-	and	#3
-	adc	#2
+	jsr	random8				; random yadd
+	and	#3				; from 3-4?
+	adc	#3
 	sta	(INL),Y
 
 	pla
@@ -115,15 +118,9 @@ drop_loop:
 
 	jsr	hgr_clearscreen
 
-	;===================
-	; move text screen
-	;===================
-
-	jsr	move_text_screen
-
-	;===================
-	; draw text screen
-	;===================
+	;=========================
+	; draw text + move screen
+	;=========================
 
 	jsr	draw_text_screen
 
@@ -160,231 +157,162 @@ restart:
 
 draw_text_screen:
 	ldx	#0
-	ldy	#0
-	stx	TEXT_X
-	sty	TEXT_Y
 
 copy_text_screen_outer_loop:
-	ldx	TEXT_Y
-	lda	gr_offsets_l,X
-	sta	OUTL
-	sta	INL
-	lda	gr_offsets_h,X
-	sta	OUTH
-	clc
-	adc	#(>drop_y-$4)
-	sta	INH
+	stx	TEXT_Y			; backup X into text_y		; 3
 
-	ldy	#0
-	sty	TEXT_X
+	lda	gr_offsets_l,X		; setup OUTL/H to point		; 4+
+	sta	ctsi_smc2+1		; to text line			; 4
+	sta	ctsi_smc1+1						; 4
+	sta	mts_drop_y_smc1+1					; 4
+	sta	mts_drop_y_smc2+1					; 4
+	sta	mts_drop_yadd_smc1+1					; 4
+
+	lda	gr_offsets_h,X						; 4+
+	sta	ctsi_smc2+2						; 4
+
+	clc				; setup INL/H to point		; 2
+	adc	#(>drop_y-$4)		;  to Ypos table		; 2
+	sta	ctsi_smc1+2						; 4
+	sta	mts_drop_y_smc1+2					; 4
+	sta	mts_drop_y_smc2+2					; 4
+	adc	#(>drop_yadd- >drop_y)					; 2
+	sta	mts_drop_yadd_smc1+2					; 4
+
+	ldy	#0			; xpos starts at 0		; 2
+
 copy_text_screen_inner_loop:
 
+ctsi_smc1:
+	ldx	$dddd,Y		; ypos from table			; 4+
 
-	ldy	TEXT_X		; xpos
-
-	lda	(INL),Y		; ypos
-	tax
-
-	lda	(OUTL),Y	; load from current line
-	and	#$7f		; strip extraneous high bit
+ctsi_smc2:
+	lda	$dddd,Y		; load char from current line		; 4+
+	and	#$7f		; strip extraneous high bit		; 2
 				; note: this means inverse not supported
 
-	cmp	#$20		; don't draw space character
-	bne	font_putchar
+	cmp	#$20		; don't draw space character		; 2
+	bne	font_putchar						; 2/3
 
-	jmp	skip_space
+	jmp	skip_space						; 3
 
 	;================================
 	; inline font_putchar
 
 font_putchar:
 
-;	tay				; put letter to load into Y
+	sta	ctsi_smc3+1	; save char for later			; 4
 
-	pha
+	clc			; only need to clear once		; 2
+				; as not possible to overflow
+				; going from $20 to $40
 
 	; row0
 
-	lda	hposn_low, X		; get low memory offset
-;	clc
-;	adc	TEXT_X			; add in x-coord
-	sta	dcb2_row0+1
-	lda	hposn_high, X		; get high memory offset
-	clc
-	adc	DRAW_PAGE
-	sta	dcb2_row0+2		; save it out
-
-
-	inx				; go to next row
+	lda	hposn_low, X	; get low memory offset			; 4+
+	sta	dcb2_row0+1						; 4
+	lda	hposn_high, X	; get high memory offset		; 4+
+	adc	DRAW_PAGE						; 3
+	sta	dcb2_row0+2	; save it out				; 4
 
 	; row1
 
-	lda	hposn_low, X		; get low memory offset
-;	clc
-;	adc	TEXT_X			; add in x-coord
+	lda	hposn_low+1, X		; get low memory offset
 	sta	dcb2_row1+1
-	lda	hposn_high, X		; get high memory offset
-	clc
+	lda	hposn_high+1, X		; get high memory offset
 	adc	DRAW_PAGE
 	sta	dcb2_row1+2		; save it out
 
-	inx				; go to next row
-
 	; row2
 
-	lda	hposn_low, X		; get low memory offset
-;	clc
-;	adc	TEXT_X			; add in x-coord
+	lda	hposn_low+2, X		; get low memory offset
 	sta	dcb2_row2+1
-	lda	hposn_high, X		; get high memory offset
-	clc
+	lda	hposn_high+2, X		; get high memory offset
 	adc	DRAW_PAGE
 	sta	dcb2_row2+2		; save it out
 
-	inx				; go to next row
-
 	; row3
 
-	lda	hposn_low, X		; get low memory offset
-;	clc
-;	adc	TEXT_X			; add in x-coord
+	lda	hposn_low+3, X		; get low memory offset
 	sta	dcb2_row3+1
-	lda	hposn_high, X		; get high memory offset
-	clc
+	lda	hposn_high+3, X		; get high memory offset
 	adc	DRAW_PAGE
 	sta	dcb2_row3+2		; save it out
 
-	inx				; go to next row
-
 	; row4
 
-	lda	hposn_low, X		; get low memory offset
-;	clc
-;	adc	TEXT_X			; add in x-coord
+	lda	hposn_low+4, X		; get low memory offset
 	sta	dcb2_row4+1
-	lda	hposn_high, X		; get high memory offset
-	clc
+	lda	hposn_high+4, X		; get high memory offset
 	adc	DRAW_PAGE
 	sta	dcb2_row4+2		; save it out
 
-	inx				; go to next row
-
 	; row5
 
-	lda	hposn_low, X		; get low memory offset
-;	clc
-;	adc	TEXT_X			; add in x-coord
+	lda	hposn_low+5, X		; get low memory offset
 	sta	dcb2_row5+1
-	lda	hposn_high, X		; get high memory offset
-	clc
+	lda	hposn_high+5, X		; get high memory offset
 	adc	DRAW_PAGE
 	sta	dcb2_row5+2		; save it out
 
-	inx				; go to next row
-
 	; row6
 
-	lda	hposn_low, X		; get low memory offset
-;	clc
-;	adc	TEXT_X			; add in x-coord
+	lda	hposn_low+6, X		; get low memory offset
 	sta	dcb2_row6+1
-	lda	hposn_high, X		; get high memory offset
-	clc
+	lda	hposn_high+6, X		; get high memory offset
 	adc	DRAW_PAGE
 	sta	dcb2_row6+2		; save it out
 
-	inx				; go to next row
-
 	; row7
 
-	lda	hposn_low, X		; get low memory offset
-;	clc
-;	adc	TEXT_X			; add in x-coord
+	lda	hposn_low+7, X		; get low memory offset
 	sta	dcb2_row7+1
-	lda	hposn_high, X		; get high memory offset
-	clc
+	lda	hposn_high+7, X		; get high memory offset
 	adc	DRAW_PAGE
 	sta	dcb2_row7+2		; save it out
 
 
-	pla
-	tay
+ctsi_smc3:
+	ldx	#$DD			; get character
 
-	ldx	TEXT_X
-
-	lda	CondensedRow0-$19, Y	; get 1-byte font row
+	lda	CondensedRow0-$19, X	; get 1-byte font row		; 4+
 dcb2_row0:
-	sta	$8888,X
+	sta	$dddd,Y							; 5
 
-	lda	CondensedRow1-$19, Y	; get 1-byte font row
+	lda	CondensedRow1-$19, X	; get 1-byte font row
 dcb2_row1:
-	sta	$8888,X
+	sta	$dddd,Y
 
-	lda	CondensedRow2-$19, Y	; get 1-byte font row
+	lda	CondensedRow2-$19, X	; get 1-byte font row
 dcb2_row2:
-	sta	$8888,X
+	sta	$dddd,Y
 
-	lda	CondensedRow3-$19, Y	; get 1-byte font row
+	lda	CondensedRow3-$19, X	; get 1-byte font row
 dcb2_row3:
-	sta	$8888,X
+	sta	$dddd,Y
 
-	lda	CondensedRow4-$19, Y	; get 1-byte font row
+	lda	CondensedRow4-$19, X	; get 1-byte font row
 dcb2_row4:
-	sta	$8888,X
+	sta	$dddd,Y
 
-	lda	CondensedRow5-$19, Y	; get 1-byte font row
+	lda	CondensedRow5-$19, X	; get 1-byte font row
 dcb2_row5:
-	sta	$8888,X
+	sta	$dddd,Y
 
-	lda	CondensedRow6-$19, Y	; get 1-byte font row
+	lda	CondensedRow6-$19, X	; get 1-byte font row
 dcb2_row6:
-	sta	$8888,X
+	sta	$dddd,Y
 
-	lda	CondensedRow7-$19, Y	; get 1-byte font row
+	lda	CondensedRow7-$19, X	; get 1-byte font row
 dcb2_row7:
-	sta	$8888,X
+	sta	$dddd,Y
 
 	;=====================================
 
-skip_space:
-
-	inc	TEXT_X
-	lda	TEXT_X
-	cmp	#40
-	beq	goto_texty
-;	bne	copy_text_screen_inner_loop
-	jmp	copy_text_screen_inner_loop
-
-goto_texty:
-	inc	TEXT_Y
-	lda	TEXT_Y
-	cmp	#24
-
-	beq	goto_done
-
-;	bne	copy_text_screen_outer_loop
-	jmp	copy_text_screen_outer_loop
-
-
-goto_done:
-	rts
-
-
-
-	;===================
-	; move text screen
-	;===================
-	; drop the text
+	;===========================
+	; move text screen inlined
 
 move_text_screen:
-	lda	#>drop_y
-	sta	mts_drop_y_smc1+2
-	sta	mts_drop_y_smc2+2
-	lda	#>drop_yadd
-	sta	mts_drop_yadd_smc1+2
-
-	ldy	#0							; 2
-move_text_screen_inner_loop:
 
 mts_drop_y_smc1:
 	lda	drop_y,Y						; 4
@@ -400,16 +328,28 @@ mts_drop_y_smc2:
 
 skip_drop:
 
-	iny								; 2
-	bne	move_text_screen_inner_loop				; 2/3
+	;==========================================================
 
-	inc	mts_drop_y_smc1+2					; 6
-	inc	mts_drop_y_smc2+2					; 6
-	inc	mts_drop_yadd_smc1+2					; 6
+skip_space:
 
-	lda	mts_drop_y_smc1+2					; 4
-	cmp	#(>drop_y + 4)						; 2
-	bne	move_text_screen_inner_loop				; 2/3
+	iny			; move to next text column		; 2
+	cpy	#40		; see if hit end			; 2
 
+	beq	goto_texty						; 2/3
+	jmp	copy_text_screen_inner_loop				; 3
+
+goto_texty:
+
+	ldx	TEXT_Y		; restore ypos				; 3
+	inx			; move to next text row			; 2
+	cpx	#24		; see if hit end			; 2
+
+	beq	goto_done						; 2/3
+
+	jmp	copy_text_screen_outer_loop				; 3
+
+
+goto_done:
 	rts
+
 
