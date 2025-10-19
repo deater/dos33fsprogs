2c282bf5481322bca0815f52ec7378963cb139c4
Vince Weaver <vince@deater.net>
2025-10-18 21:33:40 -0400

    hgr: drop: have it working sorta

--- graphics/hgr/hgr_font_collapse/font_drop.s
+++ graphics/hgr/hgr_font_collapse/font_drop.s
@@ -22,20 +22,28 @@ font_drop:
 	jsr	build_tables
 
 	;====================
+	;====================
 	; setup drop tables
 	;====================
+	;====================
 
+reinit:
+	;================
 	; set up Y table
 
 	ldx	#0
 setup_drop_y_outer:
 	lda	gr_offsets_l,X
 	sta	OUTL
+	sta	INL
+
 	lda	gr_offsets_h,X
 	clc
 	adc	#(>drop_y-$4)
 	sta	OUTH
-
+	clc
+	adc	#(>drop_yadd - >drop_y)
+	sta	INH
 
 	ldy	#0
 
@@ -47,6 +55,14 @@ setup_drop_y_inner:
 
 	sta	(OUTL),Y
 
+	pha
+
+	jsr	random8
+	and	#3
+	adc	#2
+	sta	(INL),Y
+
+	pla
 	iny
 	cpy	#40
 	bne	setup_drop_y_inner
@@ -109,20 +125,21 @@ drop_loop:
 	; flip page
 	;===================
 
-;retry:
-;	lda	KEYPRESS
-;	bpl	retry
-;	bit	KEYRESET
-
 	jsr	hgr_page_flip
+
+retry:
+	lda	KEYPRESS
+	bmi	restart
 
 	jmp	drop_loop
 
+restart:
+	bit	KEYRESET
+	jmp	reinit
 
 
 	.include "font_putchar.s"
 	.include "fonts/a2_lowercase_font.inc"
-
 
 	.include "hgr_table.s"
 	.include "hgr_clearscreen.s"
@@ -150,6 +167,7 @@ copy_text_screen_outer_loop:
 	sta	INL
 	lda	gr_offsets_h,X
 	sta	OUTH
+	clc
 	adc	#(>drop_y-$4)
 	sta	INH
 
@@ -168,7 +186,7 @@ copy_text_screen_inner_loop:
 	and	#$7f		; strip extraneous high bit
 				; note: this means inverse not supported
 
-	cmp	#$20
+	cmp	#$20		; don't draw space character
 	beq	skip_space
 
 	jsr	font_putchar
@@ -179,12 +197,6 @@ skip_space:
 	lda	TEXT_X
 	cmp	#40
 	bne	copy_text_screen_inner_loop
-
-;	lda	copy_text_screen_ysmc+1
-;	clc
-;	adc	#8
-;	sta	copy_text_screen_ysmc+1
-
 
 	inc	TEXT_Y
 	lda	TEXT_Y
@@ -212,10 +224,12 @@ move_text_screen_outer_loop:
 	sta	OUTL
 	sta	INL
 	lda	gr_offsets_h,X
-	sta	OUTH
 	clc
 	adc	#(>drop_y-$4)
 	sta	INH
+	clc
+	adc	#(>drop_yadd - > drop_y)
+	sta	OUTH
 
 	ldy	#0
 	sty	TEXT_X
@@ -224,10 +238,12 @@ move_text_screen_inner_loop:
 
 	ldy	TEXT_X		; xpos
 
-	jsr	random8
-	and	#3
+;	jsr	random8
+;	and	#3
 
-	adc	(INL),Y		; load current ypos for char
+	lda	(INL),Y		; load current ypos for char
+	clc
+	adc	(OUTL),Y
 
 ;	clc			; adjust it
 ;	adc	#1
