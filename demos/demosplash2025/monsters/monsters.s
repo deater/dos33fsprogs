8c691ccbf1380890971712e6a4326e3293464109
Vince Weaver <vince@deater.net>
2025-10-26 00:00:18 -0400

    ds25: fix up monsters to use new code

--- demos/demosplash2025/monsters/monsters.s
+++ demos/demosplash2025/monsters/monsters.s
@@ -4,6 +4,8 @@
 .include "../music.inc"
 .include "../common_defines.inc"
 
+; 8742 -- original
+
 	;=============================
 	; draw monsters
 	;=============================
@@ -12,60 +14,84 @@ monsters:
 	bit	KEYRESET	; just to be safe
 
 	;=================================
-	; Scrolling Intro Logo
-	;=================================
-
-	;=================================
 	; init graphics
 	;=================================
 
-	; We are first to run, so init double-hires
+	; come in HGR mode, so switch back to dhgr
 
-;	bit	SET_GR
-;	bit	HIRES
-;	bit	FULLGR
-;	sta	AN3		; set double hires
-;	sta	EIGHTYCOLON	; 80 column
-;	sta	CLR80COL
+	bit	SET_GR
+	bit	HIRES
+	bit	FULLGR
+	sta	AN3		; set double hires
+	sta	EIGHTYCOLON	; 80 column
+	sta	CLR80COL
 ;	sta	SET80COL	; (allow page1/2 flip main/aux)
 
 	bit	PAGE1		; display page1
-	lda	#$20
-	sta	DRAW_PAGE	; draw to page2
+;	lda	#$20
+;	sta	DRAW_PAGE	; draw to page2
 
+	;========================
+	;========================
+	; load pumpkin
+	;========================
+	;========================
 
-	;=======================
-	; load graphic to page1
+	;=============================
+	; load top part to MAIN $A000
 
-	lda	#$0
+	lda	#$80
 	sta	DRAW_PAGE
 
-	lda	#<monster1_bin
+	lda	#<monster1_top
 	sta	zx_src_l+1
-	lda	#>monster1_bin
+	lda	#>monster1_top
 	sta	zx_src_h+1
 	jsr	zx02_full_decomp_main
 
-	lda	#<monster1_aux
+	; depack to page1
+
+	lda	#$00
+	sta	DRAW_PAGE
+
+	lda	#$a0
+	jsr	dhgr_repack_top
+
+
+	;=============================
+	; load bottom part to MAIN $A000
+
+	lda	#$80
+	sta	DRAW_PAGE
+
+	lda	#<monster1_bottom
 	sta	zx_src_l+1
-	lda	#>monster1_aux
+	lda	#>monster1_bottom
 	sta	zx_src_h+1
-	jsr	zx02_full_decomp_aux
+	jsr	zx02_full_decomp_main
 
-	; wait a bit
+	; depack to page1
 
-	lda	#5
-	jsr	wait_seconds
+	lda	#$00
+	sta	DRAW_PAGE
 
+	lda	#$a0
+	jsr	dhgr_repack_bottom
 
+	jsr	wait_until_keypress
 
 	rts
 
+monster1_top:
+	.incbin "graphics/monster_pumpkin.raw_top.zx02"
+
+monster1_bottom:
+	.incbin "graphics/monster_pumpkin.raw_bottom.zx02"
 
 
 
-monster1_bin:
-	.incbin "graphics/monster_pumpkin.bin.zx02"
+;monster1_bin:
+;	.incbin "graphics/monster_pumpkin.bin.zx02"
 
-monster1_aux:
-	.incbin "graphics/monster_pumpkin.aux.zx02"
+;monster1_aux:
+;	.incbin "graphics/monster_pumpkin.aux.zx02"
