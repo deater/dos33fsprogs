80a2b66af870aefd3cc64427c899ea3a756c8c1c
Vince Weaver <vince@deater.net>
2025-10-25 23:27:40 -0400

    ds25: fix up files

---
+++ demos/demosplash2025/intro/extra.s
@@ -0,0 +1,8 @@
+; contains extra data that goes into $A000 that will get over-written
+; this frees up some room at start
+
+title_hgr:
+	.incbin "graphics/ms_title.hgr.zx02"
+
+logo1_main:
+	.incbin "graphics/logo_grafA.bin.zx02"
