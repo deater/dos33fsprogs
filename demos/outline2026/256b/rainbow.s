d4ce60acde9351aa2b23f309a72058609e3d10ab
Vince Weaver <vincent.weaver@maine.edu>
2026-05-15 13:08:15 -0400

    outline2026: known good

--- demos/outline2026/256b/rainbow.s
+++ demos/outline2026/256b/rainbow.s
@@ -143,41 +143,36 @@ lp17029:
 	;================================================
 	; do the cycle-counted effect
 	;================================================
-	;
+	; 184 lines of zig-zag lo/hires
+	; 8 lines of text mode (scrolling OUTLINE)
+	; vblank
+	; alternate page1/page2 each time through
 
 	ldx	#0
 
 main_loop:
 
-	nop			; 2	; 2
-	nop			; 2	; 4
-	nop			; 2	; 6
+	; wait 24 cycles then switch to HIRES mode
+	;	24 = 12 nop instrcutions (12 bytes)
+	;		jsr to delay 12 twice (6 bytes)
 
-	nop			; 2	; 8
-	nop			; 2	; 10
-	nop			; 2	; 12
+	jsr	delay_12	; 12	; 12
+	jsr	delay_12	; 12	; 24
 
-	nop			; 2	; 14
-	nop			; 2	; 16
-	nop			; 2	; 18
-	nop			; 2	; 20
-	nop			; 2	; 22
+	; switch to HIRES
 
-	bit	HIRES		; 4	; 26
+	bit	HIRES		; 4	; 28
 
-	nop			; 2	; 28
+	; delay 12 cycles then switch to LORES
 
-	nop			; 2	; 30
-	nop			; 2	; 32
-	nop			; 2	; 34
+	jsr	delay_12	; 12	; 40
 
-	nop			; 2	; 36
-	nop			; 2	; 38
-	nop			; 2	; 40
 	bit	LORES		; 4	; 44
 
-	nop
-;	inx			; 2	; 46
+	; create zig-zags
+	; delay one fewer/one more depending on current row in X
+
+	nop			; 2	; 46
 	txa			; 2	; 48
 	and	#$4		; 2	; 50
 	beq	less		; 2/3
@@ -192,31 +187,76 @@ more_done:
 	nop			; 2	; 55 / 57
 
 	nop			; 2	; 57 / 59
-;	nop			; 2	; 59 / 61
-;	nop			; 2	; 61 / 63
 
-;	jmp	main_loop	; 3	; 64 / 66
+	; increment row and see if at end
 
-	inx			; 2
-	cpx	#184		; 2
-	bne	main_loop	; 3/2
+	inx			; 2	; 59 / 61
+	cpx	#184		; 2	; 61 / 63
+	bne	main_loop	; 3/2	; 64 / 66
+
+
+	;=======================================
+	; enter text mode for 8 lines (well, until end of VBLANK)
+start_text:
+	bit	SET_TEXT		; 4
+
+
+	;=======================================
+	; VBLANK
+	;=======================================
 
 	; need to delay 4551 = (4550+1) from fall through
 	; plus8*65 for text mode = 5071
 
+	;====================================
+	; delay first to avoid text tearing
+	; 5071
+	; -999 string move
+	; -5   inc frame
+	; -9 end loop
+	;===============
+	; 4058
+
+	; dealy = 4058
+
+
+	;=============================
+	; delay!
+	;=============================
+
+	; 9*(256+192)=4032
+
+	lda	#1		; 2
+	ldy	#192		; 2
+	jsr	size_delay	; 20+4032 = 4052
+
+	; padding
+
+	nop			; 2
+				;=============
+				; 4032+20+6=4058
+
+
+	;================================================
+	; move the string at the bottom
+	;================================================
+	; note it's too fast, only do this one time in 4?
 
 move_string:
-	bit	SET_TEXT		; 4
 
 	ldy	#0			; 2
 move_string_loop:
 	lda	$7D1,Y			; 4+
 	sta	$7D0,Y			; 5
-	lda	$BD1,Y			; 4+
+	nop
+	nop
+;	lda	$BD1,Y			; 4+
 	sta	$BD0,Y			; 5
 	iny				; 2
 	cpy	#39			; 2
 	bne	move_string_loop	; 2/3
+
+	; draw new character
 
 	lda	FRAME		; 3
 	and	#$7		; 2
@@ -230,26 +270,16 @@ move_string_loop:
 	; 4+2+(25*39)-1 = 975+5 =980
 	; 980+19 = 999
 
-	; 5071-995=4072
+	;===========================
+	; increment frame
 
-					; 4072
-	ldx	#0	; 2		; 4070
-	lda	#1	; 2		; 4068
-	ldy	#192	; 2		; 4066
+	inc	FRAME		; 5
 
-					; 7 from end = 4059
+	;==========================
+	; restore graphics mode
 
-
-	; 9*(256+192)=4032
-
-	jsr	size_delay	;	; 20+4032 = 4052
-
-
-	inc	FRAME	; 5
-	nop
-
-
-	bit	SET_GR		; 4
+	ldx	#0		; 2		; clear X
+	bit	SET_GR		; 4		; reset graphics
 	jmp	main_loop	; 3
 
 
