c1df5514fb06cde980cb111a570ebc8e04cd746a
Vince Weaver <vince@deater.net>
2026-06-13 01:26:55 -0400

    target: one more sound effect

--- games/peasant_mini/archery/target.s
+++ games/peasant_mini/archery/target.s
@@ -842,6 +842,39 @@ wind_offset_h:
 
 
 launch_sound:
+
+	lda	#NOTE_A4		; 440
+	sta	speaker_frequency
+	lda	#10
+	sta	speaker_duration
+	jsr	speaker_tone
+
+	lda	#NOTE_G4		; 400
+	sta	speaker_frequency
+	lda	#10
+	sta	speaker_duration
+	jsr	speaker_tone
+
+	lda	#NOTE_E4		;
+	sta	speaker_frequency
+	lda	#10
+	sta	speaker_duration
+	jsr	speaker_tone
+
+	lda	#NOTE_C4		;
+	sta	speaker_frequency
+	lda	#10
+	sta	speaker_duration
+	jsr	speaker_tone
+
+	lda	#NOTE_G5		; 800
+	sta	speaker_frequency
+	lda	#30
+	sta	speaker_duration
+	jsr	speaker_tone
+
+
+
 	rts
 
 miss_sound:
