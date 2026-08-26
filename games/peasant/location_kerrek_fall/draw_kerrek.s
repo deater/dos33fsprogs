2a420ed72e20c4d90842bdb5ec6e00fee5d9bcca
Vince Weaver <vince@deater.net>
2026-08-26 01:07:02 -0400

    peasant: update sprites
    more consistent colors

--- games/peasant/location_kerrek_fall/draw_kerrek.s
+++ games/peasant/location_kerrek_fall/draw_kerrek.s
@@ -59,18 +59,8 @@ kerrek_draw_walk:
 	lda	KERREK_Y
 	sta	SPRITE_Y
 
-	ldx	KERREK_COUNT
+	ldx	KERREK_COUNT		; no need to adjust left/right
 
-;	lda	KERREK_STATE
-;	and	#KERREK_DIRECTION	; 0=LEFT
-;	beq	kerrek_correct_dir
-
-;	txa				; could just OR with 8?
-;	clc
-;	adc	#$8
-;	tax
-
-kerrek_correct_dir:
 	jsr	hgr_draw_sprite_mask
 
 	rts				; tail call?
